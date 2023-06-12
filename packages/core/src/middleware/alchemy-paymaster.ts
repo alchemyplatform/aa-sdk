7ad25b2eda9e193865822d5e3d8680f0948df148
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2023-06-12 11:27:35 -0400

    refactor: modify pamyaster middleware to be able to account for gas (#19)

--- packages/core/src/middleware/alchemy-paymaster.ts
+++ packages/core/src/middleware/alchemy-paymaster.ts
@@ -2,8 +2,7 @@ import type { Address, Hex, Transport } from "viem";
 import type { BaseSmartContractAccount } from "../account/base.js";
 import type { PublicErc4337Client } from "../client/types.js";
 import type { SmartAccountProvider } from "../provider/base.js";
-import type { ISmartAccountProvider } from "../provider/types.js";
-import type { UserOperationRequest, UserOperationStruct } from "../types.js";
+import type { UserOperationRequest } from "../types.js";
 import { deepHexlify, resolveProperties } from "../utils.js";
 
 type ClientWithAlchemyMethods = PublicErc4337Client & {
@@ -76,69 +75,24 @@ export const withAlchemyGasManager = <
         maxFeePerGas: 0n,
         maxPriorityFeePerGas: 0n,
       }))
-      .withCustomMiddleware(async (struct) => {
-        const result = await (
-          config.provider as ClientWithAlchemyMethods
-        ).request({
-          method: "alchemy_requestGasAndPaymasterAndData",
-          params: [
-            {
-              policyId: config.policyId,
-              entryPoint: config.entryPoint,
-              userOperation: deepHexlify(await resolveProperties(struct)),
-              dummySignature: provider.account.getDummySignature(),
-            },
-          ],
-        });
+      .withPaymasterMiddleware({
+        paymasterDataMiddleware: async (struct) => {
+          const result = await (
+            config.provider as ClientWithAlchemyMethods
+          ).request({
+            method: "alchemy_requestGasAndPaymasterAndData",
+            params: [
+              {
+                policyId: config.policyId,
+                entryPoint: config.entryPoint,
+                userOperation: deepHexlify(await resolveProperties(struct)),
+                dummySignature: provider.account.getDummySignature(),
+              },
+            ],
+          });
 
-        return {
-          ...struct,
-          ...result,
-        };
+          return result;
+        },
       })
   );
 };
-
-/**
- * This is the middleware for calling the alchemy paymaster API which does not estimate gas. It's recommend to use
- * {@link withAlchemyGasManager} instead which handles estimating gas + getting paymaster data in one go.
- *
- * @param config {@link AlchemyPaymasterConfig}
- * @returns middleware overrides for paymaster middlewares
- */
-export const alchemyPaymasterAndDataMiddleware = (
-  config: AlchemyPaymasterConfig
-): Parameters<ISmartAccountProvider["withPaymasterMiddleware"]>["0"] => ({
-  dummyPaymasterDataMiddleware: async (_struct: UserOperationStruct) => {
-    switch (config.provider.chain.id) {
-      case 1:
-      case 137:
-      case 42161:
-        return {
-          paymasterAndData:
-            "0x4Fd9098af9ddcB41DA48A1d78F91F1398965addcfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c",
-        };
-      default:
-        return {
-          paymasterAndData:
-            "0xc03aac639bb21233e0139381970328db8bceeb67fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c",
-        };
-    }
-  },
-  paymasterDataMiddleware: async (struct: UserOperationStruct) => {
-    const { paymasterAndData } = await (
-      config.provider as ClientWithAlchemyMethods
-    ).request({
-      method: "alchemy_requestPaymasterAndData",
-      params: [
-        {
-          policyId: config.policyId,
-          entryPoint: config.entryPoint,
-          userOperation: deepHexlify(await resolveProperties(struct)),
-        },
-      ],
-    });
-
-    return { paymasterAndData };
-  },
-});
