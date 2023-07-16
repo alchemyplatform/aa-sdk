eeeb46510ac31947f3a5d4e3bc97da969c5675db
avasisht23 <ajay@alchemy.com>
2023-07-17 01:36:33 +0200

    chore: separate e2e and integration test scripts

--- packages/core/vitest.config.e2e.ts
+++ packages/core/vitest.config.e2e.ts
@@ -2,11 +2,10 @@ import { configDefaults, defineProject } from "vitest/config";
 
 export default defineProject({
   test: {
-    singleThread: true,
     globals: true,
     setupFiles: ["../../.vitest/setupTests.ts"],
-    include: [...configDefaults.include, "**/__tests__/**/*.test.ts"],
-    exclude: [...configDefaults.exclude, "**/e2e-tests/**/*.test.ts"],
+    include: [...configDefaults.include, "**/e2e-tests/**/*.test.ts"],
+    exclude: [...configDefaults.exclude, "**/__tests__/**/*.test.ts"],
     name: "core",
   },
 });
