2d4eb0104b138dd09c6043579e4e48e9fa4ca0b4
avasisht23 <ajay@alchemy.com>
2023-07-17 02:33:16 +0200

    refactor: rearrange tests

--- packages/accounts/src/kernel-zerodev/__tests__/account.test.ts
+++ packages/accounts/src/kernel-zerodev/__tests__/account.test.ts
@@ -1,17 +1,10 @@
-import {
-    PrivateKeySigner
-} from "@alchemy/aa-core";
-import {
-    encodeAbiParameters,
-    parseAbiParameters,
-    type Address,
-    type Hex,
-} from "viem";
+import { PrivateKeySigner } from "@alchemy/aa-core";
+import { type Address, type Hex } from "viem";
 import { generatePrivateKey } from "viem/accounts";
 import { polygonMumbai } from "viem/chains";
 import {
-    KernelSmartContractAccount,
-    type KernelSmartAccountParams,
+  KernelSmartContractAccount,
+  type KernelSmartAccountParams,
 } from "../account.js";
 import { KernelAccountProvider } from "../provider.js";
 import { KernelBaseValidator, ValidatorMode } from "../validator/base.js";
@@ -51,10 +44,6 @@ describe("Kernel Account Tests", () => {
     config.chain
   );
 
-  function connect(index: bigint, owner = mockOwner) {
-    return provider.connect((_provider) => account(index, owner));
-  }
-
   function account(index: bigint, owner = mockOwner) {
     const accountParams: KernelSmartAccountParams = {
       rpcClient: provider.rpcClient,
@@ -68,30 +57,6 @@ describe("Kernel Account Tests", () => {
     };
     return new KernelSmartContractAccount(accountParams);
   }
-
-  it("getAddress returns valid counterfactual address", async () => {
-    //contract already deployed
-    let signerWithProvider = connect(0n);
-
-    expect(await signerWithProvider.getAddress()).eql(
-      "0x97925A25C6B8E8902D2c68A4fcd90421a701d2E8"
-    );
-
-    //contract already deployed
-    signerWithProvider = connect(3n);
-    expect(await signerWithProvider.getAddress()).eql(
-      "0xA7b2c01A5AfBCf1FAB17aCf95D8367eCcFeEb845"
-    );
-  });
-
-  it("getNonce returns valid nonce", async () => {
-    //contract deployed but no transaction
-    const signer: KernelSmartContractAccount = account(0n);
-    expect(await signer.getNonce()).eql(0n);
-
-    const signer2: KernelSmartContractAccount = account(3n);
-    expect(await signer2.getNonce()).eql(2n);
-  });
 
   it("encodeExecute returns valid encoded hash", async () => {
     const signer: KernelSmartContractAccount = account(0n);
@@ -119,39 +84,6 @@ describe("Kernel Account Tests", () => {
     );
   });
 
-  it("signWithEip6492 should correctly sign the message", async () => {
-    const messageToBeSigned: Hex =
-      "0xa70d0af2ebb03a44dcd0714a8724f622e3ab876d0aa312f0ee04823285d6fb1b";
-    const magicBytes =
-      "6492649264926492649264926492649264926492649264926492649264926492";
-    const ownerSignedMessage =
-      "0x4d61c5c27fb64b207cbf3bcf60d78e725659cff5f93db9a1316162117dff72aa631761619d93d4d97dfb761ba00b61f9274c6a4a76e494df644d968dd84ddcdb1c";
-    const factoryCode =
-      "0x296601cd000000000000000000000000180d6465f921c7e0dea0040107d342c87455fff50000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000001448D4d3536cDe7A257087206870c6B6E76e3D4ff4000000000000000000000000";
-    const signature =
-      encodeAbiParameters(parseAbiParameters("address, bytes, bytes"), [
-        config.accountFactoryAddress,
-        factoryCode,
-        ownerSignedMessage,
-      ]) + magicBytes;
-
-    const signer = connect(0n);
-    expect(
-      await signer.request({
-        method: "personal_sign",
-        params: [messageToBeSigned, await signer.getAddress()],
-      })
-    ).toBe(ownerSignedMessage);
-
-    const signer2 = connect(10n);
-    expect(
-      await signer2.request({
-        method: "personal_sign",
-        params: [messageToBeSigned, await signer2.getAddress()],
-      })
-    ).toBe(signature);
-  });
-
   it("signMessage should correctly sign the message", async () => {
     const messageToBeSigned: Hex =
       "0xa70d0af2ebb03a44dcd0714a8724f622e3ab876d0aa312f0ee04823285d6fb1b";
@@ -165,16 +97,5 @@ describe("Kernel Account Tests", () => {
     expect(await signer2.signMessage(messageToBeSigned)).toBe(
       "0x000000004d61c5c27fb64b207cbf3bcf60d78e725659cff5f93db9a1316162117dff72aa631761619d93d4d97dfb761ba00b61f9274c6a4a76e494df644d968dd84ddcdb1c"
     );
-  });
-
-  //non core functions
-  it("should correctly identify whether account is deployed", async () => {
-    //contract already deployed
-    const signer2 = account(3n);
-    expect(await signer2.isAccountDeployed()).eql(true);
-
-    //contract not deployed
-    const signer3 = account(4n);
-    expect(await signer3.isAccountDeployed()).eql(false);
   });
 });
