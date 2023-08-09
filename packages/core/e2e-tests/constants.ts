e0f7b12bdae2217ac38ab3d7d87dff2434c67c50
dancoombs <dan.coombs@alchemy.com>
2023-08-09 11:27:56 -0400

    chore(ci): allow for URL override in e2e tests

--- packages/core/e2e-tests/constants.ts
+++ packages/core/e2e-tests/constants.ts
@@ -1,2 +1,3 @@
-export const API_KEY = process.env.API_KEY!;
+export const RPC_URL = process.env.RPC_URL;
+export const API_KEY = process.env.API_KEY;
 export const OWNER_MNEMONIC = process.env.OWNER_MNEMONIC!;
