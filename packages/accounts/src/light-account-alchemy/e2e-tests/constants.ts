218068994edb1f7d3ac396e839be7e9ace4aaeb8
avasisht23 <ajay@alchemy.com>
2023-09-20 15:16:17 -0400

    fix: remove skips

--- packages/accounts/src/light-account-alchemy/e2e-tests/constants.ts
+++ packages/accounts/src/light-account-alchemy/e2e-tests/constants.ts
@@ -1,5 +1,5 @@
-export const RPC_URL = process.env.RPC_URL;
-export const API_KEY = process.env.API_KEY;
+export const RPC_URL = process.env.RPC_URL!;
+export const API_KEY = process.env.API_KEY!;
 export const LIGHT_ACCOUNT_OWNER_MNEMONIC =
-  "method purse certain chronic brave civil disorder muscle black flower hover salad";
+  process.env.LIGHT_ACCOUNT_OWNER_MNEMONIC!;
 // todo(ajay): replace with OWNER_MNEMONIC when light account factory address when live (accidentally moved owner account to a different mnemonic during testing)
