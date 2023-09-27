12f060de2ab7b704352869d8e8037036d2cf91d1
avasisht23 <ajay@alchemy.com>
2023-09-27 12:21:24 -0400

    fix: remove deprecated signers

--- packages/core/src/signer/private-key.ts
+++
@@ -1,7 +0,0 @@
-import { type PrivateKeyAccount } from "viem";
-import { LocalAccountSigner } from "./local-account.js";
-
-/**
- * @deprecated use LocalAccountSigner instead
- */
-export class PrivateKeySigner extends LocalAccountSigner<PrivateKeyAccount> {}
