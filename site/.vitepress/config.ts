744664a2fd955b9682ad8bf2058349245a0b97af
avarobinson <45052879+avarobinson@users.noreply.github.com>
2023-09-29 11:42:04 -0400

    docs: add code docs for aa-core/getUserOperationByHash (#31)

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -76,6 +76,10 @@ export default defineConfig({
                 text: "waitForUserOperationTransaction",
                 link: "/waitForUserOperationTransaction",
               },
+              {
+                text: "getUserOperationByHash",
+                link: "/getUserOperationByHash",
+              },
             ],
           },
           { text: "Accounts" },
