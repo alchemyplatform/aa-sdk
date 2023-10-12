8e7e0d4e858eec2e3cb26679801e2a4426f71993
Sahil Aujla <sahilaujla15@gmail.com>
2023-10-12 10:56:26 -0400

    fix(site): add og image for Twitter and explicit global fallback titles and descriptions (#141)

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -554,9 +554,40 @@ export default defineConfig({
       },
     ],
     ["link", { rel: "icon", href: "/favicon.ico" }],
+
+    // Open Graph tags
+    ["meta", { property: "og:title", content: "Account Kit" }],
+    [
+      "meta",
+      {
+        property: "og:description",
+        content:
+          "Account Kit is a framework to embed smart accounts in your web3 app, unlocking powerful features like email/social login, gas sponsorship, batched transactions, and more.",
+      },
+    ],
     ["meta", { property: "og:image", content: "/images/og-image.jpg" }],
     ["meta", { property: "og:image:type", content: "image/jpeg" }],
     ["meta", { property: "og:image:width", content: "2400" }],
     ["meta", { property: "og:image:height", content: "1260" }],
+
+    // Twitter Card tags
+    ["meta", { name: "twitter:card", content: "summary_large_image" }],
+    ["meta", { name: "twitter:title", content: "Account Kit" }],
+    [
+      "meta",
+      {
+        name: "twitter:description",
+        content:
+          "Account Kit is a framework to embed smart accounts in your web3 app, unlocking powerful features like email/social login, gas sponsorship, batched transactions, and more.",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "twitter:image",
+        content:
+          "https://github-production-user-asset-6210df.s3.amazonaws.com/83442423/274634864-34ec38a9-3de2-4075-9723-e998fa8aa7d8.jpg",
+      },
+    ],
   ],
 });
