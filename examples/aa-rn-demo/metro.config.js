e3518fc30615864b2d1beb524b2954e8ec4583ad
Dennis Won <jhwon0820@gmail.com>
2023-10-21 10:16:58 -0700

    fix: fix metro

--- examples/aa-rn-demo/metro.config.js
+++ examples/aa-rn-demo/metro.config.js
@@ -1,36 +1,59 @@
 /**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
+ * Metro Bundler configuration
+ * https://facebook.github.io/metro/docs/en/configuration
  *
- * @format
+ * eslint-env node, es6
  */
 
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { getDefaultConfig } = require("@react-native/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
 
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"],
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
-      },
-    ),
-    unstable_enablePackageExports: true,
-  },
-};
+const exclusionList = require("metro-config/src/defaults/exclusionList");
+const getWorkspaces = require("get-yarn-workspaces");
+const path = require("path");
 
-module.exports = mergeConfig(defaultConfig, config);
+function getConfig(appDir, options = {}) {
+  const workspaces = getWorkspaces(appDir);
+  // Add additional Yarn workspace package roots to the module map
+  // https://bit.ly/2LHHTP0
+  const watchFolders = [
+    path.resolve(appDir, "..", "..", "node_modules"),
+    ...workspaces.filter((workspaceDir) => workspaceDir.includes("packages")),
+  ];
+  console.log("watchFolders", watchFolders);
+
+  return {
+    watchFolders,
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"],
+      extraNodeModules: {
+        // Resolve all react-native module imports to the locally-installed version
+        "react-native": path.resolve(appDir, "node_modules", "react-native"),
+
+        // Resolve additional nohoist modules depended on by other packages
+        "react-native-svg": path.resolve(
+          appDir,
+          "node_modules",
+          "react-native-svg",
+        ),
+
+        // Resolve core-js imports to the locally installed version
+        "core-js": path.resolve(appDir, "node_modules", "core-js"),
+      },
+    },
+  };
+}
+
+module.exports = getConfig(__dirname);
