d92764add3efdbaa1a58896eaabe2cd490af1c00
Ajay Vasisht <43521356+avasisht23@users.noreply.github.com>
2023-11-01 13:28:10 -0400

    docs: add particle network to signers (#194)

---
+++ site/snippets/particle.ts
@@ -0,0 +1,25 @@
+import { ParticleNetwork } from "@particle-network/auth";
+import { ParticleProvider } from "@particle-network/provider";
+import { createWalletClient, custom } from "viem";
+import { WalletClientSigner, type SmartAccountSigner } from "@alchemy/aa-core";
+
+const particle = new ParticleNetwork({
+  projectId: process.env.REACT_APP_PROJECT_ID as string,
+  clientKey: process.env.REACT_APP_CLIENT_KEY as string,
+  appId: process.env.REACT_APP_APP_ID as string,
+  chainName: "polygon",
+  chainId: 80001,
+});
+
+const particleProvider = new ParticleProvider(particle.auth);
+
+// Assumes user has logged in with something like particle.auth.login(), Particle Connect, or through other misc. connection
+
+const particleWalletClient = createWalletClient({
+  transport: custom(particleProvider),
+});
+
+export const particleSigner: SmartAccountSigner = new WalletClientSigner(
+  particleWalletClient,
+  "particle"
+);
