d9824c5d9d985f801e075322062ef245d754f44a
avasisht23 <ajay@alchemy.com>
2023-11-02 00:22:18 -0400

    docs: rename overview

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -146,7 +146,7 @@ export default defineConfig({
                 link: "/introduction",
               },
               {
-                text: "constructor for SmartContractProvider",
+                text: "constructor for SmartAccountProvider",
                 link: "/constructor",
               },
               {
