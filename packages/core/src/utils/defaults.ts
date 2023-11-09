1bb816a7f9106572745304ae8bd405d0aa9c6ab7
Dennis Won <denniswon@users.noreply.github.com>
2023-11-09 10:34:18 -0800

    feat: aa-sdk with native arb sepolia support (#231)

--- packages/core/src/utils/defaults.ts
+++ packages/core/src/utils/defaults.ts
@@ -67,8 +67,6 @@ export const getDefaultSimpleAccountFactoryAddress = (
     case optimismGoerli.id:
     case arbitrumGoerli.id:
       return "0x9406Cc6185a346906296840746125a0E44976454";
-    case arbitrumSepolia.id:
-      return "0x8d4CCBaa194ED721AC3e231a1A3863966c2D7921";
   }
 
   throw new Error(
