5bfbee98d2ec90180d940b2d969da391ff73d755
avasisht23 <ajay@alchemy.com>
2023-11-12 15:47:39 -0500

    chore: lint

--- packages/accounts/src/light-account/e2e-tests/constants.ts
+++ packages/accounts/src/light-account/e2e-tests/constants.ts
@@ -2,4 +2,5 @@ export const API_KEY = process.env.API_KEY!;
 export const PAYMASTER_POLICY_ID = process.env.PAYMASTER_POLICY_ID!;
 
 export const UNDEPLOYED_OWNER_MNEMONIC = process.env.UNDEPLOYED_OWNER_MNEMONIC!;
-export const LIGHT_ACCOUNT_OWNER_MNEMONIC = process.env.LIGHT_ACCOUNT_OWNER_MNEMONIC!;
+export const LIGHT_ACCOUNT_OWNER_MNEMONIC =
+  process.env.LIGHT_ACCOUNT_OWNER_MNEMONIC!;
