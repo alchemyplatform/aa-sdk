db8a64fef4198e24826614d20775c48b7a8d5878
Ajay Vasisht <43521356+avasisht23@users.noreply.github.com>
2023-11-17 02:00:34 -0500

    docs: re-write getting started doc (#256)

--- site/snippets/getting-started/provider.ts
+++ site/snippets/getting-started/provider.ts
@@ -4,13 +4,16 @@ import {
 } from "@alchemy/aa-accounts";
 import { AlchemyProvider } from "@alchemy/aa-alchemy";
 import { LocalAccountSigner, type Hex } from "@alchemy/aa-core";
-import { sepolia } from "viem/chains";
+import { polygonMumbai } from "viem/chains";
 
-const chain = sepolia;
+const chain = polygonMumbai;
+
+// The private key of your EOA that will be the owner of Light Account
 const PRIVATE_KEY = "0xYourEOAPrivateKey" as Hex;
 const owner = LocalAccountSigner.privateKeyToAccountSigner(PRIVATE_KEY);
 
-export const provider = new AlchemyProvider({
+// Create a provider to send user operations from your smart account
+const provider = new AlchemyProvider({
   // get your Alchemy API key at https://dashboard.alchemy.com
   apiKey: "ALCHEMY_API_KEY",
   chain,
@@ -23,3 +26,9 @@ export const provider = new AlchemyProvider({
       factoryAddress: getDefaultLightAccountFactoryAddress(chain),
     })
 );
+
+(async () => {
+  // Fund your account address with ETH to send for the user operations
+  // (e.g. Get Sepolia ETH at https://sepoliafaucet.com)
+  console.log("Smart Account Address: ", await provider.getAddress()); // Log the smart account address
+})();
