0a0a007acafbf4f417bbbf46504b2588d920a7bf
avasisht23 <ajay@alchemy.com>
2023-11-29 13:23:25 -0500

    feat: update manifest call

--- packages/accounts/src/msca/plugins/multi-owner.ts
+++ packages/accounts/src/msca/plugins/multi-owner.ts
@@ -13,105 +13,105 @@ import type {
 
 const MultiOwnerPlugin_ = {
   meta: {
-    name: 'Multi Owner Plugin',
-    version: '1.0.0',
+    name: "Multi Owner Plugin",
+    version: "1.0.0",
   },
   accountDecorators: (account: ISmartContractAccount) => ({
     encodeUpdateOwnersData: ({
       args,
     }: GetFunctionArgs<
       typeof MultiOwnerPluginExecutionFunctionAbi,
-      'updateOwners'
+      "updateOwners"
     >) => {
       return encodeFunctionData({
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'updateOwners',
+        functionName: "updateOwners",
         args,
-      })
+      });
     },
 
     encodeOwnersData: () => {
       return encodeFunctionData({
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'owners',
-      })
+        functionName: "owners",
+      });
     },
 
     readOwners: async () => {
       return account.rpcProvider.readContract({
         address: await account.getAddress(),
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'owners',
-      })
+        functionName: "owners",
+      });
     },
 
     encodeIsOwnerData: ({
       args,
     }: GetFunctionArgs<
       typeof MultiOwnerPluginExecutionFunctionAbi,
-      'isOwner'
+      "isOwner"
     >) => {
       return encodeFunctionData({
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'isOwner',
+        functionName: "isOwner",
         args,
-      })
+      });
     },
 
     readIsOwner: async ({
       args,
     }: GetFunctionArgs<
       typeof MultiOwnerPluginExecutionFunctionAbi,
-      'isOwner'
+      "isOwner"
     >) => {
       return account.rpcProvider.readContract({
         address: await account.getAddress(),
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'isOwner',
+        functionName: "isOwner",
         args,
-      })
+      });
     },
 
     encodeEip712DomainData: () => {
       return encodeFunctionData({
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'eip712Domain',
-      })
+        functionName: "eip712Domain",
+      });
     },
 
     readEip712Domain: async () => {
       return account.rpcProvider.readContract({
         address: await account.getAddress(),
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'eip712Domain',
-      })
+        functionName: "eip712Domain",
+      });
     },
 
     encodeIsValidSignatureData: ({
       args,
     }: GetFunctionArgs<
       typeof MultiOwnerPluginExecutionFunctionAbi,
-      'isValidSignature'
+      "isValidSignature"
     >) => {
       return encodeFunctionData({
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'isValidSignature',
+        functionName: "isValidSignature",
         args,
-      })
+      });
     },
 
     readIsValidSignature: async ({
       args,
     }: GetFunctionArgs<
       typeof MultiOwnerPluginExecutionFunctionAbi,
-      'isValidSignature'
+      "isValidSignature"
     >) => {
       return account.rpcProvider.readContract({
         address: await account.getAddress(),
         abi: MultiOwnerPluginExecutionFunctionAbi,
-        functionName: 'isValidSignature',
+        functionName: "isValidSignature",
         args,
-      })
+      });
     },
   }),
   providerDecorators: <
@@ -146,54 +146,54 @@ export const MultiOwnerPlugin: Plugin<
 
 export const MultiOwnerPluginExecutionFunctionAbi = [
   {
-    stateMutability: 'nonpayable',
-    type: 'function',
+    stateMutability: "nonpayable",
+    type: "function",
     inputs: [
-      { name: 'ownersToAdd', internalType: 'address[]', type: 'address[]' },
-      { name: 'ownersToRemove', internalType: 'address[]', type: 'address[]' },
+      { name: "ownersToAdd", internalType: "address[]", type: "address[]" },
+      { name: "ownersToRemove", internalType: "address[]", type: "address[]" },
     ],
-    name: 'updateOwners',
+    name: "updateOwners",
     outputs: [],
   },
   {
-    stateMutability: 'view',
-    type: 'function',
+    stateMutability: "view",
+    type: "function",
     inputs: [],
-    name: 'owners',
-    outputs: [{ name: '', internalType: 'address[]', type: 'address[]' }],
+    name: "owners",
+    outputs: [{ name: "", internalType: "address[]", type: "address[]" }],
   },
   {
-    stateMutability: 'view',
-    type: 'function',
+    stateMutability: "view",
+    type: "function",
     inputs: [
-      { name: 'ownerToCheck', internalType: 'address', type: 'address' },
+      { name: "ownerToCheck", internalType: "address", type: "address" },
     ],
-    name: 'isOwner',
-    outputs: [{ name: '', internalType: 'bool', type: 'bool' }],
+    name: "isOwner",
+    outputs: [{ name: "", internalType: "bool", type: "bool" }],
   },
   {
-    stateMutability: 'view',
-    type: 'function',
+    stateMutability: "view",
+    type: "function",
     inputs: [],
-    name: 'eip712Domain',
+    name: "eip712Domain",
     outputs: [
-      { name: 'fields', internalType: 'bytes1', type: 'bytes1' },
-      { name: 'name', internalType: 'string', type: 'string' },
-      { name: 'version', internalType: 'string', type: 'string' },
-      { name: 'chainId', internalType: 'uint256', type: 'uint256' },
-      { name: 'verifyingContract', internalType: 'address', type: 'address' },
-      { name: 'salt', internalType: 'bytes32', type: 'bytes32' },
-      { name: 'extensions', internalType: 'uint256[]', type: 'uint256[]' },
+      { name: "fields", internalType: "bytes1", type: "bytes1" },
+      { name: "name", internalType: "string", type: "string" },
+      { name: "version", internalType: "string", type: "string" },
+      { name: "chainId", internalType: "uint256", type: "uint256" },
+      { name: "verifyingContract", internalType: "address", type: "address" },
+      { name: "salt", internalType: "bytes32", type: "bytes32" },
+      { name: "extensions", internalType: "uint256[]", type: "uint256[]" },
     ],
   },
   {
-    stateMutability: 'view',
-    type: 'function',
+    stateMutability: "view",
+    type: "function",
     inputs: [
-      { name: 'digest', internalType: 'bytes32', type: 'bytes32' },
-      { name: 'signature', internalType: 'bytes', type: 'bytes' },
+      { name: "digest", internalType: "bytes32", type: "bytes32" },
+      { name: "signature", internalType: "bytes", type: "bytes" },
     ],
-    name: 'isValidSignature',
-    outputs: [{ name: '', internalType: 'bytes4', type: 'bytes4' }],
+    name: "isValidSignature",
+    outputs: [{ name: "", internalType: "bytes4", type: "bytes4" }],
   },
-] as const
+] as const;
