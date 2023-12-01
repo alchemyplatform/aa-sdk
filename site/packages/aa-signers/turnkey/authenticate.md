ed6ae29a0ff3f0ff4d70baa0eeaf56b90b3bdc96
Olivia Thet <olivia@turnkey.io>
2023-12-01 09:53:15 -0500

    docs: add turnkey aa-sign docs and update turnkey snippet

---
+++ site/packages/aa-signers/turnkey/authenticate.md
@@ -0,0 +1,68 @@
+---
+outline: deep
+head:
+  - - meta
+    - property: og:title
+      content: TurnkeySigner • authenticate
+  - - meta
+    - name: description
+      content: Overview of the authenticate method on TurnkeySigner
+  - - meta
+    - property: og:description
+      content: Overview of the authenticate method on TurnkeySigner
+---
+
+# authenticate
+
+`authenticate` is a method on the `TurnkeySigner` which leverages the `Turnkey` SDK to authenticate a user.
+
+This method must be called before accessing the other methods available on the `TurnkeySigner`, such as signing messages or typed data or accessing user details.
+
+## Usage
+
+::: code-group
+
+```ts [example.ts]
+// [!code focus:99]
+import { TurnkeySigner } from "@alchemy/aa-signers";
+import { ApiKeyStamper } from "@turnkey/api-key-stamper";
+
+const turnkeySigner = new TurnkeySigner({
+  apiUrl: "api.turnkey.com",
+  stamper: new ApiKeyStamper({
+    apiPublicKey: "TURNKEY_API_PUBLIC_KEY",
+    apiPrivateKey: "TURNKEY_API_PRIVATE_KEY",
+  }),
+});
+
+const authParams = {
+  organizationId: "TURNKEY_ORGANIZATION_ID",
+  signWith: "TURNKEY_WALLET_ID",
+  transport: http("https://eth-sepolia.g.alchemy.com/v2/ALCHEMY_API_KEY");
+};
+
+await turnkeySigner.authenticate(authParams);
+```
+
+:::
+
+## Returns
+
+### `Promise<TurnkeyUserMetadata>`
+
+A Promise containing the `TurnkeyUserMetadata`, and object with the following fields:
+
+- `organizationId: string` -- unique identifier for the user's organization.
+- `organizationName: string` -- human-readable name for the user's organization.
+- `userId: string` -- unique identifier for the user.
+- `username: string` -- human-readable name for the user.
+
+## Parameters
+
+### `authParams: <TurnkeyAuthParams>`
+
+An object with the following fields:
+
+- `organizationId` -- unique identifier for the user's organization that you can generate using the [Turnkey quickstart guide](https://docs.turnkey.com/getting-started/quickstart).
+- `signWith` -- an address or unique identifer for a user's wallet. A user can create a wallet using the [`create_wallet`](https://docs.turnkey.com/api#tag/Wallets/operation/CreateWallet) endpoint.
+- `transport` -- a `viem` [Transport](https://viem.sh/docs/clients/intro.html#transports) you can define to execute RPC requests.
--- site/snippets/turnkey.ts
+++ site/snippets/turnkey.ts
@@ -1,47 +1,34 @@
-import { LocalAccountSigner, SmartAccountSigner } from "@alchemy/aa-core";
+import { TurnkeySigner } from "@alchemy/aa-signers";
 import { ApiKeyStamper } from "@turnkey/api-key-stamper";
-import { TurnkeyClient } from "@turnkey/http";
-import { createAccount } from "@turnkey/viem";
+import { http } from "viem";
 
-export async function newTurnkeySigner() {
-  const turnkeyClient = new TurnkeyClient(
-    {
-      /* 
-        Configurable, but you will likely use "https://api.turnkey.com".
-      */
-      baseUrl: process.env.TURNKEY_BASE_URL!,
-    },
-    new ApiKeyStamper({
-      /* 
-        You will generate these values as part of our quickstart guide: 
-        
-        https://docs.turnkey.com/getting-started/quickstart
-        
-        They are what the signer will use to authenticate requests to the Turnkey.com.
-      */
-      apiPublicKey: process.env.TURNKEY_API_PUBLIC_KEY!,
-      apiPrivateKey: process.env.TURNKEY_API_PRIVATE_KEY!,
-    })
-  );
+const TURNKEY_BASE_URL = "https://api.turnkey.com";
 
-  const turnkeyAccount = await createAccount({
-    client: turnkeyClient,
-    /* 
-        You can pull this from the top right widget in the Turnkey dashboard. 
-        The quickstart guide details how to do this.
-    */
-    organizationId: process.env.TURNKEY_ORGANIZATION_ID!,
-    /* 
-        This value should be the ID of the private key that you want to own
-        the light smart contract. Similarly pulled from the dashboard and
-        documented in the quickstart guide.
-    */
-    privateKeyId: process.env.TURNKEY_PRIVATE_KEY_ID!,
+// These are generated using the [Turnkey quickstart guide](https://docs.turnkey.com/getting-started/quickstart)
+// and can be found on the [Turnkey Dashboard](https://app.turnkey.com/dashboard/).
+const TURNKEY_API_PUBLIC_KEY = "test";
+const TURNKEY_API_PRIVATE_KEY = "test";
+const TURNKEY_ORGANIZATION_ID = "12345678-1234-1234-1234-123456789abc";
+
+// This is a wallet address or ID.
+const TURNKEY_SIGN_WITH = "0x1234567890123456789012345678901234567890";
+
+export const createTurnkeySigner = async () => {
+  const turnkeySigner = new TurnkeySigner({
+    apiUrl: TURNKEY_BASE_URL,
+    // API Key, WebAuthn, and Email Auth [stampers](https://docs.turnkey.com/category/api-design)
+    // must sign all requests to Turnkey.
+    stamper: new ApiKeyStamper({
+      apiPublicKey: TURNKEY_API_PUBLIC_KEY,
+      apiPrivateKey: TURNKEY_API_PRIVATE_KEY,
+    }),
   });
 
-  const turnkeySigner: SmartAccountSigner = new LocalAccountSigner(
-    turnkeyAccount
-  );
+  await turnkeySigner.authenticate({
+    organizationId: TURNKEY_ORGANIZATION_ID,
+    signWith: TURNKEY_SIGN_WITH,
+    transport: http("https://eth-sepolia.g.alchemy.com/v2/ALCHEMY_API_KEY"),
+  });
 
   return turnkeySigner;
-}
+};
