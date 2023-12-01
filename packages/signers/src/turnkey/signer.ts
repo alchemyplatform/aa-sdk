2c18a3689b6130e5c9210347c519f277133dd888
Olivia Thet <olivia@turnkey.io>
2023-12-01 09:52:29 -0500

    refactor: rename TurnkeyAuthMetadata

--- packages/signers/src/turnkey/signer.ts
+++ packages/signers/src/turnkey/signer.ts
@@ -7,7 +7,7 @@ import { TurnkeyClient } from "@turnkey/http";
 import { createAccount } from "@turnkey/viem";
 import { createWalletClient } from "viem";
 import type {
-  TurnkeyAuthMetadata,
+  TurnkeyUserMetadata,
   TurnkeyAuthParams,
   TurnkeyClientParams,
 } from "./types.js";
@@ -23,7 +23,7 @@ export class TurnkeySigner
   implements
     SmartAccountAuthenticator<
       TurnkeyAuthParams,
-      TurnkeyAuthMetadata,
+      TurnkeyUserMetadata,
       TurnkeyClient
     >
 {
