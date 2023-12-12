91516638784ec59185d5976f8feefd8edf1e2ad1
moldy <michael.moldoveanu@alchemy.com>
2023-12-12 18:53:33 -0500

    fix: update the logic for signing 1271 messages

--- packages/accounts/src/index.ts
+++ packages/accounts/src/index.ts
@@ -28,8 +28,6 @@ export type { LightAccountProviderConfig } from "./light-account/types.js";
 export { getDefaultLightAccountFactoryAddress } from "./light-account/utils.js";
 
 // msca exports
-export { BaseModularSmartContractAccount } from "./msca/base.js";
-export type { ModularSmartContractAccountParams } from "./msca/base.js";
 export {
   MSCABuilder,
   StandardExecutor,
