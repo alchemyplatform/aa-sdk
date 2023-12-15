e373b01d42f7ef769828dc3d90e067e048736b66
Ajay Vasisht <43521356+avasisht23@users.noreply.github.com>
2023-12-15 18:49:24 -0500

    docs: update magic docs (#334)

--- site/snippets/magic.ts
+++ site/snippets/magic.ts
@@ -1,7 +1,7 @@
 import { MagicSigner } from "@alchemy/aa-signers/magic";
 
 // this is generated from the [Magic Dashboard](https://dashboard.magic.link/)
-const MAGIC_API_KEY = "pk_test_...";
+const MAGIC_API_KEY = "pk_live_...";
 
 export const createMagicSigner = async () => {
   const magicSigner = new MagicSigner({ apiKey: MAGIC_API_KEY });
