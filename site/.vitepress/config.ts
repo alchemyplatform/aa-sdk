49968e8fa5ad1b8cb8e4637aa0aac7ed36bb50ae
Ava Robinson <avamrobinson@gmail.com>
2023-12-18 14:20:59 -0500

    docs: adding faq page

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -56,6 +56,10 @@ export default defineConfig({
             link: "/demos",
           },
           {
+            text: "FAQs",
+            link: "/faqs",
+          },
+          {
             text: "Contact Us",
             link: "/contact-us",
           },
