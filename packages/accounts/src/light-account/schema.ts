9561742c99fe98782be091bd974435aca2d1ce3f
moldy <michael.moldoveanu@alchemy.com>
2024-01-12 17:02:02 -0500

    test: add tests for LA signatures

--- packages/accounts/src/light-account/schema.ts
+++ packages/accounts/src/light-account/schema.ts
@@ -5,6 +5,15 @@ import {
 import { Address } from "abitype/zod";
 import { isHex } from "viem";
 import { z } from "zod";
+import { LightAccountVersions, type LightAccountVersion } from "./utils.js";
+
+const isLightAccountVersion = (x: unknown): x is LightAccountVersion => {
+  if (typeof x === "string" && x in LightAccountVersions) {
+    return true;
+  }
+
+  return false;
+};
 
 export const LightAccountFactoryConfigSchema = z.object({
   owner: SignerSchema,
@@ -19,6 +28,13 @@ export const LightAccountFactoryConfigSchema = z.object({
   factoryAddress: Address.optional().describe(
     "Optional override for the factory address which deploys the smart account."
   ),
+  version: z
+    .string()
+    .refine<LightAccountVersion>(
+      isLightAccountVersion,
+      "Version must be a valid Light Account version."
+    )
+    .optional(),
 });
 
 export const LightAccountProviderConfigSchema =
