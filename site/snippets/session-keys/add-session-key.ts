fa9d2394146c7647eb6d54148a2e374d660b9dab
Dan Nolan <DanNolan99@gmail.com>
2024-02-14 09:33:34 -0500

    Update site/snippets/session-keys/add-session-key.ts
    Co-authored-by: Dennis Won <denniswon@users.noreply.github.com>

--- site/snippets/session-keys/add-session-key.ts
+++ site/snippets/session-keys/add-session-key.ts
@@ -5,6 +5,6 @@ import { keccak256 } from "viem";
 const result = await client.addSessionKey({
   key: "0xSessionKeyAddress",
   // tag is an identifier for the emitted SessionKeyAdded event
-  tag: keccak256(new TextEncoder().encode("your-app-tag")),
+  tag: keccak256(new TextEncoder().encode("session-key-tag")),
   permissions: new SessionKeyPermissionsBuilder().encode(),
 });
