53ba81d33422bb3f18134c1dd75e68d64f5cc3f0
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-03-14 11:42:14 -0400

    feat(plugins): make plugingen a cli tool (#507)

---
+++ packages/plugingen/src/commands/generate/phases/contract-abi-gen.ts
@@ -0,0 +1,12 @@
+import dedent from "dedent";
+import type { Phase } from "../types";
+
+export const ContractAbiGenPhase: Phase = async (input) => {
+  const { content, pluginConfig } = input;
+  content.push(dedent`
+    export const ${pluginConfig.name}Abi = ${JSON.stringify(
+    pluginConfig.abi
+  )} as const;
+  `);
+  return input;
+};
---
+++ packages/plugingen/src/exports/index.ts
@@ -0,0 +1,7 @@
+export { defineConfig, type Config, type PluginConfig } from "../config.js";
+
+export * as logger from "../logger.js";
+
+export { loadEnv } from "../utils/loadEnv.js";
+
+export { VERSION } from "../version.js";
