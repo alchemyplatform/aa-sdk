baab7bd7500157af744fe1c581ac12cbe2e0d8b2
avarobinson <45052879+avarobinson@users.noreply.github.com>
2024-03-27 12:07:05 -0400

    fix: dummy paymaster and data addresses updates (#532)

--- packages/alchemy/src/gas-manager.ts
+++ packages/alchemy/src/gas-manager.ts
@@ -11,35 +11,43 @@ import {
   optimismSepolia,
   polygon,
   polygonAmoy,
+  polygonMumbai,
+  sepolia,
   zora,
   zoraSepolia,
 } from "viem/chains";
 
-export const NewAlchemyPaymasterAddress =
+export const AlchemyPaymasterAddressV3 =
+  "0x4f84a207A80c39E9e8BaE717c1F25bA7AD1fB08F";
+export const AlchemyPaymasterAddressV2 =
   "0x4Fd9098af9ddcB41DA48A1d78F91F1398965addc";
-export const OldAlchemyPaymasterAddress =
-  "0xc03aac639bb21233e0139381970328db8bceeb67";
 export const ArbSepoliaPaymasterAddress =
   "0x0804Afe6EEFb73ce7F93CD0d5e7079a5a8068592";
+export const AlchemyPaymasterAddressV1 =
+  "0xc03aac639bb21233e0139381970328db8bceeb67";
 
 export const getAlchemyPaymasterAddress = (chain: Chain): Address => {
-  switch (chain) {
-    case mainnet:
-    case optimism:
-    case polygon:
-    case arbitrum:
-    case polygonAmoy:
-    case optimismSepolia:
-    case base:
-    case baseSepolia:
-    case zora:
-    case zoraSepolia:
-    case fraxtal:
-    case fraxtalTestnet:
-      return NewAlchemyPaymasterAddress;
-    case arbitrumSepolia:
-      return OldAlchemyPaymasterAddress;
-    default:
+  switch (chain.id) {
+    case polygonAmoy.id:
+    case optimismSepolia.id:
+    case baseSepolia.id:
+    case zora.id:
+    case zoraSepolia.id:
+    case fraxtal.id:
+    case fraxtalTestnet.id:
+      return AlchemyPaymasterAddressV3;
+    case mainnet.id:
+    case arbitrum.id:
+    case optimism.id:
+    case polygon.id:
+    case base.id:
+      return AlchemyPaymasterAddressV2;
+    case arbitrumSepolia.id:
       return ArbSepoliaPaymasterAddress;
+    case sepolia.id:
+    case polygonMumbai.id:
+      return AlchemyPaymasterAddressV1;
+    default:
+      throw new Error(`Unsupported chain: ${chain}`);
   }
 };
