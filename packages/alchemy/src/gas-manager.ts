3a641de4b696bbf4161c271b793fe3e37a975672
avasisht23 <ajay@alchemy.com>
2024-03-27 12:00:24 -0400

    fix: update from nits

--- packages/alchemy/src/gas-manager.ts
+++ packages/alchemy/src/gas-manager.ts
@@ -6,7 +6,6 @@ import {
   baseSepolia,
   fraxtal,
   fraxtalTestnet,
-  goerli,
   mainnet,
   optimism,
   optimismSepolia,
@@ -18,38 +17,37 @@ import {
   zoraSepolia,
 } from "viem/chains";
 
+export const AlchemyPaymasterAddressV3 =
+  "0x4f84a207A80c39E9e8BaE717c1F25bA7AD1fB08F";
 export const AlchemyPaymasterAddressV2 =
   "0x4Fd9098af9ddcB41DA48A1d78F91F1398965addc";
-export const OldAlchemyPaymasterAddress =
-  "0xc03aac639bb21233e0139381970328db8bceeb67";
 export const ArbSepoliaPaymasterAddress =
   "0x0804Afe6EEFb73ce7F93CD0d5e7079a5a8068592";
-export const AlcehmyPaymasterAddressV3 =
-  "0x4f84a207A80c39E9e8BaE717c1F25bA7AD1fB08F";
+export const AlchemyPaymasterAddressV1 =
+  "0xc03aac639bb21233e0139381970328db8bceeb67";
 
 export const getAlchemyPaymasterAddress = (chain: Chain): Address => {
-  switch (chain) {
-    case mainnet:
-    case arbitrum:
-    case optimism:
-    case polygon:
-    case base:
+  switch (chain.id) {
+    case polygonAmoy.id:
+    case optimismSepolia.id:
+    case baseSepolia.id:
+    case zora.id:
+    case zoraSepolia.id:
+    case fraxtal.id:
+    case fraxtalTestnet.id:
+      return AlchemyPaymasterAddressV3;
+    case mainnet.id:
+    case arbitrum.id:
+    case optimism.id:
+    case polygon.id:
+    case base.id:
       return AlchemyPaymasterAddressV2;
-    case polygonAmoy:
-    case optimismSepolia:
-    case baseSepolia:
-    case zora:
-    case zoraSepolia:
-    case fraxtal:
-    case fraxtalTestnet:
-      return AlcehmyPaymasterAddressV3;
-    case arbitrumSepolia:
+    case arbitrumSepolia.id:
       return ArbSepoliaPaymasterAddress;
-    case sepolia:
-    case goerli:
-    case polygonMumbai:
-      return OldAlchemyPaymasterAddress;
+    case sepolia.id:
+    case polygonMumbai.id:
+      return AlchemyPaymasterAddressV1;
     default:
-      return OldAlchemyPaymasterAddress;
+      throw new Error(`Unsupported chain: ${chain}`);
   }
 };
