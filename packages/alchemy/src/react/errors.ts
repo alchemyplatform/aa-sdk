de5262c585260925adfa08e21d008fa8011284a9
Ajay Vasisht <43521356+avasisht23@users.noreply.github.com>
2024-04-18 15:38:58 -0400

    feat: add useSignMessage and useSignTypedData hooks (#568)

--- packages/alchemy/src/react/errors.ts
+++ packages/alchemy/src/react/errors.ts
@@ -2,6 +2,12 @@ import { BaseError } from "../errors/base.js";
 
 export class NoAlchemyAccountContextError extends BaseError {
   constructor(hookName: string) {
-    super(`${hookName} must be used within a AlchemyAccountProvider`);
+    super(`${hookName} must be used within a AlchemyAxccountProvider`);
+  }
+}
+
+export class ClientUndefinedHookError extends BaseError {
+  constructor(hookName: string) {
+    super(`client must be defined in ${hookName}`);
   }
 }
