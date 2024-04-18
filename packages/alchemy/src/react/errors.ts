4090d5d8980ac41b20b820827990a4f37bbc1d0e
avasisht23 <ajay@alchemy.com>
2024-04-18 15:39:27 -0400

    docs: update error

--- packages/alchemy/src/react/errors.ts
+++ packages/alchemy/src/react/errors.ts
@@ -2,7 +2,7 @@ import { BaseError } from "../errors/base.js";
 
 export class NoAlchemyAccountContextError extends BaseError {
   constructor(hookName: string) {
-    super(`${hookName} must be used within a AlchemyAxccountProvider`);
+    super(`${hookName} must be used within a AlchemyAccountProvider`);
   }
 }
 
