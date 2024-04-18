4aafb28ba544cfecc498061dc2bdb269246ee4df
avasisht23 <ajay@alchemy.com>
2024-04-18 03:01:23 -0400

    docs: update docs

--- site/snippets/react/useLogout.tsx
+++ site/snippets/react/useLogout.tsx
@@ -5,7 +5,14 @@ export function ComponentWithLogout() {
    * Assumes the app has context of a signer with an authenticated user
    * by using the `AlchemyAccountProvider` from `@alchemy/aa-alchemy/react`.
    */
-  const { logout } = useLogout();
+  const { logout } = useLogout({
+    onSuccess: () => {
+      // [optional] Do something after the user has been logged out
+    },
+    onError: (error) => {
+      // [optional] Do something with the error
+    },
+  });
 
   return (
     <div>
