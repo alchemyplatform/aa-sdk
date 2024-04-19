7eabe7843acfa3e10d20bb362082a6d159df9747
Ajay Vasisht <43521356+avasisht23@users.noreply.github.com>
2024-04-19 15:16:19 -0400

    feat: add js docs to client undefined error (#600)

--- packages/alchemy/src/react/errors.ts
+++ packages/alchemy/src/react/errors.ts
@@ -6,6 +6,10 @@ export class NoAlchemyAccountContextError extends BaseError {
   }
 }
 
+/**
+ * Error thrown when a hook is called without a client.
+ * @param hookName The name of the hook.
+ */
 export class ClientUndefinedHookError extends BaseError {
   constructor(hookName: string) {
     super(`client must be defined in ${hookName}`);
