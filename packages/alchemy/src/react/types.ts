c781e811e5d0347953ca0a2d7511fc46747e473b
Ajay Vasisht <43521356+avasisht23@users.noreply.github.com>
2024-04-19 15:02:41 -0400

    feat: add js docs to types (#599)

--- packages/alchemy/src/react/types.ts
+++ packages/alchemy/src/react/types.ts
@@ -1,5 +1,10 @@
 import type { UseMutationOptions } from "@tanstack/react-query";
 
+/**
+ * Base hook mutation arguments.
+ * @template TData The mutation data type.
+ * @template TVariable The mutation variable type.
+ */
 export type BaseHookMutationArgs<
   TData extends any = void,
   TVariable extends any = void
