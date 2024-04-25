9baf4f80301fd7b5cb3c29e98ca6fe58d3405b37
avasisht23 <ajay@alchemy.com>
2024-04-24 23:27:37 -0400

    docs: remove turnkey iframe step from getting started in favor of createConfig

--- site/snippets/getting-started/log-in-users/TurnkeyIframe.tsx
+++
@@ -1,9 +0,0 @@
-export const TurnkeyIframe = () => {
-  return (
-    <div
-      className="w-full"
-      style={{ display: "none" }}
-      id="turnkey-iframe-container-id"
-    ></div>
-  );
-};
