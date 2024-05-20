38dfa2578e1dccdedca5295b2c2871bafa1aef7c
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-05-20 12:51:43 -0400

    feat: allow setting gas manager config in the config (#670)

--- site/snippets/react/multi-chain-config.ts
+++ site/snippets/react/multi-chain-config.ts
@@ -1,6 +1,5 @@
 import { createConfig } from "@alchemy/aa-alchemy/config";
-import { sepolia } from "@alchemy/aa-core";
-import { mainnet } from "viem/chains";
+import { base, mainnet, sepolia } from "@alchemy/aa-core";
 
 // the examples use different routes for your chains and signer connection, but you can handle
 // this in one route if you prefer
@@ -10,8 +9,16 @@ export const config = createConfig({
   // this adds configs for all other chains you want to support.
   // NOTE: you must include a config for the default chain as well
   connections: [
+    // connection using rpcUrl
     { chain: sepolia, rpcUrl: "/api/rpc/sepolia" },
-    { chain: mainnet, rpcUrl: "/api/rpc/mainnet" },
+    // connection using an api key
+    { chain: mainnet, apiKey: "ALCHEMY_API_KEY" },
+    // connection using a gas manager
+    {
+      chain: base,
+      apiKey: "ALCHEMY_API_KEY",
+      gasManagerConfig: { policyId: "POLICY_ID" },
+    },
   ],
   // when using multiple chains, you must specify the signer connection config since it might be different than your chain RPC urls
   signerConnection: {
