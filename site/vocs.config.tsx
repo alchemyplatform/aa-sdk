a80d2f29c41bd09b6171b9a2b7304ca6bdc8557b
moldy <michael.moldoveanu@alchemy.com>
2024-05-29 18:53:48 -0400

    docs: update core head options

--- site/vocs.config.tsx
+++ site/vocs.config.tsx
@@ -14,10 +14,33 @@ export default defineConfig({
   rootDir: "./",
   head: (
     <>
+      <script
+        src="https://static.alchemyapi.io/scripts/anayltics/alchemy-analytics.js"
+        defer
+      />
+      <link rel="icon" href="/favicon.ico" />
       <meta
         name="google-site-verification"
         content="W4pmFb0Oe26_OndBLdy5uqNrsl_gfmKfTlHwlHPd4Co"
       />
+      {/* Open Graph Tags */}
+      <meta property="og:title" content="Account Kit"></meta>
+      <meta
+        property="og:description"
+        content="Account Kit is a framework to embed smart accounts in your web3 app, unlocking powerful features like email/social login, gas sponsorship, batched transactions, and more."
+      ></meta>
+      <meta property="og:image" content="/images/og-image.jpg"></meta>
+      <meta property="og:image:type" content="image/jpeg"></meta>
+      <meta property="og:image:width" content="2400"></meta>
+      <meta property="og:image:height" content="1260"></meta>
+      {/* Twitter Card Tags */}
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:title" content="Account Kit"></meta>
+      <meta
+        name="twitter:description"
+        content="Account Kit is a framework to embed smart accounts in your web3 app, unlocking powerful features like email/social login, gas sponsorship, batched transactions, and more."
+      ></meta>
+      <meta name="twitter:image" content="/images/og-image.jpg"></meta>
     </>
   ),
   topNav: [
@@ -365,7 +388,6 @@ export default defineConfig({
   socials: [
     { icon: "github", link: "https://github.com/alchemyplatform/aa-sdk" },
   ],
-  // TODO: add head from previous config
   // This is not supported in vocs
   // sitemap: {
   //   hostname: "https://accountkit.alchemy.com",
