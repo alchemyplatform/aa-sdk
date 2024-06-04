eada9d848f054bdcb475ff79bec6bcec0544b9a7
Tina Zheng <tina.s.zheng+github@gmail.com>
2024-06-04 19:01:41 -0400

    fix: remove unused variables

--- examples/ui-demo/src/app/providers.tsx
+++ examples/ui-demo/src/app/providers.tsx
@@ -3,7 +3,7 @@ import { createConfig } from "@alchemy/aa-alchemy/config";
 import { AlchemyAccountProvider, AlchemyAccountsProviderProps } from "@alchemy/aa-alchemy/react";
 import { sepolia } from "@alchemy/aa-core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren, Suspense, useRef } from "react";
+import { PropsWithChildren, Suspense } from "react";
 
 const config = createConfig({
   // required
@@ -21,7 +21,6 @@ const uiConfig: AlchemyAccountsProviderProps["uiConfig"] = {
     sections: [[{type: "email"}], [{type: "passkey"}]],
     addPasskeyOnSignup: true,
   },
-  errorContainerId: "example-custom-error-boundary"
 };
 
 export const Providers = (props: PropsWithChildren<{}>) => {
