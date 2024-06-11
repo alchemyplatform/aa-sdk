13e87da4dacfbf97bf424111341a907b339601ae
Brian Wagner <brian.wagner35@gmail.com>
2024-06-11 19:37:55 -0400

    feat: integrate shadcn and start on auth methods in demo app

--- examples/ui-demo/src/components/shared/button.tsx
+++ examples/ui-demo/src/components/shared/button.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function Button({ children }: PropsWithChildren<object>) {
   return (
-    <button className="border bg-white border-fg-secondary rounded-lg px-4 py-3 text-sm font-semibold">
+    <button className="border bg-white border-fg-secondary rounded-lg px-3 text-sm font-semibold h-10 flex items-center">
       {children}
     </button>
   );
