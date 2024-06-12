a3fa8344f97dbc4595332a9fdb9121c00c540bee
moldy <michael.moldoveanu@alchemy.com>
2024-06-12 18:10:55 -0400

    chore: add eslint rule to check just the re-exported functions have jsdocs

---
+++ templates/eslint/src/index.ts
@@ -0,0 +1,5 @@
+import requireJsdocOnReexportedFunctions from "./rules/jsdoc-reexported-functions.js";
+
+export const rules = {
+  "require-jsdoc-on-reexported-functions": requireJsdocOnReexportedFunctions,
+};
