ec868e802972cc088d41028077b80d7fb9941326
Brian Wagner <brian.wagner35@gmail.com>
2024-06-12 15:30:52 -0400

    feat: add tabs and passkey section to demo app

--- examples/ui-demo/src/components/configuration/index.tsx
+++ examples/ui-demo/src/components/configuration/index.tsx
@@ -1,8 +1,29 @@
-import { useState } from "react"
-import { Authentication } from "./Authentication"
+import { useState } from "react";
+import { Authentication } from "./Authentication";
+import { Tab, Tabs } from "../shared/Tabs";
+import { LockIcon } from "../icons/lock";
+import { PaintIcon } from "../icons/paint";
+
+const TABS: Tab[] = [
+  {
+    id: "authentication",
+    name: "Authentication",
+    icon: <LockIcon />,
+  },
+  {
+    id: "style",
+    name: "Styling",
+    icon: <PaintIcon />,
+  },
+];
 
 export const Configuration = () => {
-  const [tab, setTab] = useState<'authentication' | 'style'>('authentication')
+  const [tab, setTab] = useState<string>("authentication");
 
-  return tab === "authentication" ? <Authentication /> : <div />
-}
+  return (
+    <div className="flex flex-col">
+      <Tabs tabs={TABS} activeTab={tab} setActive={setTab} />
+      {tab === "authentication" ? <Authentication className="pt-6" /> : <div />}
+    </div>
+  );
+};
