eca13d8b33c8625edb810c4c13018fa8b0cce43b
Tina <59578595+tinaszheng@users.noreply.github.com>
2024-06-17 15:45:38 -0400

    feat: add custom theme switch component (#726)
    * feat: add custom theme switch
    
    * fix: undo lockfile changes
    
    * feat: update theme in global config

---
+++ examples/ui-demo/src/components/shared/ThemeSwitch.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import * as React from "react"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+
+import { cn } from "@/lib/utils"
+import { Moon, Sun } from "lucide-react"
+
+const ThemeSwitch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ className, ...props }, ref) => (
+  <SwitchPrimitives.Root
+    className={cn(
+      "relative peer inline-flex h-10 w-48 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gray-500 data-[state=unchecked]:bg-input",
+      className
+    )}
+    {...props}
+    ref={ref}
+  >
+    <SwitchPrimitives.Thumb
+      className={cn(
+        "pointer-events-none block h-9 w-24 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[92px] data-[state=unchecked]:translate-x-0"
+      )}
+    ></SwitchPrimitives.Thumb>
+    <div className="absolute flex text-sm items-center right-[2px] left-[2px] top-[2px] bottom-[2px] z-10 justify-between bg-transparent">
+      <div className="flex items-center data-[state=unchecked]:font-medium text-gray-950 justify-center gap-1 flex-1 basis-0">
+        <Sun size={18} /><div className={cn(!props.checked && "font-medium")}>Light</div>
+      </div>
+      <div className="flex items-center justify-center gap-1 flex-1 basis-0">
+        <Moon size={18} /><div className={cn(props.checked && "font-medium")}>Dark</div>
+      </div>
+    </div>
+  </SwitchPrimitives.Root>
+))
+ThemeSwitch.displayName = SwitchPrimitives.Root.displayName
+
+export { ThemeSwitch }
