63219d4daf78291a3896a80add040befd4a487c4
Tina <59578595+tinaszheng@users.noreply.github.com>
2024-06-18 13:36:08 -0400

    feat: add toggle for code preview mode (#731)
    * feat: add dev mode toggle
    
    * fix: update switch color

---
+++ examples/ui-demo/src/components/shared/CodePreviewSwitch.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import * as React from "react"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+
+import { cn } from "@/lib/utils"
+import { Code } from "../icons/code"
+
+const CodePreviewSwitch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ className, ...props }, ref) => (
+  <SwitchPrimitives.Root
+    className={cn(
+      "peer inline-flex h-[28px] w-[50px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-btn-primary data-[state=unchecked]:bg-input",
+      className
+    )}
+    {...props}
+    ref={ref}
+  >
+    <SwitchPrimitives.Thumb
+      className={cn(
+        "flex items-center justify-center pointer-events-none h-[24px] w-[24px] rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[22px] data-[state=unchecked]:translate-x-0"
+      )}
+    >
+      <Code />
+    </SwitchPrimitives.Thumb>
+  </SwitchPrimitives.Root>
+))
+CodePreviewSwitch.displayName = SwitchPrimitives.Root.displayName
+
+export { CodePreviewSwitch }
