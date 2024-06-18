0d0cc241833eae7ebef13a9dde914268a67a22c2
Brian Wagner <brian.wagner35@gmail.com>
2024-06-18 17:11:21 -0400

    feat: add color picker (#732)

--- examples/ui-demo/src/components/configuration/ColorPicker.tsx
+++ examples/ui-demo/src/components/configuration/ColorPicker.tsx
@@ -1,16 +1,34 @@
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { useConfig } from "@/src/app/state";
+import { Chrome, Sketch } from "@uiw/react-color";
 
 export function ColorPicker() {
-  const {
-    config: {
-      ui: { primaryColor },
-    },
-  } = useConfig();
-  // TODO: add color picker library
+  const { config, setConfig } = useConfig();
+
+  const onSetThemeColor = (color: string) => {
+    setConfig(prev => ({
+      ...prev,
+      ui: {
+        ...prev.ui,
+        primaryColor: color
+      }
+    }))
+  }
+
   return (
-    <button className="rounded-lg py-2 px-[10px] gap-2 flex items-center border border-gray-300 hover:opacity-80">
-      <div className="h-6 w-6 rounded" style={{ backgroundColor: primaryColor }} />
-      <div className="text-sm">{primaryColor}</div>
-    </button>
+    <Popover>
+      <PopoverTrigger className="rounded-lg py-2 px-[10px] min-w-[120px] gap-2 flex items-center justify-center border border-gray-300 hover:opacity-80">
+        <div className="h-6 w-6 rounded" style={{ backgroundColor: config.ui.primaryColor }} />
+        <div className="text-sm">{config.ui.primaryColor}</div>
+      </PopoverTrigger>
+      <PopoverContent side="right" sideOffset={8} className="p-0 w-fit border-0">
+        <Sketch
+          color={config.ui.primaryColor}
+          onChange={(color) => {
+            onSetThemeColor(color.hex);
+          }}
+        />
+      </PopoverContent>
+    </Popover>
   );
 }
