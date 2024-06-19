3015796a4c3199a325a22aec38845a6fe1c4f236
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-06-19 16:01:49 -0400

    chore: start fixing smart contracts package (#737)

--- account-kit/smart-contracts/src/light-account/decorators/lightAccount.ts
+++ account-kit/smart-contracts/src/light-account/decorators/lightAccount.ts
@@ -17,6 +17,25 @@ export type LightAccountClientActions<
   ) => Promise<Hex>;
 };
 
+/**
+ * Provides a set of actions for managing a light account client, including transferring ownership.
+ *
+ * @example
+ * ```ts
+ * import { lightAccountClientActions, createLightAccount } from "@account-kit/smart-contracts";
+ * import { createAlchemySmartAccountClient } from "@account-kit/infra";
+ * import { sepolia } from "@account-kit/infra/chains";
+ * 
+ * const smartAccountClient = createAlchemySmartAccountClient({
+ *  account: await createLightAccount(...),
+ *  apiKey: "your-api-key",
+ *  chain: sepolia,
+ * }).extend(lightAccountClientActions);
+ * ```
+ *
+ * @param {Client<TTransport, TChain, TAccount>} client The client instance for which to provide the light account actions
+ * @returns {LightAccountClientActions<TSigner, TAccount>} An object containing the available light account client actions
+ */
 export const lightAccountClientActions: <
   TTransport extends Transport = Transport,
   TChain extends Chain | undefined = Chain | undefined,
