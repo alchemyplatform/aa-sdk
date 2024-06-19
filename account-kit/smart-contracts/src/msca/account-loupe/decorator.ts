270287bf2f6c87d2c488bff5b2747c0530c29532
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-06-19 16:02:23 -0400

    chore: add more fixes smart contracts package jsdocs (#738)

--- account-kit/smart-contracts/src/msca/account-loupe/decorator.ts
+++ account-kit/smart-contracts/src/msca/account-loupe/decorator.ts
@@ -51,6 +51,21 @@ export type AccountLoupeActions<
   ): Promise<ReadonlyArray<Address>>;
 };
 
+/**
+ * Provides a set of actions for account loupe operations using the specified client.
+ * NOTE: this is already added to the client when using any of the Modular Account Clients.
+ * 
+ * @example
+ * ```ts
+ * import { accountLoupeActions } from "@account-kit/smart-contracts";
+ * import { createSmartAccountClient } from "@aa-sdk/core";
+ * 
+ * const client = createSmartAccountClient(...).extend(accountLoupeActions);
+ * ```
+ * 
+ * @param {Client<TTransport, TChain, TAccount>} client the client to be used for executing the account loupe actions
+ * @returns {AccountLoupeActions<TAccount>} an object containing account loupe actions like `getExecutionFunctionConfig`, `getExecutionHooks`, `getPreValidationHooks`, and `getInstalledPlugins`
+ */
 export const accountLoupeActions: <
   TTransport extends Transport = Transport,
   TChain extends Chain | undefined = Chain | undefined,
