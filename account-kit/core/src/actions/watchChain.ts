06b17cc13b55fe51f5783c4bab7323cdb41b22ca
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-06-19 15:57:37 -0400

    chore: fix a batch of 10 jsdocs in account-kit core (#734)
    * chore: fix a batch of 10 jsdocs in account-kit core
    
    * docs: update jsdocs per pr comments

--- account-kit/core/src/actions/watchChain.ts
+++ account-kit/core/src/actions/watchChain.ts
@@ -3,9 +3,18 @@ import type { AlchemyAccountsConfig } from "../types";
 
 /**
  * Allows you to subscribe to changes of the chain in the client store.
+ * 
+ * @example
+ * ```ts
+ * import { watchChain } from "@account-kit/core";
+ * // see createConfig for more information on how to create a config
+ * import { config } from "./config";
+ * 
+ * watchChain(config)(console.log);
+ * ```
  *
  * @param config the account config object
- * @returns a function which accepts an onChange callback that will be fired when the chain changes
+ * @returns {(onChange: (chain: Chain) => void) => (() => void)} a function which accepts an onChange callback that will be fired when the chain changes and returns a function to unsubscribe from the store
  */
 export function watchChain(config: AlchemyAccountsConfig) {
   return (onChange: (chain: Chain) => void) => {
