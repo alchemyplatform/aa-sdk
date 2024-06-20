f4d7178caac5e5bc89bd769a22d01f13410d3ab0
moldy <michael.moldoveanu@alchemy.com>
2024-06-20 16:59:05 -0400

    docs: add skeleton for new docs

---
+++ site/sidebar/core.ts
@@ -0,0 +1,11 @@
+import { SidebarItem } from "vocs";
+
+export const coreSidebar: SidebarItem[] = [
+  {
+    text: "Overview",
+    items: [
+      { text: "Introduction", link: "/core/overview" },
+      { text: "Getting started", link: "#TODO" },
+    ],
+  },
+];
