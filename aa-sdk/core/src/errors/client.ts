2fbc7b0f61a6c7fe81cde9ba77d5963e4539905e
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-06-20 11:11:18 -0400

    chore: fix the last few jsdocs in aasdk core (#742)
    * chore: fix the last few jsdcods in aasdk core
    
    * style: run the linter on some left over files
    
    * style: apply suggestions from PR review
    
    Co-authored-by: Dan Nolan <dan.nolan@alchemy.com>
    
    * style: run linter after merge
    
    ---------
    
    Co-authored-by: Dan Nolan <dan.nolan@alchemy.com>

--- aa-sdk/core/src/errors/client.ts
+++ aa-sdk/core/src/errors/client.ts
@@ -1,6 +1,9 @@
 import type { Client } from "viem";
 import { BaseError } from "./base.js";
 
+/**
+ * Represents an error thrown when a client is not compatible with the expected client type for a specific method. The error message provides guidance on how to create a compatible client.
+ */
 export class IncompatibleClientError extends BaseError {
   override name = "IncompatibleClientError";
   constructor(expectedClient: string, method: string, client: Client) {
@@ -13,6 +16,9 @@ export class IncompatibleClientError extends BaseError {
   }
 }
 
+/**
+ * Represents an error that occurs when an invalid RPC URL is provided. This class extends the `BaseError` class and includes the invalid URL in the error message.
+ */
 export class InvalidRpcUrlError extends BaseError {
   override name = "InvalidRpcUrlError";
   constructor(rpcUrl?: string) {
@@ -20,6 +26,9 @@ export class InvalidRpcUrlError extends BaseError {
   }
 }
 
+/**
+ * Error class representing a "Chain Not Found" error, typically thrown when no chain is supplied to the client.
+ */
 export class ChainNotFoundError extends BaseError {
   override name = "ChainNotFoundError";
   constructor() {
--- aa-sdk/core/src/middleware/defaults/feeEstimator.ts
+++ aa-sdk/core/src/middleware/defaults/feeEstimator.ts
@@ -3,11 +3,30 @@ import { applyUserOpOverrideOrFeeOption } from "../../utils/index.js";
 import type { MiddlewareClient } from "../actions";
 import type { ClientMiddlewareFn } from "../types";
 
-export const defaultFeeEstimator: <C extends MiddlewareClient>(
+/**
+ * Default fee estimator middleware function that estimates the maximum fee per gas and maximum priority fee per gas for a given client and applies the necessary overrides and fee options.
+ *
+ * @example
+ * ```ts
+ * import { createSmartAccountClient, defaultFeeEstimator, createBundlerClient } from "@aa-sdk/core";
+ *
+ * const bundlerClient = createBundlerClient(...);
+ *
+ * // NOTE: this is already provided by the smart account client
+ * const client = createSmartAccountClient({
+ *  feeEstimator: defaultFeeEstimator(bundlerClient),
+ *  ...otherParams
+ * });
+ * ```
+ *
+ * @template {MiddlewareClient} C The type of the client
+ * @param {C} client The client to perform the fee estimation
+ * @returns {ClientMiddlewareFn} A middleware function that takes in the struct and options, estimates the fees, and updates the struct with the estimated fees
+ */
+export function defaultFeeEstimator<C extends MiddlewareClient>(
   client: C
-) => ClientMiddlewareFn =
-  (client) =>
-  async (struct, { overrides, feeOptions }) => {
+): ClientMiddlewareFn {
+  return async (struct, { overrides, feeOptions }) => {
     // maxFeePerGas must be at least the sum of maxPriorityFeePerGas and baseFee
     // so we need to accommodate for the fee option applied maxPriorityFeePerGas for the maxFeePerGas
     //
@@ -48,3 +67,4 @@ export const defaultFeeEstimator: <C extends MiddlewareClient>(
     struct.maxPriorityFeePerGas = maxPriorityFeePerGas;
     return struct;
   };
+}
