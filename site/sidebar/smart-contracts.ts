41ad0131d689f197d21c6881f78b6a86630e830f
moldy <michael.moldoveanu@alchemy.com>
2024-06-20 16:59:05 -0400

    docs: add skeleton for new docs

---
+++ site/sidebar/smart-contracts.ts
@@ -0,0 +1,11 @@
+import { SidebarItem } from "vocs";
+
+export const smartContractsSidebar: SidebarItem[] = [
+  {
+    text: "Overview",
+    items: [
+      { text: "Introduction", link: "/infra/overview" },
+      { text: "Getting started", link: "#TODO" },
+    ],
+  },
+];
