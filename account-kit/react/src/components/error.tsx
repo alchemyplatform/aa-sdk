9abbb88854a6ccf28eecdbf21aa828ad4d6d45b7
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-06-25 18:08:50 -0400

    feat: add border radius to account kit theme config (#781)

--- account-kit/react/src/components/error.tsx
+++ account-kit/react/src/components/error.tsx
@@ -5,8 +5,8 @@ interface ErrorContainerProps {
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const ErrorContainer = ({ error }: ErrorContainerProps) => {
   return (
-    <div className="flex flex-row gap-2 p-1 items-center rounded-lg bg-bg-surface-critical text-fg-critical text-xs">
-      <div className="rounded-lg bg-fg-critical text-fg-invert font-semibold font-xs py-1 px-2">
+    <div className="flex flex-row gap-2 p-1 items-center radius bg-bg-surface-critical text-fg-critical text-xs">
+      <div className="radius bg-fg-critical text-fg-invert font-semibold font-xs py-1 px-2">
         Error
       </div>
       <div className="font-normal">
