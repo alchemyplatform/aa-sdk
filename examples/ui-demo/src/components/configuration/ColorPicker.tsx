952bd3d34dbca02a203ab72ef054b8756244b13a
Tina <59578595+tinaszheng@users.noreply.github.com>
2024-06-25 15:29:00 -0400

    feat: color picker for dark mode (#777)
    * feat: add separate dark and light mode color pickers
    
    * feat: add dark/light mode separation for code preview
    
    * feat: add copy code button (#778)

--- examples/ui-demo/src/components/configuration/ColorPicker.tsx
+++ examples/ui-demo/src/components/configuration/ColorPicker.tsx
@@ -1,8 +1,9 @@
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 import { useConfig } from "@/src/app/state";
 import { Chrome, Sketch } from "@uiw/react-color";
 
-export function ColorPicker() {
+export function ColorPicker({ theme }: { theme: 'dark' | 'light'}) {
   const { config, setConfig } = useConfig();
 
   const onSetThemeColor = (color: string) => {
@@ -10,20 +11,28 @@ export function ColorPicker() {
       ...prev,
       ui: {
         ...prev.ui,
-        primaryColor: color
+        primaryColor: {
+          ...prev.ui.primaryColor,
+          [theme]: color
+        }
       }
     }))
   }
 
+  const color = config.ui.primaryColor[theme]
+
   return (
     <Popover>
-      <PopoverTrigger className="rounded-lg py-2 px-[10px] min-w-[120px] gap-2 flex items-center justify-center border border-gray-300 hover:opacity-80">
-        <div className="h-6 w-6 rounded" style={{ backgroundColor: config.ui.primaryColor }} />
-        <div className="text-sm">{config.ui.primaryColor}</div>
+      <PopoverTrigger className={cn("self-start border rounded-lg py-2 px-[10px] gap-2 flex items-center justify-center hover:opacity-80",
+        theme === 'light' && "border-gray-300",
+        theme === 'dark' && "border-gray-600 bg-[#020617] text-white"
+      )}>
+        <div className="h-6 w-6 rounded" style={{ backgroundColor: color }} />
+        <div className="text-sm">{color}</div>
       </PopoverTrigger>
       <PopoverContent side="right" sideOffset={8} className="p-0 w-fit border-0">
         <Sketch
-          color={config.ui.primaryColor}
+          color={color}
           onChange={(color) => {
             onSetThemeColor(color.hex);
           }}
