660928fb0299aa2114a89459a2cd77543d8cfc3d
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-06-26 17:43:12 -0400

    docs: add cli for generating documentation from jsdocs (#721)

--- account-kit/core/src/actions/getChain.ts
+++ account-kit/core/src/actions/getChain.ts
@@ -4,8 +4,8 @@ import type { AlchemyAccountsConfig } from "../types";
 /**
  * Gets the currently active chain
  *
- * @param config the account config object
- * @returns the currently active chain
+ * @param {AlchemyAccountsConfig} config the account config object
+ * @returns {Chain} the currently active chain
  */
 export function getChain(config: AlchemyAccountsConfig): Chain {
   return config.coreStore.getState().chain;
