5201b71c086920476f6077725ab1a75566187a2c
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-06-26 17:50:51 -0400

    docs: generate the aa-sdk core reference docs (#788)

--- aa-sdk/core/src/actions/smartAccount/checkGasSponsorshipEligibility.ts
+++ aa-sdk/core/src/actions/smartAccount/checkGasSponsorshipEligibility.ts
@@ -12,31 +12,31 @@ import type {
 
 /**
  * This function verifies the eligibility of the connected account for gas sponsorship concerning the upcoming `UserOperation` (UO) that is intended to be sent.
- * Internally, this method invokes [`buildUserOperation`](./buildUserOperation.md), which navigates through the middleware pipeline, including the `PaymasterMiddleware`. Its purpose is to construct the UO struct meant for transmission to the bundler. Following the construction of the UO struct, this function verifies if the resulting structure contains a non-empty `paymasterAndData` field.
+ * Internally, this method invokes `buildUserOperation`, which navigates through the middleware pipeline, including the `PaymasterMiddleware`. Its purpose is to construct the UO struct meant for transmission to the bundler. Following the construction of the UO struct, this function verifies if the resulting structure contains a non-empty `paymasterAndData` field.
  * You can utilize this method before sending the user operation to confirm its eligibility for gas sponsorship. Depending on the outcome, it allows you to tailor the user experience accordingly, based on eligibility.
  *
  * @example
  * ```ts
  * import { smartAccountClient } from "./smartAccountClient";
-// [!code focus:99]
-const eligible = await smartAccountClient.checkGasSponsorshipEligibility({
-  uo: {
-    data: "0xCalldata",
-    target: "0xTarget",
-    value: 0n,
-  },
-});
- 
-console.log(
-  `User Operation is ${
-    eligible ? "eligible" : "ineligible"
-  } for gas sponsorship.`
-);
+ * // [!code focus:99]
+ * const eligible = await smartAccountClient.checkGasSponsorshipEligibility({
+ *   uo: {
+ *     data: "0xCalldata",
+ *     target: "0xTarget",
+ *     value: 0n,
+ *   },
+ * });
+ *
+ * console.log(
+ *   `User Operation is ${
+ *     eligible ? "eligible" : "ineligible"
+ *   } for gas sponsorship.`
+ * );
  * ```
- * 
- * @param client the smart account client to use for making RPC calls
- * @param args {@link SendUserOperationParameters} containing the user operation, account, context, and overrides
- * @returns a Promise containing a boolean indicating if the account is elgibile for sponsorship
+ *
+ * @param {Client<TTransport, TChain, TAccount>} client the smart account client to use for making RPC calls
+ * @param {SendUserOperationParameters} args containing the user operation, account, context, and overrides
+ * @returns {Promise<boolean>} a Promise containing a boolean indicating if the account is elgibile for sponsorship
  */
 export function checkGasSponsorshipEligibility<
   TTransport extends Transport = Transport,
