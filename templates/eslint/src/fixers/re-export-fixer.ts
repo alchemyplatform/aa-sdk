b260be23b2479e32c68ab2ba1cb3a558f36ed820
moldy <michael.moldoveanu@alchemy.com>
2024-06-27 17:48:32 -0400

    refactor: eslint rule so that it shows errors in the correct file

--- templates/eslint/src/fixers/re-export-fixer.ts
+++ templates/eslint/src/fixers/re-export-fixer.ts
@@ -1,82 +1,26 @@
-import { builders } from "ast-types";
 import "dotenv/config";
-import fs from "node:fs";
-import * as recast from "recast";
+import type { Rule } from "eslint";
 import { createSyncFn } from "synckit";
-import type ts from "typescript";
-import { resolveReExport } from "../resolveReExport.js";
 
-let fixedInBatch = 0;
-
-export function reExportFixer(
-  sourceFilePath: string,
-  importedName: string,
-  fixBatchSize: number
+export function reExportFixerEslint(
+  fixer: Rule.RuleFixer,
+  node: Rule.Node,
+  context: Rule.RuleContext
 ) {
-  return () => {
-    if (fixBatchSize > 0 && fixedInBatch === fixBatchSize) return null;
+  const commentBody = getCommentSync(context.sourceCode.getText(node));
 
-    const node = resolveReExport(sourceFilePath, importedName);
-    if (!node) return null;
-
-    const sourceFile = node.getSourceFile();
-    const ast = recast.parse(sourceFile.getFullText(), {
-      parser: require("recast/parsers/typescript"),
-    });
-
-    recast.visit(ast, {
-      visitExportNamedDeclaration(path) {
-        const pathNode = path.node;
-        if (
-          (pathNode.declaration?.type === "VariableDeclaration" &&
-            pathNode.declaration.declarations.some(
-              (decl) =>
-                decl.type === "VariableDeclarator" &&
-                decl.init?.type === "ArrowFunctionExpression" &&
-                decl.id.type === "Identifier" &&
-                decl.id.name === importedName
-            )) ||
-          (pathNode.declaration?.type === "FunctionDeclaration" &&
-            pathNode.declaration.id?.name === importedName) ||
-          (pathNode.declaration?.type === "ClassDeclaration" &&
-            pathNode.declaration.id?.name === importedName)
-        ) {
-          const jsdocComment = createJsDocComment(node);
-          const comment = builders.commentBlock(
-            `*\n ${jsdocComment.replace("/**", "").replace("*/", "").trim()}\n`,
-            true,
-            false
-          );
-
-          // the comment block is not properly formatted by recast, it needs a whitespace after the closing block
-          comment.value = comment.value.replace("*/", " */");
-
-          if (!pathNode.comments) pathNode.comments = [];
-          pathNode.comments.unshift(comment);
-        }
-
-        this.traverse(path);
-      },
-    });
-
-    fs.writeFileSync(
-      sourceFile.fileName,
-      recast.print(ast, { reuseWhitespace: true }).code,
-      "utf-8"
-    );
-
-    fixedInBatch++;
-
+  let parent = node.parent;
+  // TODO: this is not right for Method and Property definitions in classes
+  while (parent && parent.type !== "ExportNamedDeclaration") {
+    parent = parent.parent;
+  }
+  if (!parent) {
     return null;
-  };
+  }
+
+  return fixer.insertTextBefore(parent, commentBody);
 }
 
 const getCommentSync = createSyncFn(require.resolve("./openai-worker.js")) as (
   code: string
 ) => string;
-
-function createJsDocComment(node: ts.Node) {
-  const commentText = getCommentSync(node.getFullText());
-
-  return commentText;
-}
