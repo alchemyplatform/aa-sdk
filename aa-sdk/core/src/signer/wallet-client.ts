c16874a410f8c48b904335ad07388d653798eb60
moldy <michael.moldoveanu@alchemy.com>
2024-06-28 16:51:13 -0400

    chore: fix the jsdocs for class methods

--- aa-sdk/core/src/signer/wallet-client.ts
+++ aa-sdk/core/src/signer/wallet-client.ts
@@ -16,6 +16,27 @@ export class WalletClientSigner implements SmartAccountSigner<WalletClient> {
   signerType: string;
   inner: WalletClient;
 
+  /**
+   * Initializes a signer with a given wallet client and signer type.
+   *
+   * @example
+   * ```ts
+   * import { WalletClientSigner } from "@aa-sdk/core";
+   * import { createWalletClient, custom } from 'viem'
+   * import { mainnet } from 'viem/chains'
+   *
+   * const client = createWalletClient({
+   *   chain: mainnet,
+   *   transport: custom(window.ethereum!)
+   * });
+   *
+   * const signer = new WalletClientSigner(client, 'wallet');
+   * ```
+   *
+   * @param {WalletClient} client The wallet client to interact with
+   * @param {string} signerType The type of signer; must be a valid signer type, otherwise an error will be thrown
+   * @throws {InvalidSignerTypeError} If the signer type is invalid
+   */
   constructor(client: WalletClient, signerType: string) {
     this.inner = client;
     if (!signerType) {
@@ -24,11 +45,52 @@ export class WalletClientSigner implements SmartAccountSigner<WalletClient> {
     this.signerType = signerType;
   }
 
+  /**
+   * Asynchronously retrieves addresses from the inner object and returns the first address after applying the `getAddress` function.
+   *
+   * @example
+   * ```ts
+   * import { WalletClientSigner } from "@aa-sdk/core";
+   * import { createWalletClient, custom } from 'viem'
+   * import { mainnet } from 'viem/chains'
+   *
+   * const client = createWalletClient({
+   *   chain: mainnet,
+   *   transport: custom(window.ethereum!)
+   * });
+   *
+   * const signer = new WalletClientSigner(client, 'wallet');
+   * console.log(await signer.getAddress());
+   * ```
+   *
+   * @returns {Promise<string>} A promise that resolves to the first address after being processed by the `getAddress` function.
+   */
   getAddress: () => Promise<`0x${string}`> = async () => {
     let addresses = await this.inner.getAddresses();
     return getAddress(addresses[0]);
   };
 
+  /**
+   * Signs a message using the account's signing method.
+   *
+   * @example
+   * ```ts
+   * import { WalletClientSigner } from "@aa-sdk/core";
+   * import { createWalletClient, custom } from 'viem'
+   * import { mainnet } from 'viem/chains'
+   *
+   * const client = createWalletClient({
+   *   chain: mainnet,
+   *   transport: custom(window.ethereum!)
+   * });
+   *
+   * const signer = new WalletClientSigner(client, 'wallet');
+   * console.log(await signer.signMessage("hello"));
+   * ```
+   *
+   * @param {string} message the message string that needs to be signed
+   * @returns {Promise<string>} a promise that resolves to the signed message
+   */
   readonly signMessage: (message: SignableMessage) => Promise<`0x${string}`> =
     async (message) => {
       const account = this.inner.account ?? (await this.getAddress());
@@ -36,6 +98,33 @@ export class WalletClientSigner implements SmartAccountSigner<WalletClient> {
       return this.inner.signMessage({ message, account });
     };
 
+  /**
+   * Signs the provided typed data using the account's private key.
+   *
+   * @example
+   * ```ts
+   * import { WalletClientSigner } from "@aa-sdk/core";
+   * import { createWalletClient, custom } from 'viem'
+   * import { mainnet } from 'viem/chains'
+   *
+   * const client = createWalletClient({
+   *   chain: mainnet,
+   *   transport: custom(window.ethereum!)
+   * });
+   *
+   * const signer = new WalletClientSigner(client, 'wallet');
+   * console.log(await signer.signTypedData({
+   *  types: {
+   *    "Message": [{ name: "content", type: "string" }]
+   *  },
+   *  primaryType: "Message",
+   *  message: { content: "Hello" },
+   * }));
+   * ```
+   *
+   * @param {TypedDataDefinition<TTypedData, TPrimaryType>} typedData The typed data to be signed
+   * @returns {Promise<Hex>} A promise that resolves to a hex string representing the signed data
+   */
   signTypedData = async <
     const TTypedData extends TypedData | { [key: string]: unknown },
     TPrimaryType extends string = string
