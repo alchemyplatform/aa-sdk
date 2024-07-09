f6f60ef8e1fb82d7f74eae7b82f20c39c3337754
Brian Wagner <brian.wagner35@gmail.com>
2024-07-09 11:09:37 -0400

    feat: responsive styling for demo app

--- examples/ui-demo/src/app/page.tsx
+++ examples/ui-demo/src/app/page.tsx
@@ -27,10 +27,10 @@ export default function Home() {
     >
       <TopNav />
       <div
-        className={`flex flex-col flex-1 px-10 py-6 w-full max-w-screen-2xl mx-auto overflow-hidden ${inter.className}`}
+        className={`flex flex-col flex-1 px-4 md:px-6 lg:px-10 py-4 md:py-6 w-full max-w-screen-2xl mx-auto overflow-hidden ${inter.className}`}
       >
         <div className="flex flex-1 gap-6 overflow-hidden">
-          <div className="flex flex-col basis-0 flex-1 bg-white border border-border rounded-lg pt-2 px-6 pb-8 overflow-y-auto scrollbar-none">
+          <div className="hidden lg:flex flex-col basis-0 flex-1 bg-white border border-border rounded-lg pt-2 px-6 pb-8 overflow-y-auto scrollbar-none">
             <Configuration />
           </div>
 
