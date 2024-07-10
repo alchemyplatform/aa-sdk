e7eae44e8f6e6fdf0b01c5c7a920271b248b4022
Brian Wagner <brian.wagner35@gmail.com>
2024-07-10 12:11:26 -0400

    feat: read auth card props from context, simplify UI config (#789)

--- examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
+++ examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
@@ -1,24 +1,12 @@
 import { useConfig } from "@/app/state";
 import { cn } from "@/lib/utils";
-import { AuthCard, AuthType, useLogout, useUser } from "@account-kit/react";
-import { useMemo } from "react";
+import { AuthCard, useLogout, useUser } from "@account-kit/react";
 
 export function AuthCardWrapper({ className }: { className?: string }) {
   const user = useUser();
 
   const { config } = useConfig();
   const { logout } = useLogout();
-
-  const sections = useMemo<AuthType[][]>(() => {
-    const output = [];
-    if (config.auth.showEmail) {
-      output.push([{ type: "email" as const }]);
-    }
-
-    output.push([{ type: "passkey" as const }]);
-
-    return output;
-  }, [config.auth]);
 
   return (
     <div
@@ -31,38 +19,12 @@ export function AuthCardWrapper({ className }: { className?: string }) {
       {!user ? (
         <div className="flex flex-col gap-2 w-[368px]">
           <div className="modal bg-surface-default shadow-md">
-          <AuthCard
-            header={<AuthCardHeader />}
-            showSignInText
-            showNavigation
-            illustrationStyle={config.ui.illustrationStyle}
-            sections={sections}
-          />
+          <AuthCard />
           </div>
         </div>
       ) : (
         <button className="text-primary font-semibold text-sm px-3 py-[11px] bg-white border border-gray-300 rounded-lg hover:shadow-md" onClick={() => logout()}>Logout</button>
       )}
     </div>
-  );
-}
-
-function AuthCardHeader() {
-  const {
-    config: {
-      ui: { logoDark, logoLight, theme },
-    },
-  } = useConfig();
-
-  const logo = theme === "dark" ? logoDark : logoLight;
-
-  if (!logo) return null;
-
-  return (
-    <img
-      style={{ height: "60px", objectFit: "contain" }}
-      src={logo.fileSrc}
-      alt={logo.fileName}
-    />
   );
 }
