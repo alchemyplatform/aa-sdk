a4b5c6492731bb20b6a1130e3ea05c0b91075029
moldy <michael.moldoveanu@alchemy.com>
2024-07-22 17:00:31 -0400

    test: add prool instances setup

--- .vitest/setupTests.ts
+++ .vitest/setupTests.ts
@@ -1,6 +1,18 @@
 import dotenv from "dotenv";
 import fetch from "node-fetch";
+import { setIntervalMining } from "viem/actions";
+import { afterAll, beforeAll } from "vitest";
+import * as instances from "./src/instances.js";
+
+const client = instances.anvilArbSepolia.getClient();
+
 dotenv.config();
 
-// @ts-expect-error wut
+// @ts-expect-error this does exist but ts is not liking it
 global.fetch = fetch;
+
+beforeAll(async () => {
+  await setIntervalMining(client, { interval: 0 });
+}, 20_000);
+
+afterAll(async () => {});
