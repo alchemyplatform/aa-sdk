f0ea57fe557aca0f659e313f7a8fedcda198a5f4
moldy <michael.moldoveanu@alchemy.com>
2024-07-29 18:06:17 -0400

    docs: add the new landing page

---
+++ site/sidebar/index.ts
@@ -0,0 +1,31 @@
+import { SidebarItem } from "vocs";
+
+export const indexSidebar: SidebarItem[] = [
+  { text: "Overview", link: "/" },
+  { text: "Quickstart", link: "/react/quickstart" },
+  {
+    text: "Concepts",
+    items: [
+      {
+        text: "Smart Contract Account",
+        link: "#TODO/concepts/smart-contract-account",
+      },
+      {
+        text: "Smart Account Client",
+        link: "#TODO/concepts/smart-account-client",
+      },
+      {
+        text: "Bundler Client",
+        link: "#TODO/concepts/bundler-client",
+      },
+      {
+        text: "Smart Account Signer",
+        link: "#TODO/concepts/smart-account-signer",
+      },
+      {
+        text: "Middleware",
+        link: "#TODO/concepts/middleware",
+      },
+    ],
+  },
+];
