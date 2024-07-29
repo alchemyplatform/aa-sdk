71665e454b7821e1a6084658e64cf019fc647c1c
moldy <michael.moldoveanu@alchemy.com>
2024-07-29 16:15:26 -0400

    docs: delete a bunch of old docs files in prep for new ones

--- site/snippets/aa-alchemy/light-account-client.ts
+++
@@ -1,8 +0,0 @@
-import { createLightAccountAlchemyClient } from "@account-kit/infra";
-import { LocalAccountSigner, sepolia } from "@aa-sdk/core";
-
-export const smartAccountClient = await createLightAccountAlchemyClient({
-  apiKey: "YOUR_API_KEY",
-  chain: sepolia,
-  signer: LocalAccountSigner.mnemonicToAccountSigner("OWNER_MNEMONIC"),
-});
--- site/snippets/react/config.ts
+++
@@ -1,8 +0,0 @@
-import { createConfig } from "@account-kit/react";
-import { sepolia } from "@aa-sdk/core";
-
-export const config = createConfig({
-  // required
-  rpcUrl: "/api/rpc",
-  chain: sepolia,
-});
