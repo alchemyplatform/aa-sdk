d1b834edccd550dd52882b0573505b215f32db01
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-07-30 11:05:39 -0400

    test: add support for testing with 070 paymaster (#864)

---
+++ .vitest/src/paymaster/types.ts
@@ -0,0 +1,34 @@
+import type { UserOperationRequest } from "@aa-sdk/core";
+import type { Address, Client, Hex } from "viem";
+
+export type Paymaster = {
+  entryPointVersion: "0.6.0" | "0.7.0";
+  entryPointAddress: Address;
+  getPaymasterStubData: () =>
+    | {
+        paymasterAndData: Hex;
+      }
+    | { paymaster: Address; paymasterData: Hex };
+  getPaymasterData: (
+    uo: UserOperationRequest,
+    client: Client & { mode: "anvil" }
+  ) => Promise<
+    { paymasterAndData: Hex } | { paymaster: Address; paymasterData: Hex }
+  >;
+  deployPaymasterContract: (
+    client: Client & { mode: "anvil" }
+  ) => Promise<Address>;
+  getPaymasterDetails: () => {
+    salt: Hex;
+    address: Address;
+    dummyData: Hex;
+    bytecode: Hex;
+    calldata: Hex;
+  };
+  getPaymasterImplDetails: () => {
+    salt: Hex;
+    address: Hex;
+    bytecode: Hex;
+    calldata: Hex;
+  };
+};
