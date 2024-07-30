a8e7f111c9c08edff77a3b4693633082320d3d87
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-07-30 18:10:28 -0400

    docs: add middleware concept doc (#882)
    * docs: add middleware concept doc
    
    * docs: apply PR comments to middleware concept docs
    
    Co-authored-by: David Philipson <david.philipson@gmail.com>
    
    ---------
    
    Co-authored-by: David Philipson <david.philipson@gmail.com>

--- site/sidebar/index.ts
+++ site/sidebar/index.ts
@@ -24,7 +24,7 @@ export const indexSidebar: SidebarItem[] = [
       },
       {
         text: "Middleware",
-        link: "#TODO/concepts/middleware",
+        link: "/concepts/middleware",
       },
     ],
   },
