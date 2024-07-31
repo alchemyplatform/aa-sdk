1b8ef7507b87a53672c33839be2751890c0f4580
moldy <michael.moldoveanu@alchemy.com>
2024-07-31 13:22:39 -0400

    docs: add back the resources section of the site

--- site/sidebar/reference/account-kit/infra.ts
+++ site/sidebar/reference/account-kit/infra.ts
@@ -64,4 +64,29 @@ export const accountKitInfraReferenceSidebar: SidebarItem[] = [
       },
     ],
   },
+  {
+    text: "Resources",
+    items: [
+      {
+        text: "FAQs",
+        link: "/resources/faqs",
+      },
+      {
+        text: "React Native",
+        link: "/resources/react-native",
+      },
+      {
+        text: "Terms",
+        link: "/resources/terms",
+      },
+      {
+        text: "Types",
+        link: "/resources/types",
+      },
+      {
+        text: "Contact us",
+        link: "/resources/contact-us",
+      },
+    ],
+  },
 ];
--- site/sidebar/smart-contracts.ts
+++ site/sidebar/smart-contracts.ts
@@ -1,4 +1,5 @@
 import { SidebarItem } from "vocs";
+import { resources } from "./shared.js";
 
 export const smartContractsSidebar: SidebarItem[] = [
   {
@@ -8,4 +9,5 @@ export const smartContractsSidebar: SidebarItem[] = [
       { text: "Getting started", link: "#TODO" },
     ],
   },
+  resources,
 ];
