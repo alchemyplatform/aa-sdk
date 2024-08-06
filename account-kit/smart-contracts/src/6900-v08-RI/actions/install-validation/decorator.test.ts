9d6bacb456a8b06f2cfa489b675c78c00f4488d6
adam <adam@alchemy.com>
2024-08-06 14:53:21 -0400

    test: add uninstallValidation test

--- account-kit/smart-contracts/src/6900-v08-RI/actions/install-validation/decorator.test.ts
+++ account-kit/smart-contracts/src/6900-v08-RI/actions/install-validation/decorator.test.ts
@@ -110,6 +110,87 @@ describe("6900 RI installValidation Tests", async () => {
     );
   });
 
+  it("should fail after uninstalling the secondary signer", async () => {
+    // Generate and add the secondary signer
+
+    const secondarySigner = new LocalAccountSigner(
+      privateKeyToAccount(keccak256(toHex("secondarySigner")))
+    );
+
+    const provider = (await givenConnectedProvider({ signer })).extend(
+      installValidationActions
+    );
+
+    await setBalance(instance.getClient(), {
+      address: provider.getAddress(),
+      value: parseEther("2"),
+    });
+
+    const entityId = 1;
+
+    const result1 = await provider.installValidation({
+      args: {
+        validationConfig: {
+          moduleAddress: SingleSignerValidationModule.meta.addresses.default,
+          entityId,
+          isGlobal: true,
+          isSignatureValidation: true,
+        },
+        selectors: [],
+        installData: SingleSignerValidationModule.encodeOnInstallData(
+          entityId,
+          await secondarySigner.getAddress()
+        ),
+        hooks: [],
+      },
+    });
+
+    const txnHash1 = provider.waitForUserOperationTransaction(result1);
+
+    await expect(txnHash1).resolves.not.toThrowError();
+
+    // Now the new validation is installed, uninstall it
+
+    const result2 = await provider.uninstallValidation({
+      args: {
+        moduleAddress: SingleSignerValidationModule.meta.addresses.default,
+        entityId,
+        uninstallData: "0x",
+        hookUninstallDatas: [],
+      },
+    });
+
+    const txnHash2 = provider.waitForUserOperationTransaction(result2);
+
+    await expect(txnHash2).resolves.not.toThrowError();
+
+    // Now the validation is uninstalled, attempt to use it to execute.
+
+    const accountAddress = provider.getAddress();
+
+    const newValidationAccountClient = await createSingleSignerRIAccountClient({
+      chain: instance.chain,
+      signer: secondarySigner,
+      accountAddress,
+      entityId,
+      transport: custom(instance.getClient()),
+    });
+
+    // Attempt to execute a transfer of 1 ETH from the account to a dead address.
+
+    const targetAddress = "0x000000000000000000000000000000000000dEaD";
+
+    const result3 = newValidationAccountClient.sendUserOperation({
+      uo: {
+        target: targetAddress,
+        value: parseEther("0.5"),
+        data: "0x",
+      },
+    });
+
+    await expect(result3).rejects.toThrowError();
+  });
+
   // todo: consolidate this test setup into a common base
   const givenConnectedProvider = async ({
     signer,
