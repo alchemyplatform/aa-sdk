3057826423422f0441273e2246715a72eee7f85a
moldy <michael.moldoveanu@alchemy.com>
2024-08-15 12:56:03 -0400

    docs: port over the old MA docs

--- site/sidebar/smart-contracts.ts
+++ site/sidebar/smart-contracts.ts
@@ -9,6 +9,65 @@ export const smartContractsSidebar: SidebarItem[] = [
       { text: "Getting started", link: "#TODO" },
     ],
   },
+  {
+    text: "Choosing a smart account",
+    items: [
+      {
+        text: "Introduction",
+        link: "/smart-contracts/choosing-a-smart-account",
+      },
+      {
+        text: "Modular Account",
+        items: [
+          { text: "Introduction", link: "/smart-contracts/modular-account" },
+          {
+            text: "Getting started",
+            link: "/smart-contracts/modular-account/getting-started",
+          },
+          {
+            text: "Deployments",
+            link: "/smart-contracts/modular-account/deployments",
+          },
+          {
+            text: "Upgrading to Modular Account",
+            link: "/smart-contracts/modular-account/upgrading-to-modular-account",
+          },
+          {
+            text: "Multisig plugin",
+            items: [
+              {
+                text: "Introduction",
+                link: "/smart-contracts/modular-account/multisig-plugin",
+              },
+              {
+                text: "Getting started",
+                link: "/smart-contracts/modular-account/multisig-plugin/getting-started",
+              },
+              {
+                text: "Technical details",
+                link: "/smart-contracts/modular-account/multisig-plugin/details",
+              },
+            ],
+            collapsed: true,
+          },
+        ],
+      },
+      { text: "Light Account", items: [] },
+      {
+        text: "Custom accounts",
+        link: "/smart-contracts/custom/using-your-own",
+      },
+      { text: "Gas benchmarks", link: "/smart-contracts/gas-benchmarks" },
+    ],
+  },
+  {
+    text: "Using smart accounts",
+    items: [],
+  },
+  {
+    text: "Extending smart accounts",
+    items: [],
+  },
   concepts,
   guides,
   resources,
