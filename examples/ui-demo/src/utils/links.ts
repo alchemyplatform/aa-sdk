a598d61d8033c984def76fa1b93e8cb0ee69bae8
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-08-16 11:08:51 -0400

    refactor: change up the sidebar in the demo app (#895)

--- examples/ui-demo/src/utils/links.ts
+++ examples/ui-demo/src/utils/links.ts
@@ -1,9 +1,8 @@
 export const links = {
-  docs: "https://accountkit.alchemy.com/getting-started/introduction.html",
+  docs: "https://aa-sdk-site-alpha.vercel.app",
   github: "https://github.com/alchemyplatform/aa-sdk/tree/v4.x.x",
   integrationCall:
     "https://calendly.com/d/cp7s-3yx-2yh/alchemy-account-kit-ui-alpha",
   // TODO: update once we have final links
-  quickstartGuide:
-    "https://alchemotion.notion.site/Alchemy-Account-Kit-UI-Components-Alpha-Quickstart-0805fd3fb8ba4c15bfd88846d4ad53ea",
+  quickstartGuide: "https://aa-sdk-site-alpha.vercel.app/react/quickstart",
 };
