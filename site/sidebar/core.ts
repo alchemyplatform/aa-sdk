f752d586172a3eb49f29b83b411142bd5c287843
moldy <michael.moldoveanu@alchemy.com>
2024-08-21 12:37:06 -0400

    docs: add more account-kit core docs

--- site/sidebar/core.ts
+++ site/sidebar/core.ts
@@ -3,5 +3,21 @@ import { SidebarItem } from "vocs";
 export const coreSidebar: SidebarItem[] = [
   { text: "Overview", link: "/core/overview" },
   { text: "Quickstart", link: "/core/quickstart" },
+  {
+    text: "Using smart accounts",
+    items: [
+      {
+        text: "Authenticate users",
+        link: "/core/authenticate-users",
+      },
+      {
+        text: "Send user operations",
+        link: "/core/send-user-operations",
+      },
+      { text: "Sponsor gas", link: "/core/sponsor-gas" },
+      { text: "Add passkey", link: "/core/add-passkey" },
+      { text: "Multi-chain apps", link: "/core/multi-chain-apps" },
+    ],
+  },
   { text: "SDK Reference", link: "/reference/account-kit/core" },
 ];
