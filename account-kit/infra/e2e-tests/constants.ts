696de7f95515e19cf56bbef22d1aceea04bb7cc1
moldy <michael.moldoveanu@alchemy.com>
2024-09-04 11:15:42 -0400

    chore: fix the publish package action

--- account-kit/infra/e2e-tests/constants.ts
+++
@@ -1,4 +0,0 @@
-export const API_KEY = process.env.API_KEY!;
-export const PAYMASTER_POLICY_ID = process.env.PAYMASTER_POLICY_ID!;
-export const LIGHT_ACCOUNT_OWNER_MNEMONIC =
-  process.env.LIGHT_ACCOUNT_OWNER_MNEMONIC!;
