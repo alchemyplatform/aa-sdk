407e6558450098f5fc70c685af8f0ee4629a322c
moldy <michael.moldoveanu@alchemy.com>
2024-09-05 19:05:40 -0400

    chore: add storybook to react components

---
+++ account-kit/react/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+import react from "@vitejs/plugin-react";
+import { mergeConfig } from "vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+
+  viteFinal: async (config) =>
+    mergeConfig(config, {
+      plugins: [react()],
+    }),
+};
+
+export default config;
