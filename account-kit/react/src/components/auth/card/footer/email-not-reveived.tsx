915c858329086fa29bc6a81b448c7cdcb8280793
Iyk Azorji <iykazorji@gmail.com>
2024-09-12 10:12:42 -0400

    fix: update email resend footer to wire in logic

--- account-kit/react/src/components/auth/card/footer/email-not-reveived.tsx
+++ account-kit/react/src/components/auth/card/footer/email-not-reveived.tsx
@@ -1,13 +1,51 @@
+import { useEffect, useState } from "react";
+import { useAuthenticate } from "../../../../hooks/useAuthenticate.js";
 import { ls } from "../../../../strings.js";
+import { useAuthContext, type AuthStep } from "../../context.js";
+import { Button } from "../../../button.js";
 
-export const EmailNotReceivedDisclaimer = () => (
-  <div className="flex flex-row gap-2 justify-center mb-3">
-    <span className="text-fg-tertiary text-xs">
-      {ls.loadingEmail.emailNotReceived}
-    </span>
-    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-    <a href="#" className="text-btn-primary text-xs underline">
-      {ls.loadingEmail.resend}
-    </a>
-  </div>
-);
+type EmailNotReceivedDisclaimerProps = {
+  authStep: Extract<AuthStep, { type: "email_verify" }>;
+};
+export const EmailNotReceivedDisclaimer = ({
+  authStep,
+}: EmailNotReceivedDisclaimerProps) => {
+  const { setAuthStep } = useAuthContext();
+  const [emailResent, setEmailResent] = useState(false);
+  const { authenticate } = useAuthenticate({
+    onSuccess: () => {
+      setAuthStep({ type: "complete" });
+    },
+  });
+
+  useEffect(() => {
+    if (emailResent) {
+      // set the text back to "Resend" after 2 seconds
+      setTimeout(() => {
+        setEmailResent(false);
+      }, 2000);
+    }
+  }, [emailResent]);
+
+  return (
+    <div className="flex flex-row gap-2 justify-center mb-3">
+      <span className="text-fg-tertiary text-xs">
+        {ls.loadingEmail.emailNotReceived}
+      </span>
+      <Button
+        variant="link"
+        className="text-xs font-normal underline"
+        disabled={emailResent}
+        onClick={() => {
+          authenticate({
+            type: "email",
+            email: authStep.email,
+          });
+          setEmailResent(true);
+        }}
+      >
+        {emailResent ? ls.loadingEmail.resent : ls.loadingEmail.resend}
+      </Button>
+    </div>
+  );
+};
