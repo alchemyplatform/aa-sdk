88a8abaf1f1f5200bb36452a4deb4ef712b25491
rob chang <robchangcodes@gmail.com>
2024-09-13 10:15:50 -0400

    fix: registration disclaimer opening new tab

--- account-kit/react/src/components/auth/card/footer/registration-disclaimer.tsx
+++ account-kit/react/src/components/auth/card/footer/registration-disclaimer.tsx
@@ -3,8 +3,12 @@ import { ls } from "../../../../strings.js";
 export const RegistrationDisclaimer = () => (
   <div className="flex flex-row gap-2 justify-center mb-3">
     <span className="text-fg-tertiary text-xs">{ls.login.tosPrefix}</span>
-    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-    <a href="#" className="text-btn-primary text-xs underline">
+    <a
+      className="text-btn-primary text-xs underline"
+      href="https://www.alchemy.com/terms-conditions/end-user-terms"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
       {ls.login.tosLink}
     </a>
   </div>
