8b103731c5bdd82d3af4a47dca552d15207606eb
Iyk Azorji <iykazorji@gmail.com>
2024-09-16 11:51:38 -0400

    feat: update wallet-icon component to use enum to switch desired wallet

--- account-kit/react/src/components/auth/card/error/icons/wallet-icon.tsx
+++ account-kit/react/src/components/auth/card/error/icons/wallet-icon.tsx
@@ -2,7 +2,7 @@ import { CoinbaseWallet } from "../../../../../icons/coinbaseWallet.js";
 import { EOAConnectionFailed } from "../../../../../icons/EOAConnectionFailed.js";
 import { MetaMask } from "../../../../../icons/metamask.js";
 import { WalletConnectIcon } from "../../../../../icons/walletConnectIcon.js";
-import type { EOAWallets } from "../types.js";
+import { EOAWallets } from "../types.js";
 import { walletTypeConfig } from "../connection-error.js";
 import { ConnectionFailed } from "../../../../../icons/connectionFailed.js";
 
@@ -14,13 +14,13 @@ export const WalletIcon = ({ walletType }: { walletType: EOAWallets }) => {
       <div className="relative">
         <EOAConnectionFailed />
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center z-[1]">
-          {walletType === "io.metamask" && (
+          {walletType === EOAWallets.METAMASK && (
             <MetaMask className="w-[35px] h-[35px]" />
           )}
-          {walletType === "WalletConnect" && (
+          {walletType === EOAWallets.WALLET_CONNECT && (
             <WalletConnectIcon className="w-[25px] h-[25px]" />
           )}
-          {walletType === "com.coinbase.wallet" && (
+          {walletType === EOAWallets.COINBASE_WALLET && (
             <CoinbaseWallet className="w-[35px] h-[35px]" />
           )}
         </div>
