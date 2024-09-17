a8c616d46ff06d581b1acaacba49c98339863400
rob chang <robchangcodes@gmail.com>
2024-09-17 15:11:12 -0400

    feat: allow passkey flow in demo app.  handleAuthSuccess callback when complete

--- account-kit/react/src/components/auth/modal.tsx
+++ account-kit/react/src/components/auth/modal.tsx
@@ -3,14 +3,17 @@ import { useAuthModal } from "../../hooks/useAuthModal.js";
 import { Dialog } from "../dialog/dialog.js";
 import { useUiConfig } from "../../hooks/useUiConfig.js";
 
-export const AuthModal = () => {
+type AuthModalProps = {
+  handleAuthSuccess?: () => void;
+};
+export const AuthModal = ({ handleAuthSuccess }: AuthModalProps) => {
   const { modalBaseClassName } = useUiConfig();
   const { isOpen, closeAuthModal } = useAuthModal();
 
   return (
     <Dialog isOpen={isOpen} onClose={closeAuthModal}>
       <div className={`modal md:w-[368px] ${modalBaseClassName ?? ""}`}>
-        <AuthCardContent showClose />
+        <AuthCardContent handleAuthSuccess={handleAuthSuccess} showClose />
       </div>
     </Dialog>
   );
--- examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
+++ examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
@@ -1,13 +1,18 @@
 import { useConfig } from "@/app/state";
 import { cn } from "@/lib/utils";
 import { AuthCard, useLogout, useUser } from "@account-kit/react";
+import { useState } from "react";
 
 export function AuthCardWrapper({ className }: { className?: string }) {
   const user = useUser();
-
-  const { config } = useConfig();
+  const {config} = useConfig();
   const { logout } = useLogout();
 
+  const [isAuthComplete, setIsAuthComplete] = useState(false);
+  const handleAuthSuccess = () => {
+    console.log("Auth Success");
+    setIsAuthComplete(true);
+  }
   return (
     <div
       className={cn(
@@ -16,20 +21,18 @@ export function AuthCardWrapper({ className }: { className?: string }) {
         className
       )}
     >
-      {!user ? (
         <div className="flex flex-col gap-2 w-[368px]">
           <div className="modal bg-surface-default shadow-md overflow-hidden">
-            <AuthCard />
+            <AuthCard handleAuthSuccess={handleAuthSuccess} />
           </div>
         </div>
-      ) : (
-        <button
+       {user && (<button
           className="text-primary font-semibold text-sm px-3 py-[11px] bg-white border border-gray-300 rounded-lg hover:shadow-md"
           onClick={() => logout()}
         >
           Logout
-        </button>
-      )}
+          {isAuthComplete && <span> (Auth Complete)</span>}
+        </button>)} 
     </div>
   );
 }
