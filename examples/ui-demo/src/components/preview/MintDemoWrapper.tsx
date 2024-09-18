3499c2fe4a2f8fb8c056ad24e312a40258c66c5d
rob chang <robchangcodes@gmail.com>
2024-09-18 16:32:20 -0400

    feat: basic styling for nft collect

--- examples/ui-demo/src/components/preview/MintDemoWrapper.tsx
+++ examples/ui-demo/src/components/preview/MintDemoWrapper.tsx
@@ -1,4 +1,9 @@
 import { UserConnectionAvatarWithPopover } from "@/components/shared/user-connection-avatar/UserConnectionAvatarWithPopover";
+import Image from "next/image";
+import { CheckIcon } from "../icons/check";
+import { GasIcon } from "../icons/gas";
+import { DrawIcon } from "../icons/draw";
+import { ReceiptIcon } from "../icons/receipt";
 
 export function MintDemoWrapper() {
   return (
