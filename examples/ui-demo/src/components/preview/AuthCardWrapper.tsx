936e7d8142e2d92c3242d5395f4137d5a51a5808
rob chang <robchangcodes@gmail.com>
2024-09-20 09:50:53 -0400

    feat: mdx useAuthentication update

--- examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
+++ examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
@@ -5,7 +5,6 @@ import {
   useLogout,
   useUser,
 } from "@account-kit/react";
-import { MintDemoWrapper } from "./MintDemoWrapper";
 import { useMemo } from "react";
 
 export function AuthCardWrapper({ className }: { className?: string }) {
@@ -29,7 +28,8 @@ export function AuthCardWrapper({ className }: { className?: string }) {
           </div>
         </>
       ) : (
-        <MintDemoWrapper />
+        null
+        // In flight- will be uncommented in the mint pr, a fast follow <MintDemoWrapper />
       )}
       {user && (
         <button
