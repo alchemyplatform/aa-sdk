be1357bdc7aed3fb07636f93ed9c8862243eb3aa
Iyk Azorji <iykazorji@gmail.com>
2024-09-26 17:38:17 -0400

    feat: update footer styling to match updated design specs

--- account-kit/react/src/icons/timeout.tsx
+++ account-kit/react/src/icons/timeout.tsx
@@ -1,34 +1,180 @@
 import { type SVGProps } from "react";
+import { useUiConfig } from "../hooks/useUiConfig.js";
 
 export const Timeout = ({
   className,
   ...props
 }: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => {
+  const { illustrationStyle } = useUiConfig();
   return (
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 37 45"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      {...props}
-    >
-      <circle cx="18.3428" cy="26.543" r="18.0571" fill="#FECACA" />
-      <path
-        d="M18.3999 17.4004L18.3999 27.8004"
-        stroke="#DC2626"
-        strokeWidth="2.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M13.3269 2.2002L23.3587 2.20019"
-        stroke="#DC2626"
-        strokeWidth="2.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
+    <>
+      {illustrationStyle === "outline" && (
+        <TimedOutIconOutline className={className} {...props} />
+      )}
+      {illustrationStyle === "filled" && (
+        <TimedOutIconFilled className={className} {...props} />
+      )}
+      {illustrationStyle === "linear" && (
+        <TimedOutIconLinear className={className} {...props} />
+      )}
+      {illustrationStyle === "flat" && (
+        <TimedOutIconFlat className={className} {...props} />
+      )}
+    </>
   );
 };
+const TimedOutIconOutline = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <circle
+      cx="24.3428"
+      cy="27.5426"
+      r="18.0571"
+      stroke="#DC2626"
+      stroke-width="2.5"
+      className="stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+    <path
+      d="M24.3999 18.3999L24.3999 28.7999"
+      stroke="#DC2626"
+      stroke-width="2.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+    <path
+      d="M19.3269 3.19995L29.3587 3.19995"
+      stroke="#DC2626"
+      stroke-width="2.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+  </svg>
+);
+
+const TimedOutIconFilled = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <circle cx="24.3428" cy="27.5427" r="18.0571" fill="#EF4444" />
+    <path
+      d="M24.3999 18.4L24.3999 28.8"
+      stroke="white"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M19.3269 3.20001L29.3587 3.20001"
+      stroke="#EF4444"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+const TimedOutIconLinear = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M25.5 10.5324V29.5H23.5002L14.7756 12.536C17.3152 11.1731 20.2185 10.4 23.3024 10.4C24.0463 10.4 24.7797 10.445 25.5 10.5324Z"
+      fill="#FEE2E2"
+      className="fill-[#FEE2E2] dark:fill-[rgba(220,38,38,0.2)]"
+    />
+    <circle
+      cx="24.3428"
+      cy="27.5427"
+      r="18.0571"
+      stroke="#DC2626"
+      strokeWidth="2.5"
+      className="=stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+    <path
+      d="M24.3999 18.4L24.3999 28.8"
+      stroke="#DC2626"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="=stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+    <path
+      d="M19.3269 3.20001L29.3587 3.20001"
+      stroke="#DC2626"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="=stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+  </svg>
+);
+
+const TimedOutIconFlat = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <circle
+      cx="24.3428"
+      cy="27.5426"
+      r="18.0571"
+      fill="#FECACA"
+      className="fill-[#FEE2E2] dark:fill-[rgba(220,38,38,0.2)]"
+    />
+    <path
+      d="M24.3999 18.3999L24.3999 28.7999"
+      stroke="#DC2626"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+    <path
+      d="M19.3269 3.19995L29.3587 3.19995"
+      stroke="#DC2626"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+  </svg>
+);
