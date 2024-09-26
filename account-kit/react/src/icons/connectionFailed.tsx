ce1e6c7e6573ee8ee502da6bfe5ddba87113afbd
Iyk Azorji <iykazorji@gmail.com>
2024-09-26 14:52:09 -0400

    feat: add illustration styling to connection faild icons

--- account-kit/react/src/icons/connectionFailed.tsx
+++ account-kit/react/src/icons/connectionFailed.tsx
@@ -1,26 +1,143 @@
 import { type SVGProps } from "react";
+import { useUiConfig } from "../hooks/useUiConfig.js";
 
 export const ConnectionFailed = ({
   className,
   ...props
 }: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => {
+  const { illustrationStyle } = useUiConfig();
   return (
-    <svg
-      width="100%"
-      height="100%"
-      viewBox="0 0 47 47"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      {...props}
-    >
-      <circle cx="23.6" cy="23.5998" r="22.8" fill="#FECACA" />
-      <path
-        d="M23.76 29.3717V30.1431M23.76 17.0288L23.7662 24.7431"
-        stroke="#DC2626"
-        strokeWidth="3"
-        strokeLinecap="round"
-      />
-    </svg>
+    <>
+      {illustrationStyle === "outline" && (
+        <ConnectionFailedOutline className={className} {...props} />
+      )}
+      {illustrationStyle === "filled" && (
+        <ConnectionFailedFilled className={className} {...props} />
+      )}
+      {illustrationStyle === "linear" && (
+        <ConnectionFailedLinear className={className} {...props} />
+      )}
+      {illustrationStyle === "flat" && (
+        <ConnectionFailedFlat className={className} {...props} />
+      )}
+    </>
   );
 };
+
+const ConnectionFailedOutline = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="39"
+    height="39"
+    viewBox="0 0 39 39"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <circle
+      cx="19.6572"
+      cy="19.6571"
+      r="18.0571"
+      stroke="#DC2626"
+      strokeWidth="2.5"
+      className="stroke-[#DC2626] dark:[#EF4444]"
+    />
+    <path
+      d="M19.76 25.3718V26.1432M19.76 13.0289L19.7662 20.7432"
+      stroke="#DC2626"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+const ConnectionFailedFilled = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="37"
+    height="37"
+    viewBox="0 0 37 37"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <circle cx="18.6572" cy="18.6571" r="18.0571" fill="#EF4444" />
+    <path
+      d="M18.76 24.3718V25.1432M18.76 12.0289L18.7662 19.7432"
+      stroke="white"
+      stroke-width="3"
+      stroke-linecap="round"
+    />
+  </svg>
+);
+const ConnectionFailedLinear = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="39"
+    height="39"
+    viewBox="0 0 39 39"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M2 15.857C1.738 17.0812 1.6001 18.3515 1.6001 19.654C1.6001 20.9565 1.738 22.2267 2 23.451V15.857ZM20 37.7079C29.8145 37.5252 37.7144 29.5122 37.7144 19.654C37.7144 9.79581 29.8145 1.78275 20 1.60004V37.7079Z"
+      fill="#FEE2E2"
+      className="fill-[#FEE2E2] dark:fill-[rgba(220,38,38,0.2)]"
+    />
+    <circle
+      cx="19.6572"
+      cy="19.6571"
+      r="18.0571"
+      stroke="#DC2626"
+      stroke-width="2.5"
+    />
+    <path
+      d="M19.76 25.3717V26.1432M19.76 13.0289L19.7662 20.7432"
+      stroke="#DC2626"
+      stroke-width="3"
+      stroke-linecap="round"
+    />
+  </svg>
+);
+
+const ConnectionFailedFlat = ({
+  className,
+  ...props
+}: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    width="37"
+    height="37"
+    viewBox="0 0 37 37"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <circle
+      cx="18.6572"
+      cy="18.6571"
+      r="18.0571"
+      fill="#FECACA"
+      className="fill-[#FECACA] dark:fill-[rgba(239,68,68,0.2)]"
+    />
+    <path
+      d="M18.76 24.3718V25.1432M18.76 12.0289L18.7662 19.7432"
+      stroke="#DC2626"
+      stroke-width="3"
+      stroke-linecap="round"
+      className="stroke-[#DC2626] dark:stroke-[#EF4444]"
+    />
+  </svg>
+);
--- account-kit/react/src/icons/timeout.tsx
+++ account-kit/react/src/icons/timeout.tsx
@@ -18,16 +18,16 @@ export const Timeout = ({
       <path
         d="M18.3999 17.4004L18.3999 27.8004"
         stroke="#DC2626"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M13.3269 2.2002L23.3587 2.20019"
         stroke="#DC2626"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
