febb5688d96ae31cd0423bd65a46602079ca4c73
Iyk Azorji <iykazorji@gmail.com>
2024-09-26 19:07:03 -0400

    fix: remove jsdoc type issues

--- examples/ui-demo/src/utils/truncate-address.ts
+++ examples/ui-demo/src/utils/truncate-address.ts
@@ -1,15 +1,10 @@
 // Captures 0x + 4 characters, then the last 5 characters.
 const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{5})$/;
 
-/**
- * Truncates an ethereum address to the format 0x0000…0000
- * @param address Full address to truncate
- * @returns Truncated address
- */
 const truncateAddress = (address: string) => {
-  const match = address.match(truncateRegex);
-  if (!match) return address;
-  return `${match[1]}…${match[2]}`;
+	const match = address.match(truncateRegex);
+	if (!match) return address;
+	return `${match[1]}…${match[2]}`;
 };
 
 export default truncateAddress;
