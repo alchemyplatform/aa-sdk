48e805c14264c74fa71c13562e33e0948ac44186
rob chang <robchangcodes@gmail.com>
2024-10-01 14:00:28 -0400

    fix: linting

--- examples/ui-demo/src/hooks/useToast.tsx
+++ examples/ui-demo/src/hooks/useToast.tsx
@@ -4,8 +4,10 @@ import { ToastContext } from "@/contexts/ToastProvider";
 import { useContext } from "react";
 
 export const useToast = () => {
-  if (!ToastContext) { 
-    throw Error("useToast can only be used by components that are descendants of the ToastProvider`");
+  if (!ToastContext) {
+    throw Error(
+      "useToast can only be used by components that are descendants of the ToastProvider`"
+    );
   }
   const toastContext = useContext(ToastContext);
   return toastContext;
