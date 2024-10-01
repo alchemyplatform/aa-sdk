d8c898918dd9935b681366b38dcb8b01113ab950
rob chang <robchangcodes@gmail.com>
2024-10-01 13:57:51 -0400

    fix: remove destructured context, add error state, hard-coded white text on button

--- examples/ui-demo/src/hooks/useToast.tsx
+++ examples/ui-demo/src/hooks/useToast.tsx
@@ -4,6 +4,9 @@ import { ToastContext } from "@/contexts/ToastProvider";
 import { useContext } from "react";
 
 export const useToast = () => {
-  const { toast, setToast, closeToast } = useContext(ToastContext);
-  return { toast, setToast, closeToast };
+  if (!ToastContext) { 
+    throw Error("useToast can only be used by components that are descendants of the ToastProvider`");
+  }
+  const toastContext = useContext(ToastContext);
+  return toastContext;
 };
