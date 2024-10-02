5060b9153cb5d3850efa1cdf593a196bd5b527b0
moldy <michael.moldoveanu@alchemy.com>
2024-10-02 14:46:09 -0400

    chore: add the logging package skeleton

--- aa-sdk/core/e2e-tests/constants.ts
+++
@@ -1,4 +0,0 @@
-export const STAGING = process.env.STAGING!;
-export const API_KEY = process.env.API_KEY!;
-export const API_KEY_STAGING = process.env.API_KEY_STAGING!;
-export const OWNER_MNEMONIC = process.env.OWNER_MNEMONIC!;
