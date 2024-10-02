bcbcac5f1f879a2b58fee5cfb805ce6eab07eb33
rob chang <robchangcodes@gmail.com>
2024-10-02 11:37:10 -0400

    fix: prevent mintcard without client loaded

--- examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
+++ examples/ui-demo/src/components/preview/AuthCardWrapper.tsx
@@ -1,11 +1,14 @@
 import { useConfig } from "@/app/state";
 import { cn } from "@/lib/utils";
-import { AuthCard, useUser } from "@account-kit/react";
+import { AuthCard, useSmartAccountClient, useUser } from "@account-kit/react";
 import { MintCard } from "../shared/MintCard";
+import { LoadingIcon } from "../icons/loading";
 
 export function AuthCardWrapper({ className }: { className?: string }) {
   const user = useUser();
   const { config } = useConfig();
+  const { client } = useSmartAccountClient({ type: "LightAccount" });
+
   return (
     <div
       className={cn(
@@ -14,15 +17,31 @@ export function AuthCardWrapper({ className }: { className?: string }) {
         className
       )}
     >
-      {!user ? (
-        <div className="flex flex-col gap-2 w-[368px]">
-          <div className="modal bg-surface-default shadow-md overflow-hidden">
-            <AuthCard />
-          </div>
-        </div>
-      ) : (
-        <MintCard />
-      )}
+      <RenderContent hasUser={!!user} hasClient={!!client} />
     </div>
   );
 }
+
+const RenderContent = ({
+  hasUser,
+  hasClient,
+}: {
+  hasUser: boolean;
+  hasClient: boolean;
+}) => {
+  if (!hasUser) {
+    return (
+      <div className="flex flex-col gap-2 w-[368px]">
+        <div className="modal bg-surface-default shadow-md overflow-hidden">
+          <AuthCard />
+        </div>
+      </div>
+    );
+  }
+
+  if (hasClient) {
+    return <MintCard />;
+  }
+
+  return <LoadingIcon />;
+};
