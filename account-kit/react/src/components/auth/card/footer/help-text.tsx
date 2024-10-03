5b9cf48da9122f1c63ae85f15c43b8b9859217c2
Rob <30500864+RobChangCA@users.noreply.github.com>
2024-10-03 11:02:11 -0400

    fix: supportURL wired into config (#1006)
    * fix: supportURL wired into config
    
    * fix: linting and clean scripts
    
    * fix: support url casing, plug into passkey, story update

--- account-kit/react/src/components/auth/card/footer/help-text.tsx
+++ account-kit/react/src/components/auth/card/footer/help-text.tsx
@@ -1,13 +1,17 @@
+import { useUiConfig } from "../../../../hooks/useUiConfig.js";
 import { ls } from "../../../../strings.js";
 
-export const HelpText = () => (
-  <div className="flex flex-row gap-2 justify-center mb-3">
-    <span className="text-fg-tertiary text-xs">
-      {ls.loadingPasskey.supportText}
-    </span>
-    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-    <a href="#" className="text-btn-primary text-xs underline">
-      {ls.loadingPasskey.supportLink}
-    </a>
-  </div>
-);
+export const HelpText = () => {
+  const { supportUrl } = useUiConfig();
+  if (!supportUrl) return null;
+  return (
+    <div className="flex flex-row gap-2 justify-center mb-3">
+      <span className="text-fg-tertiary text-xs">
+        {ls.loadingPasskey.supportText}
+      </span>
+      <a href={supportUrl} className="text-btn-primary text-xs underline">
+        {ls.loadingPasskey.supportLink}
+      </a>
+    </div>
+  );
+};
