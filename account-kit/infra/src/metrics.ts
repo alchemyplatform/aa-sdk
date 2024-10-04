8f87ecfda0d3fb7470d715e5c43ce3ea72122cc7
moldy <michael.moldoveanu@alchemy.com>
2024-10-04 13:23:55 -0400

    feat: log infra events

--- account-kit/infra/src/metrics.ts
+++ account-kit/infra/src/metrics.ts
@@ -1,11 +1,14 @@
 import { createLogger } from "@account-kit/logging";
+import type { Address } from "viem";
 import { VERSION } from "./version.js";
 
 export type InfraEventsSchema = [
   {
     EventName: "client_send_uo";
     EventData: {
-      signer_type: string;
+      signerType: string;
+      chainId: number;
+      entryPoint: Address;
     };
   }
 ];
