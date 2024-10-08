4785962ad281f57851f0d98680252f816f384df9
linna <linna@alchemy.com>
2024-10-08 12:03:07 -0700

    fix: handle connection error

--- account-kit/react/src/components/auth/hooks/useOAuthVerify.ts
+++ account-kit/react/src/components/auth/hooks/useOAuthVerify.ts
@@ -3,29 +3,37 @@ import { useCallback } from "react";
 import { useAuthenticate } from "../../../hooks/useAuthenticate.js";
 import { useAuthContext } from "../context.js";
 import { useUiConfig } from "../../../hooks/useUiConfig.js";
+import type { AuthType } from "../types.js";
 
 export type UseOAuthVerifyReturnType = {
   authenticate: () => void;
   isPending: boolean;
 };
 
-export const useOAuthVerify = (): UseOAuthVerifyReturnType => {
+export const useOAuthVerify = ({
+  type,
+  ...config
+}: Extract<AuthType, { type: "social" }>): UseOAuthVerifyReturnType => {
   const { setAuthStep } = useAuthContext();
   const { supportUrl } = useUiConfig();
 
-  const { authenticateAsync: authenticateAsync_, isPending } = useAuthenticate({
+  const { authenticate: authenticate_, isPending } = useAuthenticate({
     onMutate: () => {
       setAuthStep({
         type: "oauth_completing",
         provider: "google",
+        // We dont need this here, because of useUiConfig can get this from context
         supportUrl: supportUrl,
       });
     },
     onError: (err) => {
       setAuthStep({
         type: "oauth_completing",
+        // Parse config.authProviderID unless it's Auth0, the check for auth0Connection(?) for the provider name
         provider: "google",
+        // We dont need this here, because of useUiConfig can get this from context
         supportUrl: supportUrl,
+        // optionally pass in the logoUrl if Auth0
         error: err,
       });
     },
@@ -34,16 +42,17 @@ export const useOAuthVerify = (): UseOAuthVerifyReturnType => {
     },
   });
 
-  const authenticateAsync = useCallback(() => {
-    authenticateAsync_({
+  const authenticate = useCallback(() => {
+    authenticate_({
       type: "oauth",
-      authProviderId: "google",
-      mode: "popup",
+      ...config,
+      // authProviderId: "google",
+      // mode: "popup",
     });
-  }, [authenticateAsync_]);
+  }, [authenticate_]);
 
   return {
     isPending,
-    authenticate: authenticateAsync,
+    authenticate: authenticate,
   };
 };
