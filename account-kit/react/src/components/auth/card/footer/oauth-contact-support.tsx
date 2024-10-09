70c63da67d7caf6561424d78becbd971b65dc3ff
linna <linna@alchemy.com>
2024-10-08 17:19:04 -0700

    chore: move hard coded strings into ls

--- account-kit/react/src/components/auth/card/footer/oauth-contact-support.tsx
+++ account-kit/react/src/components/auth/card/footer/oauth-contact-support.tsx
@@ -1,18 +1,21 @@
 import { useUiConfig } from "../../../../hooks/useUiConfig.js";
+import { ls } from "../../../../strings.js";
 
 export const OAuthContactSupport = () => {
   const { supportUrl } = useUiConfig();
   return (
     supportUrl && (
       <div className="flex flex-row gap-1 justify-center mb-3">
-        <span className="text-fg-tertiary text-xs">Having trouble?</span>
+        <span className="text-fg-tertiary text-xs">
+          {ls.oauthContactSupport.title}
+        </span>
         <a
           className="text-btn-primary text-xs underline"
           href={supportUrl}
           target="_blank"
           rel="noreferrer noopener"
         >
-          Contact Support
+          {ls.oauthContactSupport.body}
         </a>
       </div>
     )
