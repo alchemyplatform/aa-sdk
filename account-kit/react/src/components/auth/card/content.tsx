368629f2abf4c0f13d1eec52242a43c24395b1d9
Iyk Azorji <iykazorji@gmail.com>
2024-10-10 16:28:58 -0400

    feat: add cancel buttons to auth loading states (#1029)
    * feat: add cancel buttons to auth loading states
    
    * feat: use secondary button instead of render prop to render cancel button on cardContent

--- account-kit/react/src/components/auth/card/content.tsx
+++ account-kit/react/src/components/auth/card/content.tsx
@@ -6,6 +6,10 @@ interface CardContentProps {
   description: ReactNode | string;
   error?: Error | string;
   className?: string;
+  secondaryButton?: {
+    title: string;
+    onClick: () => void;
+  };
 }
 
 // eslint-disable-next-line jsdoc/require-jsdoc
@@ -13,23 +17,34 @@ export const CardContent = ({
   header,
   icon,
   description,
+  secondaryButton,
   className,
 }: CardContentProps) => {
   return (
-    <div className={`flex flex-col gap-5 items-center ${className}`}>
-      {icon && (
-        <div className="flex flex-col items-center justify-center w-[56px] h-[56px]">
-          {icon}
-        </div>
+    <>
+      <div className={`flex flex-col gap-5 items-center ${className}`}>
+        {icon && (
+          <div className="flex flex-col items-center justify-center w-[56px] h-[56px]">
+            {icon}
+          </div>
+        )}
+        <span className="text-lg text-fg-primary font-semibold">{header}</span>
+        {typeof description === "string" ? (
+          <p className="text-fg-secondary text-center font-normal text-sm">
+            {description}
+          </p>
+        ) : (
+          description
+        )}
+      </div>
+      {secondaryButton && (
+        <button
+          className="btn btn-secondary w-full"
+          onClick={secondaryButton.onClick}
+        >
+          {secondaryButton.title}
+        </button>
       )}
-      <span className="text-lg text-fg-primary font-semibold">{header}</span>
-      {typeof description === "string" ? (
-        <p className="text-fg-secondary text-center font-normal text-sm">
-          {description}
-        </p>
-      ) : (
-        description
-      )}
-    </div>
+    </>
   );
 };
