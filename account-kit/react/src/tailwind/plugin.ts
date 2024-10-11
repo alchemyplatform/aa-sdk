5571849c8d932fa25b1ef101bf035c9a5ff98924
Rob <30500864+RobChangCA@users.noreply.github.com>
2024-10-11 09:00:17 -0400

    fix: drawer animation, zoom on input, clip path error (#1042)
    * fix: drawer animation, zoom on input, clip path error
    
    * fix: email
    
    * fix: logo now refreshes page
    
    * fix: update header text in demo app
    
    ---------
    
    Co-authored-by: Rohan Thomare <3498866+rthomare@users.noreply.github.com>

--- account-kit/react/src/tailwind/plugin.ts
+++ account-kit/react/src/tailwind/plugin.ts
@@ -116,8 +116,14 @@ export const accountKitUi: (
               "100%": { opacity: "1" },
             },
             "slide-up": {
-              "0%": { transform: "translateY(100%)", opacity: "0" },
-              "100%": { transform: "translateY(0%))", opacity: "1" },
+              "0%": {
+                transform: "translateY(100%) translateZ(0)",
+                opacity: "0",
+              },
+              "100%": {
+                transform: "translateY(0%))  translateZ(0)",
+                opacity: "1",
+              },
             },
           },
           animation: {
