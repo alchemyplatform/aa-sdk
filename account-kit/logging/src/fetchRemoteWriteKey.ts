a24533de0bad567933306979590da0a648e4424f
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-10-14 18:20:21 -0400

    refactor: use remote logger config (#1058)

---
+++ account-kit/logging/src/fetchRemoteWriteKey.ts
@@ -0,0 +1,17 @@
+export async function fetchRemoteWriteKey(): Promise<string | undefined> {
+  try {
+    const json = await fetch(
+      "https://static.alchemyapi.io/assets/accountkit/logger_config.json",
+      {
+        headers: {
+          ContentType: "application/json",
+        },
+      }
+    ).then((res) => res.json());
+
+    return json.writeKey as string | undefined;
+  } catch (e) {
+    console.warn("failed to fetch write key");
+    return undefined;
+  }
+}
