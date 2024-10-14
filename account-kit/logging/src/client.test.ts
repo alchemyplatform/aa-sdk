765568db713d50f1256477c6e7c1378105b588dd
Michael Moldoveanu <michael.moldoveanu@alchemy.com>
2024-10-14 18:20:21 -0400

    refactor: use remote logger config (#1058)

--- account-kit/logging/src/client.test.ts
+++ account-kit/logging/src/client.test.ts
@@ -1,61 +1,85 @@
 import { vi } from "vitest";
 import * as writeKeyConfig from "./_writeKey.js";
+import * as fetchKeyModule from "./fetchRemoteWriteKey.js";
 import { noopLogger } from "./noop.js";
+import type { LoggerContext } from "./types.js";
 
 const devSpy = vi.spyOn(writeKeyConfig, "WRITE_IN_DEV", "get");
-const keySpy = vi.spyOn(writeKeyConfig, "WRITE_KEY", "get");
+const keySpy = vi.spyOn(fetchKeyModule, "fetchRemoteWriteKey");
+
+type TestSchema = [
+  {
+    EventName: "test";
+    EventData: {
+      test: boolean;
+    };
+  }
+];
 
 describe("Client Logger", () => {
+  const loggerContext: LoggerContext = {
+    package: "test/signer",
+    version: "1.0.0",
+  };
+
   beforeEach(() => {
     vi.resetAllMocks();
     // @ts-ignore this does exist
     global.jsdom.reconfigure({ url: "http://localhost" });
+
+    keySpy.mockImplementation(async () => undefined);
   });
 
-  it("should create a no-op logger if no write key", async () => {
-    keySpy.mockImplementation(() => undefined);
-
+  it("should not log events if no write key", async () => {
+    // @ts-ignore this does exist
+    global.jsdom.reconfigure({ url: "https://test.com" });
+    const consoleSpy = vi.spyOn(console, "log");
+    consoleSpy.mockImplementation(() => {});
     const { createClientLogger } = await import("./client.js");
 
-    const logger = createClientLogger();
-    expect(logger).toBe(noopLogger);
+    const logger = createClientLogger<TestSchema>(loggerContext);
+    await logger._internal.ready;
+    await logger.trackEvent({ name: "test", data: { test: true } });
+
+    expect(consoleSpy).toHaveBeenCalledTimes(0);
   });
 
   it("should create a no-op logger if not write in dev", async () => {
-    devSpy.mockImplementation(() => true);
+    devSpy.mockImplementation(() => false);
 
     const { createClientLogger } = await import("./client.js");
 
-    const logger = createClientLogger();
+    const logger = createClientLogger<TestSchema>(loggerContext);
     expect(logger).toBe(noopLogger);
   });
 
   it("should create a valid logger if not localhost", async () => {
-    keySpy.mockImplementation(() => "test-key");
+    keySpy.mockImplementation(() => Promise.resolve("test-key"));
     // @ts-ignore this does exist
     global.jsdom.reconfigure({ url: "https://test.com" });
 
     const { createClientLogger } = await import("./client.js");
 
-    const logger = createClientLogger();
+    const logger = createClientLogger<TestSchema>(loggerContext);
     expect(logger).not.toBe(noopLogger);
   });
 
   it("should create a console logger in dev", async () => {
     const fetchSpy = vi.spyOn(global, "fetch");
-    keySpy.mockImplementation(() => "test-key");
+    keySpy.mockImplementation(() => Promise.resolve("test-key"));
     devSpy.mockImplementation(() => true);
     const consoleSpy = vi.spyOn(console, "log");
     consoleSpy.mockImplementation(() => {});
 
     const { createClientLogger } = await import("./client.js");
 
-    const logger = createClientLogger();
+    const logger = createClientLogger<TestSchema>(loggerContext);
     expect(logger).not.toBe(noopLogger);
     await logger._internal.ready;
 
     await logger.trackEvent({ name: "test", data: { test: true } });
     expect(fetchSpy).toHaveBeenCalledTimes(0);
+
     expect(JSON.parse(consoleSpy.mock.calls[0][0])).toMatchInlineSnapshot(
       {
         timestamp: expect.any(String),
@@ -80,7 +104,9 @@ describe("Client Logger", () => {
         },
         "messageId": Any<String>,
         "properties": {
+          "package": "test/signer",
           "test": true,
+          "version": "1.0.0",
         },
         "timestamp": Any<String>,
         "type": "track",
