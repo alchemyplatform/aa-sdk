1f5f18cc3cb5fde1dd169703b51f7c98c7060530
Ava Robinson <avamrobinson@gmail.com>
2024-10-15 13:35:33 -0400

    docs: adding intro concepts doc

--- site/sidebar/index.ts
+++ site/sidebar/index.ts
@@ -1,10 +1,16 @@
 import { SidebarItem } from "vocs";
-import { sharedSidebar } from "./shared.js";
+import { sharedSidebar, concepts } from "./shared.js";
 
 export const indexSidebar: (
   section?: "react" | "core" | "infra" | "signer" | "contracts"
-) => SidebarItem[] = (section) => [
-  { text: "Overview", link: "/" },
-  { text: "Quickstart", link: "/react/quickstart" },
-  ...sharedSidebar(section),
-];
+) => SidebarItem[] = (section) => [intro, ...sharedSidebar(section)];
+
+export const intro: SidebarItem = {
+  text: "Introduction",
+  items: [
+    { text: "Overview", link: "/" },
+    { text: "Quickstart", link: "/react/quickstart" },
+    { text: "Demo", link: "https://demo.alchemy.com/" },
+    concepts,
+  ],
+};
