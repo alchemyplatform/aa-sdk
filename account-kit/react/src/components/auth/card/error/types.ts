150525a9245d26029105509de51c175b61d6e4ab
Iyk Azorji <iykazorji@gmail.com>
2024-10-25 23:42:31 -0400

    feat:  add custom error messages for chainId errors (#1101)
    * fix: update EOA connections to re-call connect() funciton on re-try
    
    * feat: clear signer state when the user cancels EOA operation
    
    * fix: add logic to clear walletConnect indexedDB store on failed connection
    
    * feat: add errror message to handle wallet connect chainId not supported
    
    * feat: add custom error messages for unsupported chainIds
    
    * feat: move walletConnect store clearing logic to disconnect function
    
    * feat: update disconnect() usage not to use signer.disconnect()
    
    * feat: remove signer disconnection logic from cancel button on EOA card
    
    * feat: remove signer disconnection logic from cancel button on EOA card
    
    * feat: add default error message to factory
    
    * feat: add walletConnect params not found error state
    
    * feat: throw error if walletConnect parameters are not properly set

--- account-kit/react/src/components/auth/card/error/types.ts
+++ account-kit/react/src/components/auth/card/error/types.ts
@@ -1,5 +1,13 @@
 import type { KnownAuthProvider } from "@account-kit/signer";
 import type { Connector } from "@wagmi/core";
+
+export type CustomErrorMessage = {
+  heading?: string;
+  body?: string;
+  tryAgainCTA?: string;
+  useAnotherMethodCTA?: string;
+};
+
 export enum EOAWallets {
   COINBASE_WALLET = "com.coinbase.wallet",
   METAMASK = "io.metamask",
@@ -10,6 +18,7 @@ export type ConnectionErrorProps = {
   connectionType: "passkey" | "oauth" | "wallet" | "timeout";
   oauthProvider?: KnownAuthProvider; // TO DO: extend for BYO auth provider
   EOAConnector?: Connector | EOAWallets.WALLET_CONNECT;
+  customErrorMessage?: CustomErrorMessage | null;
   handleTryAgain?: () => void;
   handleUseAnotherMethod?: () => void;
 };
