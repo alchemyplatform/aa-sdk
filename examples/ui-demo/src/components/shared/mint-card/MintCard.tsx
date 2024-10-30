74b82fdda5bdee7166012dac9d884249fb8ed494
rob chang <robchangcodes@gmail.com>
2024-10-30 12:47:09 -0400

    fix: center text

--- examples/ui-demo/src/components/shared/mint-card/MintCard.tsx
+++ examples/ui-demo/src/components/shared/mint-card/MintCard.tsx
@@ -120,7 +120,7 @@ export const MintCard = () => {
         <div className="flex items-center flex-col justify-center mb-12 lg:mb-0  lg:flex-row lg:justify-center lg:items-start bg-bg-surface-default radius-1 border-btn-secondary border md:mx-6 lg:mx-0 overflow-hidden lg:h-[470px]">
           <RenderUserConnectionAvatar className="lg:hidden w-full p-6 mb-0 pb-6 relative after:absolute after:bottom-0 after:left-6 after:right-6  after:h-[1px] after:bg-border" />
           <div className="hidden lg:block max-w-[410px] overflow-auto p-8 h-full">
-            <h1 className="text-3xl lg:text-2xl font-semibold  leading-10 mb-8 text-fg-primary">
+            <h1 className="text-3xl lg:text-2xl font-semibold text-center leading-10 mb-8 text-fg-primary">
               {!nftTransfered
                 ? "One-click checkout"
                 : "You collected your NFT!"}
