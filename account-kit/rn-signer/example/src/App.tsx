ad179773f64228d141e463578705b1f7a5ef0f18
moldy <michael.moldoveanu@alchemy.com>
2024-11-04 16:45:22 -0500

    chore(rn): create example from template

---
+++ account-kit/rn-signer/example/src/App.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, View, Text } from "react-native";
+import { multiply } from "@account-kit/react-native-signer";
+
+const result = multiply(3, 7);
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
---
+++ account-kit/rn-signer/react-native.config.js
@@ -0,0 +1,11 @@
+/**
+ */
+module.exports = {
+  dependency: {
+    platforms: {
+      android: {
+        cmakeListsPath: "generated/jni/CMakeLists.txt",
+      },
+    },
+  },
+};
