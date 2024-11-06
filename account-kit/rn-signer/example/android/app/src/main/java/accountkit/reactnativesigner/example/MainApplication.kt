ee03d628c8cc19b48d83e3f9004663daeed840eb
moldy <michael.moldoveanu@alchemy.com>
2024-11-06 01:52:57 -0500

    fix(rn): get login working e2e

--- account-kit/rn-signer/example/android/app/src/main/java/accountkit/reactnativesigner/example/MainApplication.kt
+++ account-kit/rn-signer/example/android/app/src/main/java/accountkit/reactnativesigner/example/MainApplication.kt
@@ -1,6 +1,9 @@
 package accountkit.reactnativesigner.example
 
 import android.app.Application
+import com.accountkit.reactnativesigner.NativeTEKStamperModule
+import com.accountkit.reactnativesigner.NativeTEKStamperSpec
+import com.accountkit.reactnativesigner.ReactNativeSignerPackage
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -15,20 +18,21 @@ import com.facebook.soloader.SoLoader
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
-            }
+          object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                    PackageList(this).packages.apply {
+                      // Packages that cannot be autolinked yet can be added manually here, for
+                      // example:
+                       add(ReactNativeSignerPackage())
+                    }
 
-        override fun getJSMainModuleName(): String = "index"
+            override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+          }
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
