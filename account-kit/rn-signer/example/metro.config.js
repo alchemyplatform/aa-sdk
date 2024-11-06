5392295b1752391236da6a00fd4aa397ca77e888
moldy <michael.moldoveanu@alchemy.com>
2024-11-06 16:46:13 -0500

    refactor(rn): clean things up so they're more digestable

--- account-kit/rn-signer/example/metro.config.js
+++ account-kit/rn-signer/example/metro.config.js
@@ -4,14 +4,19 @@ const { getConfig } = require("react-native-builder-bob/metro-config");
 const pkg = require("../package.json");
 
 const root = path.resolve(__dirname, "..");
+// handles the hoisted modules
+const repoRoot = path.resolve(__dirname, "../../../node_modules");
 
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
  */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
-});
+module.exports = {
+  ...getConfig(getDefaultConfig(__dirname), {
+    root,
+    pkg,
+    project: __dirname,
+  }),
+  watchFolders: [root, repoRoot],
+};
