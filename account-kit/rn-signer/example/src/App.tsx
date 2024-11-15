59c3f4ebe1b3c63976e673b5d42bceccfc9e3f5a
Iyk Azorji <iykazorji@gmail.com>
2024-11-15 13:20:51 -0500

    feat: add a global polyfill for react-native-mmkv

--- account-kit/rn-signer/example/src/App.tsx
+++ account-kit/rn-signer/example/src/App.tsx
@@ -14,6 +14,11 @@ export default function App() {
     connection: { apiKey: Config.API_KEY! },
   });
 
+  // Test the localstorage polyfill
+  localStorage.setItem("test", "This is from localstorage polyfill");
+
+  const test = localStorage.getItem("test");
+
   return (
     <View style={styles.container}>
       <Text>User: {user ? JSON.stringify(user) : "none"}</Text>
@@ -50,6 +55,7 @@ export default function App() {
             .catch(console.error);
         }}
       ></Button>
+      <Text>{test}</Text>
     </View>
   );
 }
---
+++ account-kit/rn-signer/src/utils/mmkv-localstorage-polyfill.ts
@@ -0,0 +1,41 @@
+import { MMKV } from "react-native-mmkv";
+
+class MMKVLocalStorage {
+  private storage;
+  constructor() {
+    this.storage = new MMKV({ id: "mmkv-localstorage" });
+  }
+
+  // implement the Storage interface
+  getItem(key: string) {
+    return this.storage.getString(key) ?? null;
+  }
+
+  setItem(key: string, value: string) {
+    return this.storage.set(key, value);
+  }
+
+  removeItem(key: string) {
+    if (this.storage.contains(key)) {
+      this.storage.delete(key);
+    }
+  }
+
+  clear() {
+    this.storage.clearAll();
+  }
+
+  get length() {
+    return this.storage.getAllKeys().length;
+  }
+
+  key(index: number) {
+    const keys = this.storage.getAllKeys();
+    return keys[index] || null;
+  }
+}
+
+if (typeof window !== "undefined" && !global.localStorage) {
+  const mmkvLocalStorage = new MMKVLocalStorage();
+  global.localStorage = mmkvLocalStorage;
+}
