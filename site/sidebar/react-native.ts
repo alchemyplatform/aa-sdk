a71dfc59bdf563d7ceffa80e1fce7b9d8ebc6575
Iyk Azorji <iykazorji@gmail.com>
2024-11-28 12:58:17 -0500

    feat: init commit

---
+++ site/sidebar/react-native.ts
@@ -0,0 +1,6 @@
+import { SidebarItem } from "vocs";
+
+export const reactNativeSidebar: SidebarItem[] = [
+	{ text: "Overview", link: "/react-native/overview" },
+	{ text: "Getting Started", link: "/react-native/getting-started" },
+];
