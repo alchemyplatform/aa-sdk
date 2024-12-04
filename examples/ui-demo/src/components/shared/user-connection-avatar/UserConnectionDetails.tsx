41542c03c173879f5ccd7a6db80359324317c165
rob chang <robchangcodes@gmail.com>
2024-12-04 11:46:36 -0500

    fix: revert signer removal

--- examples/ui-demo/src/components/shared/user-connection-avatar/UserConnectionDetails.tsx
+++ examples/ui-demo/src/components/shared/user-connection-avatar/UserConnectionDetails.tsx
@@ -44,7 +44,22 @@ export function UserConnectionDetails({
           </span>
           <UserAddressLink address={user?.address} />
         </div>
-
+        {/* Signer */}
+        <div className="flex flex-row justify-between items-center mt-[17px]">
+          <a
+            target="_blank"
+            href="https://accountkit.alchemy.com/concepts/smart-account-signer"
+            className="flex justify-center items-center"
+          >
+            <span className="text-md md:text-sm text-fg-secondary mr-1">
+              Signer
+            </span>
+            <div className="flex flex-row justify-center items-center w-[14px] h-[14px] ml-1">
+              <ExternalLinkIcon className="stroke-fg-secondary" />
+            </div>
+          </a>
+          <UserAddressLink address={signerAddress} />
+        </div>
         {/* Logout */}
         <button
           className="flex flex-row justify-start items-center mt-[17px] hover:cursor-pointer active:opacity-70"
