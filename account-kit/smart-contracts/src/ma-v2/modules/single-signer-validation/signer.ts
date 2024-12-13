3e3dbd6660100fa40aed771c60854b469ffe0263
Linna <38363056+linnall@users.noreply.github.com>
2024-12-13 18:00:41 -0500

    feat: add mav2 signer (#1203)
    * fix: rn stamper was returning stamp instead of response (#1201)
    
    * chore(release): publish v4.6.1 [skip-ci]
    
    * feat: add react-native-bare implementation (#1202)
    
    * feat: add react-native-bare implementation
    
    * fix: fix linting
    
    * fix: fix linting
    
    * fix: remove unnecessary console.log statements
    
    * feat: add signer and account addresses to user logged in screen
    
    * fix: updates facebook icon used in ui component (#1196)
    
    * feat: add mav2 signer (#1203)
    
    # Pull Request Checklist
    
    - [ ] Did you add new tests and confirm existing tests pass? (`yarn test`)
    - [ ] Did you update relevant docs? (docs are found in the `site` folder, and guidelines for updating/adding docs can be found in the [contribution guide](https://github.com/alchemyplatform/aa-sdk/blob/main/CONTRIBUTING.md))
    - [ ] Do your commits follow the [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) standard?
    - [ ] Does your PR title also follow the [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/) standard?
    - [ ] If you have a breaking change, is it [correctly reflected in your commit message](https://www.conventionalcommits.org/en/v1.0.0/#examples)? (e.g. `feat!: breaking change`)
    - [ ] Did you run lint (`yarn lint:check`) and fix any issues? (`yarn lint:write`)
    - [ ] Did you follow the [contribution guidelines](https://github.com/alchemyplatform/aa-sdk/blob/main/CONTRIBUTING.md)?
    
    <!-- start pr-codex -->
    
    ---
    
    ## PR-Codex overview
    This PR introduces a new `SingleSignerValidation` module and related utilities for signature packing and handling user operation signing. It enhances the smart contract framework by implementing various functions and defining the ABI for the module.
    
    ### Detailed summary
    - Added `PackSignatureParams` type and `packSignature` utility in `utils.ts`.
    - Created `SingleSignerValidationModule` with metadata and encoding functions in `single-signer-validation/module.ts`.
    - Implemented `singleSignerMessageSigner` function in `single-signer-validation/signer.ts` for signature management.
    - Defined the ABI for `singleSignerValidation` in `abis/singleSignerValidation.ts`.
    - Added `MAV2FactoryAbi` and `smaV2Abi` with functions and events related to account management in respective ABI files.
    
    > ✨ Ask PR-Codex anything about this PR by commenting with `/codex {your question}`
    
    <!-- end pr-codex -->
    
    ---------
    
    Co-authored-by: Michael Moldoveanu <michael.moldoveanu@alchemy.com>
    Co-authored-by: Alchemy Bot <alchemy-bot@alchemy.com>
    Co-authored-by: Iyk Azorji <iykazorji@gmail.com>

---
+++ account-kit/smart-contracts/src/ma-v2/modules/single-signer-validation/signer.ts
@@ -0,0 +1,74 @@
+import type { SmartAccountSigner } from "@aa-sdk/core";
+import {
+  hashMessage,
+  hashTypedData,
+  type Hex,
+  type SignableMessage,
+  type TypedData,
+  type TypedDataDefinition,
+} from "viem";
+
+import { packSignature } from "../../utils.js";
+/**
+ * Creates an object with methods for generating a dummy signature, signing user operation hashes, signing messages, and signing typed data.
+ *
+ * @example
+ * ```ts
+ * import { singleSignerMessageSigner } from "@account-kit/smart-contracts";
+ * import { LocalAccountSigner } from "@aa-sdk/core";
+ *
+ * const MNEMONIC = "...":
+ *
+ * const signer = LocalAccountSigner.mnemonicToAccountSigner(MNEMONIC);
+ *
+ * const messageSigner = singleSignerMessageSigner(signer, chain);
+ * ```
+ *
+ * @param {TSigner} signer the signer to use for signing operations
+ * @returns {object} an object with methods for signing operations and managing signatures
+ */
+export const singleSignerMessageSigner = <TSigner extends SmartAccountSigner>(
+  signer: TSigner
+) => {
+  return {
+    getDummySignature: (): Hex => {
+      const dummyEcdsaSignature =
+        "0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c";
+
+      return packSignature({
+        // orderedHookData: [],
+        validationSignature: dummyEcdsaSignature,
+      });
+    },
+
+    signUserOperationHash: (uoHash: `0x${string}`): Promise<`0x${string}`> => {
+      return signer.signMessage({ raw: uoHash }).then((signature: Hex) =>
+        packSignature({
+          // orderedHookData: [],
+          validationSignature: signature,
+        })
+      );
+    },
+
+    // TODO: we can't implement these methods yet, because the RI at `alpha.0` doesn't have a wrapping type,
+    // and viem doesn't support raw signing, only via EIP-191 or EIP-712.
+    // When we do implement this, we need to prefix the data with the validation module address & entityId.
+
+    signMessage({
+      message,
+    }: {
+      message: SignableMessage;
+    }): Promise<`0x${string}`> {
+      return signer.signMessage({ raw: hashMessage(message) });
+    },
+
+    signTypedData: <
+      const typedData extends TypedData | Record<string, unknown>,
+      primaryType extends keyof typedData | "EIP712Domain" = keyof typedData
+    >(
+      typedDataDefinition: TypedDataDefinition<typedData, primaryType>
+    ): Promise<Hex> => {
+      return signer.signMessage({ raw: hashTypedData(typedDataDefinition) });
+    },
+  };
+};
