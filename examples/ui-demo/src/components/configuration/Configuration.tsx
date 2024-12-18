4549fbc934bfb02101968d314d48ab925b14d653
adam <adam@alchemy.com>
2024-12-18 17:03:56 -0500

    feat: 7702 ui demo
    feat: update 7702 mint flow to actually use 7702
    
    feat: partial recurring transfer support
    
    feat: ui progress
    
    feat: react state management
    
    feat: add permission hooks to session key
    
    fix: add templates to demo build
    
    fix: declare env var in turbo
    
    feat: ui demo user card reflects 7702 delegation status (#1294)
    
    * feat: have user avatar reflect if account has been delegated
    
    * feat: reflects delegation status in user details card
    
    * fix: forces delegation status query to refetch until available
    
    * chore: updates comment
    
    * refactor: uses viem public client and refetches delegation addr when nftTransferred status changes
    
    * refactor: uses defined url constant
    
    * chore: removes unnecessary check
    
    fix: coalesce undefined code result
    
    fix: forces delegation query to refetch until available
    
    feat: updated 7702 info blurb
    
    chore: cleanup

---
+++ examples/ui-demo/src/components/configuration/Configuration.tsx
@@ -0,0 +1,53 @@
+// import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+import { SettingsIcon } from "../icons/settings";
+// import { HelpTooltip } from "../shared/HelpTooltip";
+import { WalletTypeSwitch } from "../shared/WalletTypeSwitch";
+import ExternalLink from "../shared/ExternalLink";
+import { useConfigStore } from "@/state";
+import { WalletTypes } from "@/app/config";
+
+export const Configuration = ({ className }: { className?: string }) => {
+  const { setWalletType, walletType } = useConfigStore();
+  // const [walletType, setWalletType] = useState(WalletTypes.smart);
+
+  const onSwitchWalletType = () => {
+    setWalletType(
+      walletType === WalletTypes.smart
+        ? WalletTypes.hybrid7702
+        : WalletTypes.smart
+    );
+  };
+
+  return (
+    <div className={cn("flex flex-col", className)}>
+      <div className="flex flex-row gap-2 items-center pb-5">
+        <SettingsIcon />
+        <span className="font-semibold">Configuration</span>
+      </div>
+      <div className="flex flex-row items-center gap-2 pb-2">
+        <h4 className="font-normal text-sm text-secondary-foreground">
+          Embedded Wallet Type
+        </h4>
+        {/* <HelpTooltip text="An account powered by a smart contract to enable more features. Not an EOA. Recommended for new wallets." /> */}
+      </div>
+      <WalletTypeSwitch
+        id="theme-switch"
+        checked={walletType === WalletTypes.hybrid7702}
+        onCheckedChange={onSwitchWalletType}
+      />
+      <p className="text-active text-xs font-normal pt-3">
+        EIP-7702 lets you give smart account features to your EOA wallet.
+        Curious about what this means?{" "}
+        <ExternalLink
+          className="text-[#363FF9] whitespace-nowrap"
+          href="https://eips.ethereum.org/EIPS/eip-7702"
+        >
+          Learn more.
+        </ExternalLink>
+      </p>
+      <div className="w-full border-b border-border pb-5 mb-8" />
+    </div>
+  );
+};
---
+++ examples/ui-demo/src/hooks/7702/useSma7702Client.ts
@@ -0,0 +1,103 @@
+import { LocalAccountSigner } from "@aa-sdk/core";
+import type { AlchemyWebSigner } from "@account-kit/signer";
+import { useSigner } from "@account-kit/react";
+import { useState, useEffect } from "react";
+import {
+  createSMA7702AccountClient,
+  type InstallValidationActions,
+  installValidationActions,
+  SMA7702AccountClient,
+} from "@account-kit/smart-contracts/experimental";
+import { odyssey, splitOdysseyTransport } from "./transportSetup";
+import {
+  alchemyFeeEstimator,
+  alchemyGasManagerMiddleware,
+} from "@account-kit/infra";
+import { type Hex, type Address, PrivateKeyAccount } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+
+type Client = SMA7702AccountClient<
+  AlchemyWebSigner | LocalAccountSigner<PrivateKeyAccount>
+> &
+  InstallValidationActions<
+    AlchemyWebSigner | LocalAccountSigner<PrivateKeyAccount>
+  >;
+
+export const useSma7702Client = (
+  localKeyOverride?:
+    | {
+        readonly key: Hex;
+        readonly entityId: number;
+        readonly accountAddress?: Address;
+      }
+    | undefined
+): Client | undefined => {
+  const signer = useSigner();
+
+  const [client, setClient] = useState<Client | undefined>(undefined);
+
+  // Must destructure the inner fields to use as dependencies in the useEffect hook, otherwise the object reference will be compared and cause an infinite render loop
+  const { key, entityId, accountAddress } = localKeyOverride ?? {};
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const init = async () => {
+      if (!signer) {
+        return;
+      }
+
+      if (key != null && accountAddress == null) {
+        // We have an override present but don't have the account to set it for, so leave the client as undefined until we get the account address override.
+        return;
+      }
+
+      try {
+        const client = (
+          await createSMA7702AccountClient({
+            chain: odyssey,
+            transport: splitOdysseyTransport,
+            accountAddress,
+            signer:
+              key != null
+                ? new LocalAccountSigner(privateKeyToAccount(key))
+                : signer,
+            signerEntity:
+              entityId != null
+                ? {
+                    isGlobalValidation: false,
+                    entityId,
+                  }
+                : undefined,
+            feeEstimator: alchemyFeeEstimator(splitOdysseyTransport),
+            ...alchemyGasManagerMiddleware(
+              process.env.NEXT_PUBLIC_PAYMASTER_POLICY_ID!
+            ),
+          })
+        ).extend(installValidationActions);
+
+        if (!isMounted) {
+          return;
+        }
+
+        setClient(client);
+      } catch (e) {
+        console.error(e);
+
+        if (!isMounted) {
+          return;
+        }
+
+        setClient(undefined);
+      }
+    };
+
+    init();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [signer, key, entityId, accountAddress]);
+
+  return client;
+};
