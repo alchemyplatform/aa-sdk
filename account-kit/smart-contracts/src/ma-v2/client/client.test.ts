3136edccc4ccbe35ead556bd6845f576cabc4349
howydev <132113803+howydev@users.noreply.github.com>
2024-12-18 14:58:00 -0500

    fix: review fixes

--- account-kit/smart-contracts/src/ma-v2/client/client.test.ts
+++ account-kit/smart-contracts/src/ma-v2/client/client.test.ts
@@ -64,7 +64,7 @@ describe("MA v2 Tests", async () => {
 
     await provider.waitForUserOperationTransaction(result);
 
-    await expect(await getTargetBalance()).toEqual(
+    await expect(getTargetBalance()).resolves.toEqual(
       startingAddressBalance + sendAmount
     );
   });
@@ -122,7 +122,7 @@ describe("MA v2 Tests", async () => {
 
     txnHash = sessionKeyClient.waitForUserOperationTransaction(result);
     await expect(txnHash).resolves.not.toThrowError();
-    await expect(await getTargetBalance()).toEqual(
+    await expect(getTargetBalance()).resolves.toEqual(
       startingAddressBalance + sendAmount
     );
   });
