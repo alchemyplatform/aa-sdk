9e51cbe6459abd945620c055684c22d4f2dc9b11
Caleb Briancesco <caleb.briancesco@alchemy.com>
2024-12-18 22:52:02 -0600

    fix: remove un-used dependency

--- account-kit/react/src/components/auth/card/loading/otp.tsx
+++ account-kit/react/src/components/auth/card/loading/otp.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { EmailIllustration } from "../../../../icons/illustrations/email.js";
 import { ls } from "../../../../strings.js";
 import {
