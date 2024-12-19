4e9460866013389537f24a1c7eb801ecebe45740
Caleb Briancesco <caleb.briancesco@alchemy.com>
2024-12-19 13:17:19 -0600

    fix: make input disable when code is verified

--- account-kit/react/src/components/otp-input/otp-input.tsx
+++ account-kit/react/src/components/otp-input/otp-input.tsx
@@ -158,7 +158,7 @@ export const OTPInput: React.FC<OTPInputProps> = ({
             onInput={focusNextElement}
             onKeyDown={handleKeydown}
             key={i}
-            disabled={disabled}
+            disabled={disabled || isVerified}
             value={value[i]}
             aria-invalid={!!errorText}
           />
