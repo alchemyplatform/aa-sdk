8cef354f4986a68dbbf5abbf47e379cb6d2f05bc
Caleb Briancesco <caleb.briancesco@alchemy.com>
2024-12-19 14:58:08 -0600

    feat: add wallet type store and toggle cards

--- examples/ui-demo/src/components/configuration/Configuration.tsx
+++ examples/ui-demo/src/components/configuration/Configuration.tsx
@@ -1,22 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { cn } from "@/lib/utils";
 
 import { SettingsIcon } from "../icons/settings";
 // import { HelpTooltip } from "../shared/HelpTooltip";
 import { WalletTypeSwitch } from "../shared/WalletTypeSwitch";
 import ExternalLink from "../shared/ExternalLink";
+import { useConfigStore } from "@/state";
+import { WalletTypes } from "@/app/config";
 
-enum WalletTypes {
-  smart = "smart",
-  smart7702 = "7702",
-}
 export const Configuration = ({ className }: { className?: string }) => {
-  const [walletType, setWalletType] = useState(WalletTypes.smart);
+  const { setWalletType, walletType } = useConfigStore(
+    ({ walletType, setWalletType }) => {
+      return {
+        walletType,
+        setWalletType,
+      };
+    }
+  );
+  // const [walletType, setWalletType] = useState(WalletTypes.smart);
 
   const onSwitchWalletType = () => {
     setWalletType(
       walletType === WalletTypes.smart
-        ? WalletTypes.smart7702
+        ? WalletTypes.hybrid7702
         : WalletTypes.smart
     );
   };
@@ -29,18 +35,18 @@ export const Configuration = ({ className }: { className?: string }) => {
       </div>
       <div className="flex flex-row items-center gap-2 pb-2">
         <h4 className="font-normal text-sm text-secondary-foreground">
-          Embedded Wallet Type{" "}
+          Embedded Wallet Type
         </h4>
         {/* <HelpTooltip text="An account powered by a smart contract to enable more features. Not an EOA. Recommended for new wallets." /> */}
       </div>
       <WalletTypeSwitch
         id="theme-switch"
-        checked={walletType === WalletTypes.smart7702}
+        checked={walletType === WalletTypes.hybrid7702}
         onCheckedChange={onSwitchWalletType}
       />
       <p className="text-active text-xs font-medium pt-3">
-        On sign up, create a smart account or create an EOA that delegates to a
-        smart account.{" "}
+        Sentence describing all of the value props fo 7702 and educating the
+        user. Curious about what this means?
         <ExternalLink className="text-[#363FF9]" href="https://google.com">
           Learn more.
         </ExternalLink>
