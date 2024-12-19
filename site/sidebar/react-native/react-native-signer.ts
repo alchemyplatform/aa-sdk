be380a0896f9399961e2f5ccf9318290de6ac33c
Iyk Azorji <iykazorji@gmail.com>
2024-12-19 17:35:55 -0500

    feat: add otp and magic link example updates to the documentation

--- site/sidebar/react-native/react-native-signer.ts
+++ site/sidebar/react-native/react-native-signer.ts
@@ -1,16 +1,18 @@
 import { SidebarItem } from "vocs";
+import { authenticatingUsersSidebar } from "./authenticating-users.js";
 
 export const reactNativeSignerSidebar: SidebarItem[] = [
-  {
-    text: "Overview",
-    link: "/react-native/signer/overview",
-  },
-  {
-    text: "Setup",
-    link: "/react-native/signer/setup-guide",
-  },
-  {
-    text: "Authenticating Users",
-    link: "/react-native/signer/authenticating-users",
-  },
+	{
+		text: "Overview",
+		link: "/react-native/signer/overview",
+	},
+	{
+		text: "Setup",
+		link: "/react-native/signer/setup-guide",
+	},
+	{
+		text: "Authenticating Users",
+		items: authenticatingUsersSidebar,
+		collapsed: true,
+	},
 ];
