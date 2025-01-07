1624defcd72b74b2272bff5ea40db93bc5cd4117
Iyk Azorji <iykazorji@gmail.com>
2025-01-07 13:00:35 -0500

    feat: add otp and magic link example updates to the documentation (#1247)
    * feat: add otp and magic link example updates to the documentation
    
    * fix: update dependencies and linting
    
    * fix: update documentation changes
    
    * Revert "fix: update documentation changes"
    
    This reverts commit bbd1a7bb70a04f1ebf204a5ca031bb304c231dea.

---
+++ site/sidebar/react-native/authenticating-users.tsx
@@ -0,0 +1,12 @@
+import { SidebarItem } from "vocs";
+
+export const authenticatingUsersSidebar: SidebarItem[] = [
+  {
+    text: "Authenticating Users via Magic Link",
+    link: "/react-native/signer/authenticating-users/authenticating-with-magic-link",
+  },
+  {
+    text: "Authenticating Users via One-Time Password (OTP)",
+    link: "/react-native/signer/authenticating-users/authenticating-with-otp",
+  },
+];
