4d2da6ae9724d53ff35b853fe12b30e1a1cdff1a
adam <adam@alchemy.com>
2025-01-09 10:49:04 -0500

    feat: 7702 progress

--- aa-sdk/core/src/index.ts
+++ aa-sdk/core/src/index.ts
@@ -92,6 +92,8 @@ export {
 } from "./errors/useroperation.js";
 export { LogLevel, Logger } from "./logger.js";
 export { middlewareActions } from "./middleware/actions.js";
+export { default7702UserOpSigner } from "./middleware/defaults/7702signer.js";
+export { default7702GasEstimator } from "./middleware/defaults/7702gasEstimator.js";
 export { defaultFeeEstimator } from "./middleware/defaults/feeEstimator.js";
 export { defaultGasEstimator } from "./middleware/defaults/gasEstimator.js";
 export { defaultPaymasterAndData } from "./middleware/defaults/paymasterAndData.js";
