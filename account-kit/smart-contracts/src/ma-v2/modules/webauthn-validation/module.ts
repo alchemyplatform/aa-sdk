231b6004f001b53c60a8099dae1f8da7dee3c199
linna <linna@alchemy.com>
2025-01-30 19:18:48 -0500

    feat: adds WebAuthnValidationModule definition

---
+++ account-kit/smart-contracts/src/ma-v2/modules/webauthn-validation/module.ts
@@ -0,0 +1,28 @@
+import { encodeAbiParameters, type Hex } from "viem";
+
+export const WebAuthnValidationModule = {
+  encodeOnInstallData: (args: {
+    entityId: number;
+    x: bigint;
+    y: bigint;
+  }): Hex => {
+    const { entityId, x, y } = args;
+    return encodeAbiParameters(
+      [{ type: "uint32" }, { type: "uint256" }, { type: "uint256" }],
+      [entityId, x, y]
+    );
+  },
+
+  encodeOnUninstallData: (args: { entityId: number }): Hex => {
+    const { entityId } = args;
+
+    return encodeAbiParameters(
+      [
+        {
+          type: "uint32",
+        },
+      ],
+      [entityId]
+    );
+  },
+};
