88d0dc06a83db06ea99e00ae76029c666c556948
aashkrishnan <ash@alchemy.com>
2025-02-04 14:13:45 -0500

    Update useBundlerClient.ts

--- account-kit/react/src/hooks/useBundlerClient.ts
+++ account-kit/react/src/hooks/useBundlerClient.ts
@@ -9,7 +9,7 @@ export type UseBundlerClientResult = ClientWithAlchemyMethods;
 
 /**
  * Custom [hook](https://github.com/alchemyplatform/aa-sdk/blob/main/account-kit/react/src/hooks/useBundlerClient.ts) to get a bundler client using the Alchemy account context.
- * It uses `useSyncExternalStore` to watch for any changes in the bundler client configuration and provides the updated bundler client. React hooks don’t manage their own states, so they depend on an external store other than manage state, such as `useSyncExternalStore`. useBundlerClient’s only job is to call the bundler JSON RPC methods directly; it does not do additional processing, unlike useSmartAccountClient. For example, if you call sendUserOperation, it expects a fully formed user operation.
+ * It uses `useSyncExternalStore` to watch for any changes in the bundler client configuration and provides the updated bundler client. React hooks don’t handle their own state management directly, so they rely on external stores, like `useSyncExternalStore`, to manage state.. useBundlerClient’s only job is to call the bundler JSON RPC methods directly; it does not do additional processing, unlike useSmartAccountClient. For example, if you call sendUserOperation, it expects a fully formed user operation.
  * It is an extension of [Viem’s Public Client](https://viem.sh/docs/clients/public) and provides access to public actions, talking to public RPC APIs like getBlock, eth_call, etc. It does not require an account as context.
  * Use cases: connecting with a EOA or checking for gas eligibility.
  *
