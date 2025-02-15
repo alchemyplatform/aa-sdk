ae55c2b3893fffff1de0b18182f8918dcfe24e0e
jakehobbs <jacob.hobbs@alchemy.com>
2025-02-14 16:19:32 -0800

    fix: add link to explorer from smart account address

--- examples/ui-demo/src/components/small-cards/Wrapper.tsx
+++ examples/ui-demo/src/components/small-cards/Wrapper.tsx
@@ -5,9 +5,22 @@ import {
   TransactionsCard7702,
   TransactionsCardDefault,
 } from "./TransactionsCard";
+import { useChain } from "@account-kit/react";
+import { useEffect } from "react";
+import { arbitrumSepolia } from "@account-kit/infra";
+import { odyssey } from "@/hooks/7702/transportSetup";
 
 export const SmallCardsWrapper = () => {
   const { walletType } = useConfigStore();
+
+  const chain = walletType === WalletTypes.smart ? arbitrumSepolia : odyssey;
+  const { chain: activeChain, setChain, isSettingChain } = useChain();
+  useEffect(() => {
+    if (isSettingChain || chain.id === activeChain.id) {
+      return;
+    }
+    setChain({ chain });
+  }, [activeChain.id, chain, isSettingChain, setChain]);
 
   return (
     <div className="flex flex-col xl:flex-row gap-6 lg:mt-6 items-center p-6">
