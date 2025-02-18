18a4f43df3e4bad38eb2db4b496b3d31abe0e968
Iyk Azorji <iykazorji@gmail.com>
2025-02-18 12:33:37 -0500

    fix: fix window polyfill failing build

--- account-kit/rn-signer/src/utils/polyfills/window.ts
+++ account-kit/rn-signer/src/utils/polyfills/window.ts
@@ -1,57 +1,55 @@
 // window-polyfill.ts
 
-type EventCallback = (event?: any) => void;
+if (typeof global !== "undefined" && typeof global.window === "undefined") {
+  type EventCallback = (event?: any) => void;
 
-const eventListeners: Record<string, EventCallback[]> = {};
+  const eventListeners: Record<string, EventCallback[]> = {};
 
-const windowPolyfill: Partial<Window> = {
-  document: {
-    createElement: () => ({ style: {} }),
-  } as unknown as Document,
+  const windowPolyfill: Partial<Window> = {
+    document: {
+      createElement: () => ({ style: {} }),
+    } as unknown as Document,
 
-  location: {
-    href: "",
-  } as Location,
+    location: {
+      href: "",
+    } as Location,
 
-  requestAnimationFrame: (callback: FrameRequestCallback) =>
-    setTimeout(callback, 16),
-  cancelAnimationFrame: (id: number) => clearTimeout(id),
+    requestAnimationFrame: (callback: FrameRequestCallback) =>
+      setTimeout(callback, 16),
+    cancelAnimationFrame: (id: number) => clearTimeout(id),
 
-  setTimeout: global.setTimeout,
-  clearTimeout: global.clearTimeout,
-  setInterval: global.setInterval,
-  clearInterval: global.clearInterval,
+    setTimeout: global.setTimeout,
+    clearTimeout: global.clearTimeout,
+    setInterval: global.setInterval,
+    clearInterval: global.clearInterval,
 
-  localStorage: {
-    getItem: async (_: string) => null,
-    setItem: async (_: string, __: string) => {},
-    removeItem: async (_: string) => {},
-    clear: async () => {},
-  } as unknown as Storage,
+    fetch: global.fetch,
 
-  addEventListener: (type: string, callback: EventCallback) => {
-    if (!eventListeners[type]) {
-      eventListeners[type] = [];
-    }
-    eventListeners[type].push(callback);
-  },
+    addEventListener: (type: string, callback: EventCallback) => {
+      if (!eventListeners[type]) {
+        eventListeners[type] = [];
+      }
+      eventListeners[type].push(callback);
+    },
 
-  removeEventListener: (type: string, callback: EventCallback) => {
-    if (eventListeners[type]) {
-      eventListeners[type] = eventListeners[type].filter(
-        (cb) => cb !== callback
-      );
-    }
-  },
+    removeEventListener: (type: string, callback: EventCallback) => {
+      if (eventListeners[type]) {
+        eventListeners[type] = eventListeners[type].filter(
+          (cb) => cb !== callback
+        );
+      }
+    },
 
-  dispatchEvent: (event: { type: string; [key: string]: any }) => {
-    if (eventListeners[event.type]) {
-      eventListeners[event.type]?.forEach((callback) => callback(event));
-    }
+    dispatchEvent: (event: { type: string; [key: string]: any }) => {
+      if (eventListeners[event.type]) {
+        eventListeners[event.type]?.forEach((callback) => callback(event));
+      }
+      return true;
+    },
+  };
 
-    return true;
-  },
-};
+  // Merge the polyfill into the global object
+  global.window = windowPolyfill as Window & typeof globalThis;
+}
 
-// Merge the polyfill into the global object
-global.window = windowPolyfill as unknown as Window & typeof globalThis;
+export default global.window;
