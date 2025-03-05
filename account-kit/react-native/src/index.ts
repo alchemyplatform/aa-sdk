0207af0d3ec6f787bebefed13051d3b1067b22e4
Iyk Azorji <iykazorji@gmail.com>
2025-03-04 19:28:02 -0500

    feat: export necessary react hooks

--- account-kit/react-native/src/index.ts
+++ account-kit/react-native/src/index.ts
@@ -1,2 +1,21 @@
 import "./polyfills/index.js";
+export {
+  useAccount,
+  useAuthenticate,
+  useAuthError,
+  useBundlerClient,
+  useChain,
+  useConnection,
+  useDropAndReplaceUserOperation,
+  useExportAccount,
+  useLogout,
+  useSendUserOperation,
+  useSigner,
+  useSignerStatus,
+  useSignMessage,
+  useSignTypedData,
+  useSmartAccountClient,
+  useUser,
+  useWaitForUserOperationTransaction,
+} from "@account-kit/react/hooks";
 export * from "./context.js";
