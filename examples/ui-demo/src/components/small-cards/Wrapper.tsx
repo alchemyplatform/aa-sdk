a3ee547290105a800dd5c57e88a8a4a3c236cb22
rob chang <robchangcodes@gmail.com>
2025-03-14 16:48:35 -0400

    fix: solana card

--- examples/ui-demo/src/components/small-cards/Wrapper.tsx
+++ examples/ui-demo/src/components/small-cards/Wrapper.tsx
@@ -4,7 +4,7 @@ import { MintCard } from "./MintCard";
 
 export const SmallCardsWrapper = () => {
   return (
-    <div className="flex flex-col xl:flex-row gap-6 lg:mt-6 items-center p-6 w-full justify-center max-w-screen-sm xl:max-w-none">
+    <div className="flex flex-col xl:flex-row gap-6 lg:mt-6 items-center w-full justify-center max-w-screen-sm xl:max-w-none">
       <MintCard />
       <SolanaCard />
       <TransactionsCard />
