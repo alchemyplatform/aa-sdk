5391523a26e49d8dbc58a78a55cd105c29de69b8
rob chang <robchangcodes@gmail.com>
2025-03-14 16:27:50 -0400

    feat: transactions card

--- examples/ui-demo/src/components/small-cards/Wrapper.tsx
+++ examples/ui-demo/src/components/small-cards/Wrapper.tsx
@@ -1,29 +1,13 @@
-import { useConfigStore } from "@/state";
 import { SolanaCard } from "./SolanaCard";
-import {
-  TransactionsCard7702,
-  TransactionsCardDefault,
-} from "./TransactionsCard";
+import { TransactionsCard } from "./TransactionsCard";
 import { MintCard } from "./MintCard";
 
 export const SmallCardsWrapper = () => {
-  const { accountMode } = useConfigStore();
-  console.log({ accountMode });
   return (
     <div className="flex flex-col xl:flex-row gap-6 lg:mt-6 items-center p-6 w-full justify-center max-w-screen-sm xl:max-w-none">
-      {accountMode === "default" ? (
-        <>
-          <MintCard is7702={false} />
-          <SolanaCard />
-          <TransactionsCardDefault />
-        </>
-      ) : (
-        <>
-          <MintCard is7702={true} />
-          <SolanaCard />
-          <TransactionsCard7702 />
-        </>
-      )}
+      <MintCard />
+      <SolanaCard />
+      <TransactionsCard />
     </div>
   );
 };
