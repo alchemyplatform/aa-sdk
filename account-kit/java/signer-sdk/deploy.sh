92e7b7114b8a577b276cef930e50ab1596d7b593
Pengfei(Andy) Zhang <andy@alchemy.com>
2025-03-17 16:27:14 -0400

    feat: add shell script to bump version

--- account-kit/java/signer-sdk/deploy.sh
+++ account-kit/java/signer-sdk/deploy.sh
@@ -1,1 +1,16 @@
-mvn clean deploy -Dproject.version=1.1.0-alpha-$(git rev-parse --short HEAD)
+#!/bin/sh
+
+FULL_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo $FULL_VERSION
+if [[ "$FULL_VERSION" =~ ^([0-9]+\.[0-9]+\.[0-9]+)-([^-]+)-(.*)$ ]]; then
+  version="${BASH_REMATCH[1]}"    # => 0.1.0
+  qualifier="${BASH_REMATCH[2]}"  # => -alpha
+  gitsha="$(git rev-parse --short HEAD)"
+
+  echo "version:   $version"
+  echo "qualifier: $qualifier"
+  echo "gitsha:    $gitsha"
+fi
+
+newVersion=${version}-${qualifier}-${gitsha}
+mvn versions:set -DnewVersion="$newVersion"
