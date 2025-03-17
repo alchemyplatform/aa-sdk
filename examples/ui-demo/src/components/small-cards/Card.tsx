448e2fa055e20ae2988f752a3da7982e32c2b071
rob chang <robchangcodes@gmail.com>
2025-03-17 17:00:40 -0400

    refactor: success state

--- examples/ui-demo/src/components/small-cards/Card.tsx
+++ examples/ui-demo/src/components/small-cards/Card.tsx
@@ -12,7 +12,7 @@ export function Card({
   badgeSlot?: React.ReactNode;
 }) {
   return (
-    <div className="bg-bg-surface-default rounded-lg p-4 w-full xl:p-6 xl:w-[326px] xl:h-[570px] flex flex-col shadow-smallCard mb-5 xl:mb-0 min-h-[220px]">
+    <div className="bg-bg-surface-default rounded-lg p-4 w-full xl:p-6 xl:w-[326px] xl:h-[570px] flex flex-col border hover:border-fg-tertiary mb-5 xl:mb-0 min-h-[220px]">
       <div className="flex xl:flex-col gap-4 relative">
         <div className="absolute top-[-6px] left-[-6px] sm:top-1 sm:left-1 xl:left-auto xl:right-3 xl:top-3 z-10">
           {badgeSlot}
