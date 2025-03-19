e21791e3f76d40c31f2b4dc1f20781a62b38c44c
Rob <30500864+RobChangCA@users.noreply.github.com>
2025-03-19 13:35:00 -0400

    refactor: card cleanup and dimensions (#1453)
    * feat: card generic component
    
    * feat: transactions card
    
    * fix: solana card
    
    * fix: dimension for small screen
    
    * refactor: 7702 reset, badge position
    
    * refactor: success state

--- examples/ui-demo/src/components/small-cards/Wrapper.tsx
+++ examples/ui-demo/src/components/small-cards/Wrapper.tsx
@@ -1,29 +1,18 @@
-import { useConfigStore } from "@/state";
-import { MintCard7702, MintCardDefault } from "./MintCard";
 import { SolanaCard } from "./SolanaCard";
-import {
-  TransactionsCard7702,
-  TransactionsCardDefault,
-} from "./TransactionsCard";
+import { TransactionsCard } from "./TransactionsCard";
+import { MintCard } from "./MintCard";
+import { useConfigStore } from "@/state";
 
 export const SmallCardsWrapper = () => {
   const { accountMode } = useConfigStore();
-
   return (
-    <div className="flex flex-col xl:flex-row gap-6 lg:mt-6 items-center p-6 w-full justify-center max-w-screen-sm xl:max-w-none">
-      {accountMode === "default" ? (
-        <>
-          <MintCardDefault />
-          <SolanaCard />
-          <TransactionsCardDefault />
-        </>
-      ) : (
-        <>
-          <MintCard7702 />
-          <SolanaCard />
-          <TransactionsCard7702 />
-        </>
-      )}
+    <div className="flex flex-col xl:flex-row gap-6 lg:mt-6 items-center w-full justify-center max-w-screen-sm xl:max-w-none">
+      <MintCard accountMode={accountMode} key={`mint-card-${accountMode}`} />
+      <TransactionsCard
+        accountMode={accountMode}
+        key={`transactions-card-${accountMode}`}
+      />
+      <SolanaCard />
     </div>
   );
 };
