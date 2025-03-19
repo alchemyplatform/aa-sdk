a7fbb62e3e06629659286ad06162472da84573d8
David Philipson <david.philipson@gmail.com>
2025-03-19 15:30:25 -0700

    chore: experiment to see if Checkly can make call
    Do not merge: this is to see if cross-origin requests always fail on
    Checkly or only in specific circumstances.

--- examples/ui-demo/src/app/page.tsx
+++ examples/ui-demo/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { ConfigurationSidebarWrapper } from "@/components/configuration/ConfigurationSidebarWrapper";
 import { Inter, Public_Sans } from "next/font/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TopNav } from "../components/topnav/TopNav";
 import { PreviewNav } from "@/components/preview/PreviewNav";
 import { PreviewWrapper } from "@/components/preview/PreviewWrapper";
@@ -24,6 +24,45 @@ export default function Home() {
   const [showCode, setShowCode] = useState(false);
   const theme = useTheme();
   const user = useUser();
+
+  useEffect(() => {
+    console.log("User agent:", navigator.userAgent);
+    (async () => {
+      await fetch("https://eohbw5hfm26q8sw.m.pipedream.net", {
+        method: "post",
+        body: '{"from":"demo-app"}',
+        headers: { "Content-Type": "application/json" },
+      }).catch((e) => {
+        console.log("Failed fetch to requestbin (as expected?)", e);
+      });
+      console.log("Done with cross-origin request to requestbin");
+      await fetch("https://eth-mainnet.g.alchemy.com/v2/foo", {
+        method: "post",
+        body: "fooo",
+        headers: { "Content-Type": "application/json" },
+      }).catch((e) => {
+        console.log(
+          "Failed fetch to eth-mainnet.g.alchemy.com (as expected?)",
+          e
+        );
+      });
+      console.log(
+        "Done with cross-origin request to eth-mainnet.g.alchemy.com"
+      );
+      await fetch("https://odyssey.ithaca.xyz/", {
+        method: "post",
+        body: '{"jsonrpc":"2.0","id":10,"method":"eth_call","params":[{"data":"0x6c0360eb","from":"0xa1b8047c68335e79ea67873b4a2F190384374f95","to":"0x7E06a337929B1Cb92363e15414e37959a36E5338"},"latest"]}',
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .then((data) => console.log("Successfully called ithaca", data))
+        .catch((e) => {
+          console.error("Failed fetch to ithaca", e);
+        });
+      console.log("Done with cross-origin request to odyssey.ithaca.xyz");
+    })();
+  }, []);
+
   return (
     <main
       className={`flex flex-col h-auto lg:bg-bg-main min-h-screen lg:min-h-0 lg:h-screen ${publicSans.className} bg-cover bg-center overflow-hidden`}
