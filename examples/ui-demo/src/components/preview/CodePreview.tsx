94dc5953ea624c908e2f37603d808e5c63166d98
jakehobbs <jacob.hobbs@alchemy.com>
2025-03-21 15:02:48 -0700

    docs: update docs for tailwind v4 (#1464)
    * docs: update docs for tailwind v4
    
    * chore: revert changes to unrelated files
    
    * docs: update docs for tw v4 in happy path
    
    * chore: update yarn.lock
    
    * chore: update comments in ui demo code preview
    
    * chore: lint

--- examples/ui-demo/src/components/preview/CodePreview.tsx
+++ examples/ui-demo/src/components/preview/CodePreview.tsx
@@ -128,7 +128,8 @@ function getTailwindCode(ui: Config["ui"]) {
   // wrap your existing tailwind config with 'withAccountKitUi'
   export default withAccountKitUi({
     // your tailwind config here
-    // docs on setting up tailwind here: https://tailwindcss.com/docs/installation/using-postcss
+    // if using tailwind v4, this can be left empty since most options are configured via css
+    // if using tailwind v3, add your existing tailwind config here - https://v3.tailwindcss.com/docs/installation/using-postcss
   }, {
     // override account kit themes
     colors: {
