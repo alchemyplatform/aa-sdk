f9e857524a921c3ae0939d2cd91e2bc24f9d7c81
jakehobbs <jacob.hobbs@alchemy.com>
2025-03-26 23:59:49 -0700

    feat: example of signing UOs on server

---
+++ examples/embedded-accounts-server-side-eoa/app/api/send-uo/route.ts
@@ -0,0 +1,161 @@
+import { TurnkeyClient } from "@turnkey/http";
+import { ApiKeyStamper } from "@turnkey/api-key-stamper";
+import { getLatestApiKey, getUser } from "@/app/db";
+import { NextResponse } from "next/server";
+import {
+  Address,
+  createWalletClient,
+  hashMessage,
+  Hex,
+  SignableMessage,
+} from "viem";
+import {
+  alchemy,
+  createAlchemySmartAccountClient,
+  sepolia,
+} from "@account-kit/infra";
+import { toAccount } from "viem/accounts";
+import { WalletClientSigner } from "@aa-sdk/core";
+import { createModularAccountV2 } from "@account-kit/smart-contracts";
+
+const TURNKEY_BASE_URL = "https://api.turnkey.com";
+const ALCHEMY_BASE_URL = "https://api.g.alchemy.com";
+
+export async function POST(request: Request) {
+  const body: { orgId: string } = await request.json().catch((err) => {
+    console.error(err);
+    return NextResponse.json(
+      { error: "bad request" },
+      {
+        status: 400,
+      },
+    );
+  });
+  if (!body.orgId) {
+    return NextResponse.json(
+      { error: "bad request" },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const user = getUser(body.orgId);
+  if (!user) {
+    return NextResponse.json(
+      { error: "user not found" },
+      {
+        status: 404,
+      },
+    );
+  }
+  const apiKey = getLatestApiKey(body.orgId);
+
+  try {
+    const stamper = new ApiKeyStamper({
+      apiPublicKey: apiKey.publicKey,
+      apiPrivateKey: apiKey.privateKey,
+    });
+    const tk = new TurnkeyClient({ baseUrl: TURNKEY_BASE_URL }, stamper);
+
+    const signMessage = async (message: SignableMessage) => {
+      const payload = hashMessage(message);
+
+      // Sign with the api key stamper first.
+      const stampedRequest = await tk.stampSignRawPayload({
+        organizationId: user.orgId,
+        timestampMs: Date.now().toString(),
+        type: "ACTIVITY_TYPE_SIGN_RAW_PAYLOAD_V2",
+        parameters: {
+          signWith: user.address,
+          payload,
+          encoding: "PAYLOAD_ENCODING_HEXADECIMAL",
+          hashFunction: "HASH_FUNCTION_NO_OP",
+        },
+      });
+
+      // Then submit to Alchemy.
+      const alchemyResp = await fetch(
+        `${ALCHEMY_BASE_URL}/signer/v1/sign-payload`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            stampedRequest,
+          }),
+        },
+      );
+      if (!alchemyResp.ok) {
+        console.error(await alchemyResp.text());
+        throw new Error("Alchemy sign request failed");
+      }
+
+      const respJson = (await alchemyResp.json()) as { signature: Hex };
+      return respJson.signature;
+    };
+
+    const signerAccount = toAccount({
+      address: user.address as Address,
+      signMessage: async ({ message }) => {
+        return signMessage(message);
+      },
+      signTransaction: async () => {
+        throw new Error("Not implemented");
+      },
+      signTypedData: async () => {
+        throw new Error("Not implemented");
+      },
+    });
+
+    const walletClient = createWalletClient({
+      account: signerAccount,
+      chain: sepolia,
+      transport: alchemy({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
+    });
+
+    const signer = new WalletClientSigner(walletClient, "custom");
+
+    const account = await createModularAccountV2({
+      transport: alchemy({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
+      chain: sepolia,
+      signer,
+    });
+
+    const client = createAlchemySmartAccountClient({
+      account,
+      chain: sepolia,
+      transport: alchemy({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
+      policyId: process.env.NEXT_PUBLIC_PAYMASTER_POLICY_ID!,
+    });
+
+    // For proof on concept, this just sends a UO w/ no value to the user's own address.
+    const { hash } = await client.sendUserOperation({
+      uo: {
+        target: user.address as Address,
+        data: "0x",
+        value: BigInt(0),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        hash,
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "error" },
+      {
+        status: 500,
+      },
+    );
+  }
+}
