1c0fc3310a5179880e4671fce55bd52f260f9edf
dslovinsky <dslovinsky@users.noreply.github.com>
2025-03-31 16:39:37 -0400

    chore: remove docs files that will need to be re-migrated

--- docs/shared/core/config.ts
+++
@@ -1,9 +0,0 @@
-import { createConfig } from "@account-kit/core";
-import { alchemy, sepolia } from "@account-kit/infra";
-
-export const config = createConfig({
-  transport: alchemy({ apiKey: "YOUR_API_KEY" }),
-  chain: sepolia,
-  // optional if you want to sponsor gas
-  policyId: "YOUR_POLICY_ID",
-});
