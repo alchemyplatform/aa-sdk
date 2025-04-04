555f39d9069dba4bbe88e7c1dfbe5fc48d451326
dslovinsky <dslovinsky@users.noreply.github.com>
2025-04-03 23:57:03 -0400

    build: add Setup Docs action to streamline documentation generation

--- fern-gen/vitest.config.ts
+++ fern-gen/vitest.config.ts
@@ -6,6 +6,6 @@ export default defineProject({
     globals: true,
     setupFiles: ["../../.vitest/setupTests.ts"],
     exclude: [...configDefaults.exclude, "**/e2e-tests/**/*.test.ts"],
-    name: "doc-gen",
+    name: "fern-gen",
   },
 });
