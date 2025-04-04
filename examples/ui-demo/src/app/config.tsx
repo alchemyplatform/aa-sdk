2210bb4a542201323696f39a78a7a6c563513807
blake duncan <blake.duncan@alchemy.com>
2025-04-04 14:10:22 -0400

    chore: remove staging config

--- examples/ui-demo/src/app/config.tsx
+++ examples/ui-demo/src/app/config.tsx
@@ -103,7 +103,6 @@ export const alchemyConfig = () =>
       ],
       storage: cookieStorage,
       enablePopupOauth: true,
-      oauthCallbackUrl: "https://signer.alchemypreview.com/callback",
     },
     {
       illustrationStyle: DEFAULT_CONFIG.ui.illustrationStyle,
