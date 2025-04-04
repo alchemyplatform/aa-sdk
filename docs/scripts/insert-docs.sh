d9ed6bd2ed2b3a0f382819bfac6ea5fb3c142f1c
Daniel Slovinsky <slovinsky1993@gmail.com>
2025-04-04 17:38:55 -0400

    feat: create workflow for generating previews and publishing docs using fern (#1500)
    * docs: generate updated reference docs
    
    * feat: create fern:gen duplicate of docs:gen
    
    * feat: remove redundant H1 in fern mdx
    
    * fix: bug causing H1 to reappear in mdx docs
    
    * chore: rename fern dir to docs-site
    
    * chore: rename docs-site to docs
    
    * build: add fern github workflows and prune vocs files from docs dir
    
    * chore: add /assets/ directory to asset paths
    
    * refactor: convert instance of TileButton to Card
    
    * refactor: convert instance of SmallTileRow to Card
    
    * refactor: change every instance of :::code-group to <CodeBlock> component
    
    * refactor: remove unnecessary brackets in filenames in MDX code snippets
    
    * refactor: convert instance of :::note to <Note> component
    
    * refactor: convert instance of :::tip to <Tip> component
    
    * refactor: convert instance of :::warning to <Warning> component
    
    * refactor: convert instance of :::info to <Info> component
    
    * refactor: convert instance of :::danger to <Error> component
    
    * refactor: convert instance of :::details to <Accordion> component
    
    * chore: remove docs files that will need to be re-migrated
    
    * revert: re-add docs/docs.yml
    
    * chore: update gen-fern-preview workflow to change directory before generating docs
    
    * chore: update gen-fern-preview workflow to output preview URL directly in PR comments
    
    * refactor: move insert-docs to its own script file
    
    * feat: update insert-docs script to handle shared components
    
    * feat: copy contents of docs into docs-site
    
    * chore: temporarily remove image from docs homepage
    
    * fix: re-add components and shared dirs
    
    * feat: add images
    
    * fix: ensure target folder exists for account kit images
    
    * fix: add placeholder doc to docs/shared to include the dir in git
    
    * refactor: capture placeholder comments in a var in insert-docs.yml
    
    * refactor: adapt insert-docs script to be reusable by both aa-sdk and docs repos in CI
    
    * fix: cd into docs-site when needed
    
    * feat: update publish-fern-docs to work like preview and add prettier preview PR comment
    
    * chore: change date formatting
    
    * docs: add readme to docs repo
    
    * docs: add sections for local dev and SDK References
    
    * docs: switch references template to use Fern's Note component
    
    * build: add Setup Docs action to streamline documentation generation
    
    * fix: re-add checkout to individual fern actions
    
    * chore: update paths to accommodate docs file needing to be in fern dir
    
    * fix: updated paths in docs/scripts/insert-docs.sh
    
    * ci: create script update docs comments instead of making new ones each time
    
    * fix: switch preview-comment to ESM
    
    * fix: add absolute path to update script
    
    * docs: add JSDoc comment for preview-comment.js
    
    * ci: attempt using checkout action instead of downloading docs tarball
    
    * refactor: add --fern flag to doc-gen and remove fern-gen
    
    ---------
    
    Co-authored-by: dslovinsky <dslovinsky@users.noreply.github.com>

---
+++ docs/scripts/insert-docs.sh
@@ -0,0 +1,26 @@
+# Move images docs/images dir to docs-site/fern/images/account-kit so they can referenced by Fern
+mkdir -p fern/images/account-kit && \
+mv fern/docs/account-kit/images/* fern/images/account-kit/
+
+# Takes the contents of docs/docs.yml and inserts its contents into the right places in docs-site/fern/docs.yml
+COMPONENTS_PLACEHOLDER="# Account Kit components are auto-generated here"
+DOCS_PLACEHOLDER="# Account Kit docs are auto-generated here"
+
+# Extract mdx-components section and insert it into the docs.yml file
+sed -n '/^  mdx-components:/,/^[^ ]/p' fern/docs/account-kit/docs.yml | sed '1d;$d' | \
+  sed -i.bak \
+    "/$COMPONENTS_PLACEHOLDER/r /dev/stdin" \
+    fern/docs.yml && \
+  sed -i.bak \
+    "/$COMPONENTS_PLACEHOLDER/d" \
+    fern/docs.yml && \
+
+# Extract navigation section and insert it into the docs.yml file
+sed -n '/^navigation:/,/^[^ ]/p' fern/docs/account-kit/docs.yml | sed '1d;$d' | \
+  sed -i.bak \
+    "/$DOCS_PLACEHOLDER/r /dev/stdin" \
+    fern/docs.yml && \
+  sed -i.bak \
+    "/$DOCS_PLACEHOLDER/d" \
+    fern/docs.yml && \
+  rm fern/docs.yml.bak
