63375c7955ec82ffe52f6a592c2af5ba6df8855c
jakehobbs <jacob.hobbs@alchemy.com>
2025-04-22 09:43:46 -0700

    chore: remove "noreferrer"

--- account-kit/react/src/components/auth/card/footer/protected-by.tsx
+++ account-kit/react/src/components/auth/card/footer/protected-by.tsx
@@ -3,10 +3,10 @@ import { ls } from "../../../../strings.js";
 
 // eslint-disable-next-line jsdoc/require-jsdoc
 export const ProtectedBy = () => (
+  // eslint-disable-next-line react/jsx-no-target-blank
   <a
     href={`https://alchemy.com?utm_source=account_kit&utm_medium=sdk&utm_campaign=auth_footer`}
     target="_blank"
-    rel="noreferrer"
     className="flex flex-row gap-1 items-center h-[14px] text-fg-disabled"
   >
     <span className="text-[11px] pt-[1px]">{ls.protectedBy.title}</span>
