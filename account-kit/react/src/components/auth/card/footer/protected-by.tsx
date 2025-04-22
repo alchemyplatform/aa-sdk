7b358fe9a29d22e74dc5cf27c2ff12a65faa9d22
jakehobbs <jacob.hobbs@alchemy.com>
2025-04-21 17:12:52 -0700

    chore: remove return statement

--- account-kit/react/src/components/auth/card/footer/protected-by.tsx
+++ account-kit/react/src/components/auth/card/footer/protected-by.tsx
@@ -2,16 +2,14 @@ import { AlchemyLogo } from "../../../../icons/alchemy.js";
 import { ls } from "../../../../strings.js";
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-export const ProtectedBy = () => {
-  return (
-    <a
-      href={`https://alchemy.com?utm_source=account_kit&utm_medium=sdk&utm_campaign=auth_footer`}
-      target="_blank"
-      rel="noreferrer"
-      className="flex flex-row gap-1 items-center h-[14px] text-fg-disabled"
-    >
-      <span className="text-[11px] pt-[1px]">{ls.protectedBy.title}</span>
-      <AlchemyLogo />
-    </a>
-  );
-};
+export const ProtectedBy = () => (
+  <a
+    href={`https://alchemy.com?utm_source=account_kit&utm_medium=sdk&utm_campaign=auth_footer`}
+    target="_blank"
+    rel="noreferrer"
+    className="flex flex-row gap-1 items-center h-[14px] text-fg-disabled"
+  >
+    <span className="text-[11px] pt-[1px]">{ls.protectedBy.title}</span>
+    <AlchemyLogo />
+  </a>
+);
