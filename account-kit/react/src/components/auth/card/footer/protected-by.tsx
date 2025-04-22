e928e71f4e505b193bb5c8af32d44e80781a15f3
jakehobbs <jacob.hobbs@alchemy.com>
2025-04-21 17:11:21 -0700

    chore: add link to auth footer

--- account-kit/react/src/components/auth/card/footer/protected-by.tsx
+++ account-kit/react/src/components/auth/card/footer/protected-by.tsx
@@ -2,9 +2,16 @@ import { AlchemyLogo } from "../../../../icons/alchemy.js";
 import { ls } from "../../../../strings.js";
 
 // eslint-disable-next-line jsdoc/require-jsdoc
-export const ProtectedBy = () => (
-  <div className="flex flex-row gap-1 items-center h-[14px] text-fg-disabled">
-    <span className="text-[11px] pt-[1px]">{ls.protectedBy.title}</span>
-    <AlchemyLogo />
-  </div>
-);
+export const ProtectedBy = () => {
+  return (
+    <a
+      href={`https://alchemy.com?utm_source=account_kit&utm_medium=sdk&utm_campaign=auth_footer`}
+      target="_blank"
+      rel="noreferrer"
+      className="flex flex-row gap-1 items-center h-[14px] text-fg-disabled"
+    >
+      <span className="text-[11px] pt-[1px]">{ls.protectedBy.title}</span>
+      <AlchemyLogo />
+    </a>
+  );
+};
