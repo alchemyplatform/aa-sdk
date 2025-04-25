27c791b847fbe928391aabad4d84c0b629ebab2a
Pengfei(Andy) Zhang <andy@alchemy.com>
2025-04-25 15:30:13 -0400

    feat: add erc20 to 7677 middleware

--- aa-sdk/core/src/middleware/erc7677middleware.ts
+++ aa-sdk/core/src/middleware/erc7677middleware.ts
@@ -54,6 +54,30 @@ export type Erc7677Client<
   TContext extends Record<string, any> = Record<string, any>
 > = Client<T, Chain, undefined, Erc7677RpcSchema<TContext>>;
 
+export type Erc20PaymasterClientRpcSchema = [
+  {
+    Method: "alchemy_requestPaymasterTokenQuote";
+    Parameters: [
+      {
+        request: {
+          tokenAddress: Hex;
+          policyId: string;
+        };
+      }
+    ];
+    ReturnType: {
+      ethToTokenConversionRate?: Address; // conversion rate of Eth to Erc20Token
+    };
+  }
+];
+
+export type Erc20PaymasterClient<T extends Transport = Transport> = Client<
+  T,
+  Chain,
+  undefined,
+  Erc20PaymasterClientRpcSchema
+>;
+
 export type Erc7677MiddlewareParams<
   TContext extends Record<string, any> | undefined =
     | Record<string, any>
@@ -70,6 +94,8 @@ export type Erc7677MiddlewareParams<
       ) => Promise<TContext>)
     | TContext;
 };
+
+const Eth2Wei: bigint = 1_000_000_000_000_000_000n;
 
 /**
  * Middleware function for interacting with ERC-7677 enabled clients. It supports resolving paymaster and data fields for user operations.
@@ -159,21 +185,143 @@ export function erc7677Middleware<
 
   const paymasterAndData: ClientMiddlewareFn = async (
     uo,
-    { client, account, feeOptions, overrides }
+    { client, account, feeOptions, overrides, policyToken }
   ) => {
     const userOp = deepHexlify(await resolveProperties(uo));
-    const context =
+
+    let context =
       (typeof params?.context === "function"
         ? await params?.context(userOp, { overrides, feeOptions })
         : params?.context) ?? {};
+    const entrypoint = account.getEntryPoint();
 
     if (!client.chain) {
       throw new ChainNotFoundError();
     }
 
+    let max_token: BigInt | undefined = undefined;
+    if (policyToken !== undefined) {
+      let total_gas = BigInt(0);
+      total_gas += uo.callGasLimit
+        ? BigInt(uo.callGasLimit.toLocaleString())
+        : BigInt(0);
+
+      total_gas += uo.preVerificationGas
+        ? BigInt(uo.preVerificationGas.toLocaleString())
+        : BigInt(0);
+
+      total_gas += uo.verificationGasLimit
+        ? BigInt(uo.verificationGasLimit.toLocaleString())
+        : BigInt(0);
+
+      let paymasterAddress = "0x";
+
+      if (entrypoint.version === "0.7.0") {
+        const uo_07 = uo as UserOperationStruct<"0.7.0">;
+        paymasterAddress = uo_07.paymaster ? uo_07.paymaster : "0x";
+
+        total_gas += uo_07.paymasterPostOpGasLimit
+          ? BigInt(uo_07.paymasterPostOpGasLimit.toLocaleString())
+          : BigInt(0);
+        total_gas += uo_07.paymasterVerificationGasLimit
+          ? BigInt(uo_07.paymasterVerificationGasLimit.toLocaleString())
+          : BigInt(0);
+      } else {
+        const uo_06 = uo as UserOperationStruct<"0.6.0">;
+        paymasterAddress = uo_06.paymasterAndData
+          ? uo_06.paymasterAndData.toString().slice(0, 42)
+          : "0x";
+      }
+      let max_fee_per_gas = uo.maxFeePerGas
+        ? BigInt(uo.maxFeePerGas.toLocaleString())
+        : BigInt(0);
+      let max_gas_in_wei = max_fee_per_gas * total_gas;
+
+      const ratio = await (client as Erc20PaymasterClient).request({
+        method: "alchemy_requestPaymasterTokenQuote",
+        params: [
+          {
+            request: {
+              tokenAddress: policyToken.tokenAddress,
+              policyId: context["policyId"],
+            },
+          },
+        ],
+      });
+      max_token =
+        (max_gas_in_wei *
+          BigInt(
+            ratio.ethToTokenConversionRate ? ratio.ethToTokenConversionRate : 0
+          )) /
+        Eth2Wei;
+
+      const typed_permit_data = {
+        types: {
+          EIP712Domain: [
+            {
+              name: "name",
+              type: "string",
+            },
+            {
+              name: "version",
+              type: "string",
+            },
+            {
+              name: "chainId",
+              type: "uint256",
+            },
+            {
+              name: "verifyingContract",
+              type: "address",
+            },
+          ],
+          Permit: [
+            {
+              name: "owner",
+              type: "address",
+            },
+            {
+              name: "spender",
+              type: "address",
+            },
+            {
+              name: "value",
+              type: "uint256",
+            },
+            {
+              name: "nonce",
+              type: "uint256",
+            },
+            {
+              name: "deadline",
+              type: "uint256",
+            },
+          ],
+        },
+        primaryType: "Permit",
+        domain: {
+          name: policyToken.erc20Name,
+          version: policyToken.version,
+          chainId: BigInt(client.chain.id),
+          verifyingContract: policyToken.tokenAddress as Hex,
+        },
+        message: {
+          owner: account.address as Hex,
+          spender: paymasterAddress as Hex,
+          value: policyToken.maxTokenAmount,
+          nonce: (await account.getAccountNonce()) + BigInt(1),
+          deadline: BigInt(0xffffffffffffffffffffffffffffffff),
+        },
+      } as const;
+
+      context["erc20Context"] = {
+        tokenAddress: policyToken.tokenAddress,
+        maxTokenAmount: max_token,
+      };
+    }
+
     const erc7677client = client as Erc7677Client;
 
-    const entrypoint = account.getEntryPoint();
     const { paymaster, paymasterAndData, paymasterData } =
       await erc7677client.request({
         method: "pm_getPaymasterData",
