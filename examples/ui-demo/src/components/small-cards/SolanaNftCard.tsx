c230e5a282cc363639482d399ce8e1f982bd5a2b
Jade Hamilton <jade@alchemy.com>
2025-04-30 15:36:45 -0600

    feat: add first spl impl

--- examples/ui-demo/src/components/small-cards/SolanaNftCard.tsx
+++ examples/ui-demo/src/components/small-cards/SolanaNftCard.tsx
@@ -2,38 +2,134 @@ import { useToast } from "@/hooks/useToast";
 import {
   useSigner,
   useSignerStatus,
-  useSolanaTransaction,
-  useSolanaSignMessage,
+  useSolanaConnection,
 } from "@account-kit/react";
-import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  Authorized,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  sendAndConfirmRawTransaction,
+  sendAndConfirmTransaction,
+  StakeProgram,
+  Transaction,
+  TransactionConfirmationStrategy,
+  TransactionMessage,
+} from "@solana/web3.js";
+import { useQueryClient } from "@tanstack/react-query";
 import { useMemo, useState } from "react";
-import { Hex } from "viem";
 import { LoadingIcon } from "../icons/loading";
-import { UserAddressTooltip } from "../user-connection-avatar/UserAddressLink";
 import { Button } from "./Button";
 import { Card } from "./Card";
 import Image from "next/image";
 import { Badge } from "./Badge";
 import { CheckCircleFilledIcon } from "../icons/check-circle-filled";
-import { set } from "zod";
+import bs58 from "bs58";
 
 type TransactionState = "idle" | "signing" | "sponsoring" | "complete";
 
+async function getConfirmationStrategy(
+  connection: Connection,
+  signature: string
+): Promise<TransactionConfirmationStrategy> {
+  const latestBlockHash = await connection.getLatestBlockhash();
+
+  return {
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature,
+  };
+}
 export const SolanaNftCard = () => {
-  const signer = useSigner();
   const status = useSignerStatus();
   const queryClient = useQueryClient();
   const [isPending, setIsPending] = useState(false);
   const [transactionState, setTransactionState] =
     useState<TransactionState>("idle");
 
-  const solanaSigner = useMemo(() => {
-    if (!signer) return;
-    if (!status.isConnected) return;
-    return signer.toSolanaSigner();
-  }, [signer, status.isConnected]);
+  const { connection, signer: solanaSigner } = useSolanaConnection();
   const { setToast } = useToast();
+
+  const handleCollectNFT = async () => {
+    setIsPending(true);
+    try {
+      if (!solanaSigner) throw new Error("No signer found");
+      if (!connection) throw new Error("No connection found");
+      setTransactionState("signing");
+      setTransactionState("sponsoring");
+      const stakeAccount = Keypair.generate();
+      const publicKey = new PublicKey(solanaSigner.address);
+      let createStakeAccountInstruction = StakeProgram.createAccount({
+        fromPubkey: publicKey,
+        stakePubkey: stakeAccount.publicKey,
+        authorized: new Authorized(publicKey, publicKey),
+        lamports: LAMPORTS_PER_SOL * 0.02,
+      });
+      let createStakeAccountTransaction = new Transaction().add(
+        createStakeAccountInstruction
+      );
+      createStakeAccountTransaction.recentBlockhash = (
+        await connection.getLatestBlockhash()
+      ).blockhash;
+
+      createStakeAccountTransaction.feePayer = publicKey;
+
+      await solanaSigner.addSignature(createStakeAccountTransaction);
+      createStakeAccountTransaction.partialSign(stakeAccount);
+
+      const signature =
+        "version" in createStakeAccountTransaction ?
+          createStakeAccountTransaction.signatures[0]!
+        : createStakeAccountTransaction.signature!;
+
+      const confirmationStrategy = await getConfirmationStrategy(
+        connection,
+        bs58.encode(signature as any)
+      );
+      const transactionHash = await sendAndConfirmRawTransaction(
+        connection,
+        Buffer.from(createStakeAccountTransaction.serialize() as any),
+        confirmationStrategy,
+        { commitment: "confirmed" }
+      );
+
+      const votePubkey = new PublicKey(solanaSigner.address);
+      let delegateInstruction = StakeProgram.delegate({
+        stakePubkey: stakeAccount.publicKey,
+        authorizedPubkey: publicKey,
+        votePubkey,
+      });
+
+      let delegateTransaction = new Transaction().add(delegateInstruction);
+      delegateTransaction.recentBlockhash = (
+        await connection.getRecentBlockhash()
+      ).blockhash;
+      delegateTransaction.feePayer = publicKey;
+      await solanaSigner.addSignature(delegateTransaction);
+
+      const finalTransaction = await sendAndConfirmRawTransaction(
+        connection,
+        Buffer.from(delegateTransaction.serialize() as any),
+        confirmationStrategy,
+        { commitment: "confirmed" }
+      );
+
+      console.log({ finalTransaction, transactionHash });
+      debugger;
+      setTransactionState("complete");
+    } finally {
+      setIsPending(false);
+    }
+
+    // await sendTransaction({
+    //   transfer: {
+    //     amount: 1000000,
+    //     toAddress:
+    //       process.env.NEXT_PUBLIC_SOLANA_ADDRESS || solanaSigner.address || "",
+    //   },
+    // });
+  };
 
   const imageSlot = (
     <div className="w-full h-full bg-[#DCFCE7] flex justify-center items-center relative">
@@ -82,7 +178,7 @@ export const SolanaNftCard = () => {
   const content = useMemo(
     () => (
       <>
-        {transactionState === "idle" ? (
+        {transactionState === "idle" ?
           <>
             <p className="text-fg-primary text-sm mb-3">
               Transact with one click using gas sponsorship and background
@@ -100,34 +196,11 @@ export const SolanaNftCard = () => {
               </p>
             </div>
           </>
-        ) : (
-          renderTransactionStates()
-        )}
+        : renderTransactionStates()}
       </>
     ),
     [transactionState]
   );
-
-  const handleCollectNFT = async () => {
-    const wait = (ms: number) =>
-      new Promise((resolve) => setTimeout(resolve, ms));
-    setIsPending(true);
-    setTransactionState("signing");
-    await wait(1000);
-    setTransactionState("sponsoring");
-    await wait(1000);
-    setTransactionState("complete");
-    await wait(1000);
-    setIsPending(false);
-
-    // await sendTransaction({
-    //   transfer: {
-    //     amount: 1000000,
-    //     toAddress:
-    //       process.env.NEXT_PUBLIC_SOLANA_ADDRESS || solanaSigner.address || "",
-    //   },
-    // });
-  };
 
   return (
     <Card
@@ -141,11 +214,11 @@ export const SolanaNftCard = () => {
           onClick={handleCollectNFT}
           disabled={!solanaSigner || isPending}
         >
-          {transactionState === "idle"
-            ? "Collect NFT"
-            : transactionState === "complete"
-            ? "Re-collect NFT"
-            : "Collecting NFT..."}
+          {transactionState === "idle" ?
+            "Collect NFT"
+          : transactionState === "complete" ?
+            "Re-collect NFT"
+          : "Collecting NFT..."}
         </Button>
       }
     />
