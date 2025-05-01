7ae02809332fc88e87166ac9345d9f6e938361b4
howydev <132113803+howydev@users.noreply.github.com>
2025-05-01 17:06:32 -0400

    chore: fix lint, change to use type narrowing instead

--- aa-sdk/core/src/middleware/defaults/webauthnGasEstimator.ts
+++ aa-sdk/core/src/middleware/defaults/webauthnGasEstimator.ts
@@ -1,6 +1,7 @@
 import { AccountNotFoundError } from "../../errors/account.js";
 import type { ClientMiddlewareFn } from "../types";
 import { defaultGasEstimator } from "./gasEstimator.js";
+import { type Hex, hexToString } from "viem";
 
 export const rip7212CheckBytecode =
   "0x60806040526040517f532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25815260056020820152600160408201527f4a03ef9f92eb268cafa601072489a56380fa0dc43171d7712813b3a19a1eb5e560608201527f3e213e28a608ce9a2f4a17fd830c6654018a79b3e0263d91a8ba90622df6f2f0608082015260208160a0836101005afa503d5f823e3d81f3fe";
@@ -25,26 +26,27 @@ export const webauthnGasEstimator: (
 
     const uo = await gasEstimator_(struct, params);
 
-    // perform eth call read check to see if the chain has 7212
-    const { data } = await params.client.call({ data: rip7212CheckBytecode });
+    const pvg: bigint | number | Hex | undefined =
+      uo.preVerificationGas instanceof Promise
+        ? await uo.preVerificationGas
+        : uo?.preVerificationGas ?? 0n;
 
-    const chainHas7212: boolean = data ? BigInt(data) == 1n : false;
-
-    if (
-      !uo.preVerificationGas ||
-      (typeof uo.preVerificationGas !== "string" &&
-        typeof uo.preVerificationGas !== "bigint" &&
-        typeof uo.preVerificationGas !== "number")
-    ) {
+    if (!pvg) {
       throw new Error(
         "WebauthnGasEstimator: preVerificationGas is 0 or not defined"
       );
     }
 
+    // perform eth call read check to see if the chain has 7212
+    const { data } = await params.client.call({ data: rip7212CheckBytecode });
+
+    const chainHas7212: boolean = data ? BigInt(data) === 1n : false;
+
     // TODO: iterate numbers. Aim to have ~1000 gas buffer to account for longer authenticatorDatas and clientDataJSONs
     return {
       ...uo,
       preVerificationGas:
-        BigInt(uo.preVerificationGas) + (chainHas7212 ? 10000n : 300000n),
+        BigInt(typeof pvg === "string" ? hexToString(pvg) : pvg) +
+        (chainHas7212 ? 10000n : 300000n),
     };
   };
