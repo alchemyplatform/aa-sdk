dc273221abb6818af5f911d9786e2f660b6c9f7b
Alex <36385732+alex-miao@users.noreply.github.com>
2025-05-02 10:50:27 -0700

    feat: add story mainnet and aeneid testnet (#1580)

--- account-kit/infra/src/index.ts
+++ account-kit/infra/src/index.ts
@@ -44,6 +44,8 @@ export {
   openlootSepolia,
   gensynTestnet,
   riseTestnet,
+  storyMainnet,
+  storyAeneid,
 } from "./chains.js";
 export type * from "./client/decorators/alchemyEnhancedApis.js";
 export { alchemyEnhancedApiActions } from "./client/decorators/alchemyEnhancedApis.js";
