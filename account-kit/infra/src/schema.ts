d086e0a3f19891cbfc21b94587408996f49e90c1
Pengfei(Andy) Zhang <andy@alchemy.com>
2025-05-06 10:58:16 -0400

    fix: fix error message

--- account-kit/infra/src/schema.ts
+++ account-kit/infra/src/schema.ts
@@ -7,6 +7,6 @@ export const AlchemyChainSchema = z.custom<Chain>((chain) => {
   const chain_ = ChainSchema.parse(chain);
 
   return chain_.rpcUrls.alchemy != null;
-}, "chain must include an alchemy rpc url. See `createAlchemyChain` or import a chain from `@account-kit/infra`.");
+}, "chain must include an alchemy rpc url. See `defineAlchemyChain` or import a chain from `@account-kit/infra`.");
 
 export const AlchemySdkClientSchema = z.instanceof(Alchemy);
