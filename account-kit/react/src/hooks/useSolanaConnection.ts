d471359104d0ad55f1c3d4e26d694edc88acffec
Jade <2364004+Blu-J@users.noreply.github.com>
2025-05-08 10:57:55 -0600

    feat: solana demo (#1583)
    * Revert "feat: solana nft demo (#1578)"
    
    This reverts commit 7ad9c20d5c005ddaad542ba3aaf31d6d795b88f2.
    
    * Revert "Revert "feat: solana nft demo (#1578)""
    
    This reverts commit 9b84edcc51754fdb111484d2cf70ef7deb12fe4d.
    
    * Revert "Revert "feat: solana nft demo (#1578)" (#1582)"
    
    This reverts commit 469173054560a558b2b705374cc484226b368cf3.
    
    * fix: pr changes
    
    * chore: fix test that has a timing issue maybe
    
    * fix: allow the one card to do all
    
    * Update examples/ui-demo/src/components/small-cards/SolanaNftCard.tsx
    
    * feat: do auto from airdrop
    
    * chore: refactor to invert logic to injectable (#1584)
    
    * chore: refactor to invert logic to injectable
    
    * feat: do auto from airdrop
    
    * feat: make it so the adding self signer is automatically
    
    * chore: adding in the other transaction filtering based on the keys
    
    * fix: edge case on key may not be signer
    
    * feat: remove the address from the solana card
    
    * fix: link to transaction needs three lines for card
    
    * chore: remove old new badges for other cards
    
    * fix: imports are not .js
    
    * chore: undo stage change
    
    * fix: weird bug on import
    
    * chore: change the function
    
    * chore: add in the doc build
    
    * chore: change the sponsoring to include rent
    
    * fix: styling in card to match comments made in pr

---
+++ account-kit/react/src/hooks/useSolanaConnection.ts
@@ -0,0 +1,55 @@
+"use client";
+
+import * as solanaNetwork from "../solanaNetwork.js";
+import { SolanaSigner } from "@account-kit/signer";
+import { useSolanaSigner } from "./useSolanaSigner.js";
+import { getSolanaConnection, watchSolanaConnection } from "@account-kit/core";
+import { useSyncExternalStore } from "react";
+import { useAlchemyAccountContext } from "./useAlchemyAccountContext.js";
+
+/**
+ * Returned from the solana connection.
+ */
+export interface SolanaConnection {
+  /** The solana signer used to send the transaction */
+  readonly signer: SolanaSigner | null;
+  /** The solana connection used to send the transaction */
+  readonly connection: solanaNetwork.Connection | null;
+}
+
+/**
+ * The parameters for the SolanaConnectionHookParams hook.
+ */
+export type SolanaConnectionHookParams = {
+  signer?: SolanaSigner;
+};
+
+/**
+ * This hook is used for establishing a connection to Solana and returns the connection object and the signer object.
+ *
+ * @example
+ * ```ts
+  const {connection} = useSolanaConnection();
+ 
+ * ```
+ * @param {SolanaConnectionHookParams} opts Options for the hook to get setup
+ * @returns {SolanaConnection} The transaction hook.
+ */
+export function useSolanaConnection(
+  opts: SolanaConnectionHookParams = {}
+): SolanaConnection {
+  const { config } = useAlchemyAccountContext();
+  const fallbackSigner: null | SolanaSigner = useSolanaSigner();
+  const connection =
+    useSyncExternalStore(
+      watchSolanaConnection(config),
+      () => getSolanaConnection(config),
+      () => getSolanaConnection(config)
+    )?.connection || null;
+  const signer: null | SolanaSigner = opts?.signer || fallbackSigner;
+
+  return {
+    connection,
+    signer,
+  };
+}
