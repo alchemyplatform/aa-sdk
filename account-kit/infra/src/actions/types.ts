5a8f22fd3464039d7261936c2c7c6010e21b2400
Andy <andysim3d@gmail.com>
2025-06-05 04:15:47 +0800

    feat: add alchemy_requestPaymasterTokenQuote to aa-sdk (#1659)
    * feat: add alchemy_requestPaymasterTokenQuote to aa-sdk
    
    * fix: typo

--- account-kit/infra/src/actions/types.ts
+++ account-kit/infra/src/actions/types.ts
@@ -91,3 +91,14 @@ export type RequestGasAndPaymasterAndDataResponse<
           | "paymasterPostOpGasLimit"
         >
       : never);
+
+export type RequestPayamsterTokenQuoteRequest = [
+  {
+    policyId: string;
+    tokenAddress: Address;
+  },
+];
+
+export type RequestPayamsterTokenQuoteResponse = {
+  ethToTokenConversionRate: string;
+};
