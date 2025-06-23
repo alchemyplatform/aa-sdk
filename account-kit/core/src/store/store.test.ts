8127e0cf6b21d1885d3be8b9977ec11b20dc0af2
moldy <michael.moldoveanu@alchemy.com>
2025-06-23 16:08:50 -0400

    feat: support all wallet server account types in account-kit/core

--- account-kit/core/src/store/store.test.ts
+++ account-kit/core/src/store/store.test.ts
@@ -517,7 +517,7 @@ describe("createConfig tests", () => {
             },
             "smartWalletClients": {},
           },
-          "version": 14,
+          "version": ${STORAGE_VERSION},
         }
       `);
   });
