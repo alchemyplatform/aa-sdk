6f93c63beb91c8c69ae9f08156a939b703eedd02
moldy <michael.moldoveanu@alchemy.com>
2025-06-26 13:57:27 -0400

    fix: update the json rpc halp command to account for new transport

--- halp/src/commands/add-package/json-rpc/jsonRpcSchemaTemplate.ts
+++ halp/src/commands/add-package/json-rpc/jsonRpcSchemaTemplate.ts
@@ -1,6 +1,7 @@
 import dedent from "dedent";
 
 export const jsonRpcSchemaTemplate = () => dedent`
+// Once you've defined this schema, import it into the schema.ts file found in the root alchemy package
 export type TODO_MyRpcSchema = [
   {
     Method: "TODO_MyMethod";
