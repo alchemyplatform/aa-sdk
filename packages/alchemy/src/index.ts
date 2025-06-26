b2807c808786a6afb84dddd80115a7ff2a7031ad
moldy <michael.moldoveanu@alchemy.com>
2025-06-26 13:57:27 -0400

    fix: update the json rpc halp command to account for new transport

--- packages/alchemy/src/index.ts
+++ packages/alchemy/src/index.ts
@@ -1,6 +1,6 @@
 import { alchemy as alchemyCommon } from "@alchemy/common";
-import type { PublicRpcSchema } from "viem";
-import type { AlchemyJsonRpcSchema } from "./schema.js";
+import { type PublicRpcSchema } from "viem";
+import type { AlchemyHttpSchema, AlchemyJsonRpcSchema } from "./schema.js";
 
 export type * from "@alchemy/common";
 export { ChainNotFoundError } from "@alchemy/common";
@@ -8,6 +8,5 @@ export { ChainNotFoundError } from "@alchemy/common";
 // Alchemy transport gets overridden here because we need to define the Http and JSON-RPC schemas which might be aggregated from various packages
 export const alchemy = alchemyCommon<
   [...PublicRpcSchema, ...AlchemyJsonRpcSchema],
-  // TODO: need to add the HttpSchema here as we define it
-  []
+  AlchemyHttpSchema
 >;
