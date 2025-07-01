f26e1fa9ea17270d6576150f92f2458189ed93b9
jakehobbs <jacob.hobbs@alchemy.com>
2025-07-01 10:27:22 -0700

    fix: entityId shifting bug

--- examples/ui-demo/src/hooks/7702/genEntityId.ts
+++ examples/ui-demo/src/hooks/7702/genEntityId.ts
@@ -1,6 +1,8 @@
+import { maxUint32 } from "viem";
+
 export function genEntityId(): number {
   const min: number = 1;
-  const max: number = Number(999999);
+  const max: number = Number(maxUint32);
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
