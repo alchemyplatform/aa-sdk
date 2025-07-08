16734f9c1a29c29bffa022746bee8935f73a0b02
adam <adam@alchemy.com>
2025-07-08 18:50:52 -0400

    chore: update to latest

--- account-kit/wallet-client/src/internal/account.ts
+++ account-kit/wallet-client/src/internal/account.ts
@@ -5,23 +5,12 @@ import {
   createMultiOwnerLightAccount,
   createMultiOwnerModularAccount,
 } from "@account-kit/smart-contracts";
-import {
-  concatHex,
-  type Chain,
-  type Transport,
-  type Address,
-  hexToNumber,
-} from "viem";
+import { concatHex, type Chain, type Transport, type Address } from "viem";
 import type { StaticDecode } from "@sinclair/typebox";
 import { SerializedInitcode } from "@alchemy/wallet-api-types";
-import {
-  Eip7702AccountTypeToDelegationAddress,
-  PermissionsCapability,
-  decodePermissionsContext,
-  type Supported7702AccountType,
-} from "@alchemy/wallet-api-types/capabilities";
 import { InternalError, InvalidRequestError } from "ox/RpcResponse";
 import { assertNever } from "../utils.js";
+import { getAccountTypeForDelegationAddress7702 } from "@alchemy/wallet-api-types/capabilities";
 
 type CreateAccountParams = {
   chain: Chain;
@@ -29,8 +18,7 @@ type CreateAccountParams = {
   signer: SmartAccountSigner;
   accountAddress: Address;
   counterfactualInfo?: StaticDecode<typeof SerializedInitcode>; // undefined for 7702 accounts
-  permissions?: StaticDecode<typeof PermissionsCapability>;
-  delegation?: Address;
+  delegation?: Address; // for 7702 accounts
 };
 
 /**
@@ -43,45 +31,18 @@ export async function createAccount(
 ): Promise<SmartContractAccount> {
   const { counterfactualInfo: ci, ...accountParams } = params;
 
-  // This throws if we pass a permission context and the account is not MA-v2
-  // TODO: test that this edge case is handled correctly
-  const parsedContext = parsePermissionsContext(
-    params.permissions,
-    ci,
-    params.delegation,
-  );
-
-  const signerEntity =
-    parsedContext?.contextVersion === "NON_DEFERRED_ACTION"
-      ? {
-          entityId: hexToNumber(parsedContext.entityId),
-          isGlobalValidation: parsedContext.isGlobalValidation,
-        }
-      : undefined;
-
-  const mode = params.delegation ? "7702" : "default";
-
-  if (mode === "7702") {
+  if (params.delegation) {
     const accountType = getAccountTypeForDelegationAddress7702(
-      params.delegation!,
+      params.delegation,
     );
     if (accountType !== "ModularAccountV2") {
       throw new Error("7702 mode currently only supports ModularAccountV2");
     }
     return createModularAccountV2({
       ...accountParams,
-      signerEntity,
-      deferredAction: parsedContext?.deferredAction,
-      mode,
+      mode: "7702",
     });
   }
-
-  if (mode !== "default") {
-    return assertNever(mode, "Unexpected mode in createAccount");
-  }
-
-  // At this point, we are guaranteed to be in default mode, where ci
-  // (counterfactualInfo) must be defined
 
   if (!ci) {
     throw new InternalError({
@@ -100,9 +61,7 @@ export async function createAccount(
     case "MAv2.0.0-sma-b":
       return createModularAccountV2({
         ...commonParams,
-        signerEntity,
-        deferredAction: parsedContext?.deferredAction,
-        mode,
+        mode: "default",
       });
     case "LightAccountV2.0.0":
       return createLightAccount({
@@ -145,61 +104,3 @@ export async function createAccount(
       return assertNever(factoryType, "Unsupported factory type");
   }
 }
-
-function parsePermissionsContext(
-  permissions?: StaticDecode<typeof PermissionsCapability>,
-  parsedCi?: StaticDecode<typeof SerializedInitcode> | undefined,
-  delegation7702?: Address,
-) {
-  if (!permissions) {
-    return undefined;
-  }
-
-  if ("sessionId" in permissions) {
-    throw new InvalidRequestError({
-      message: "Remote permissions are not supported in isomorphic client",
-    });
-  }
-
-  if (!("context" in permissions)) {
-    return undefined;
-  }
-
-  const isMAV2 =
-    (parsedCi && parsedCi.factoryType === "MAv2.0.0-sma-b") ||
-    (delegation7702 &&
-      getAccountTypeForDelegationAddress7702(delegation7702) ===
-        "ModularAccountV2");
-
-  if (!isMAV2) {
-    throw new InvalidRequestError({
-      message: "Permissions are currently only supported by MAv2 accounts",
-    });
-  }
-
-  const context = decodePermissionsContext(permissions);
-
-  if (context?.contextVersion === "REMOTE_MODE_DEFERRED_ACTION") {
-    throw new InvalidRequestError({
-      message: "Remote mode deferred action not supported in isomorphic client",
-    });
-  }
-
-  return context;
-}
-
-const DelegationAddressToAccountType: Record<
-  Address,
-  Supported7702AccountType
-> = Object.fromEntries(
-  Object.entries(Eip7702AccountTypeToDelegationAddress).map(([key, value]) => [
-    value,
-    key,
-  ]),
-) as Record<Address, Supported7702AccountType>;
-
-const getAccountTypeForDelegationAddress7702 = (
-  address: Address,
-): Supported7702AccountType | undefined => {
-  return DelegationAddressToAccountType[address];
-};
