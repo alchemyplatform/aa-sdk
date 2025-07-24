76a4c0528eaf1cee79b54b20687885c154dae4e4
David Philipson <david.philipson@gmail.com>
2025-07-24 13:32:22 -0700

    feat(signer): v5 interfaces
    * Creates new packages, `@alchemy/signer`, `@alchemy/signer-web` and
      `@alchemy/signer-rn`. Note that the web signer is now in a separate
      package rather than being part of `/signer`.
    * Introduces a new set of interfaces: instead of multiple signer classes
      which represent both logged-in and logged-out signers, have separate
      classes `Signer` for an authenticated signer which is ready to start
      signing, and `AuthClient` which performs login tasks and returns a
      `Signer` once login is complete.
    * Speaking of auth, there are separate methods for different login
      types, e.g. `sendAuthEmail`, `submitOtpCode`, rather than a single
      `authenticate` method that does it all.
    * At the moment, nothing is done related to session storage. I'm not
      actually positive we want that to be automatic, as it adds unrelated
      responsibilities to the signer classes and isn't useful for some
      applications (e.g. scripts) `Signer` for an authenticated signer which
      is ready to start signing, and `AuthClient` which performs login tasks
      and returns a `Signer` when login is complete.
    * At the moment, nothing is done related to session storage. I'm not
      actually sure we want that to be automatic or even part of the signer
      packages, as it adds unrelated responsibilities to the signer classes
      and isn't useful for some use cases (e.g. scripts).

---
+++ packages/signer/src/types.ts
@@ -0,0 +1,77 @@
+import type { Address } from "viem";
+
+export type User = {
+  email?: string;
+  orgId: string;
+  userId: string;
+  address: Address;
+  solanaAddress?: string;
+  credentialId?: string;
+  idToken?: string;
+  claims?: Record<string, unknown>;
+};
+
+export type AuthMethods = {
+  email?: string;
+  oauthProviders: OauthProviderInfo[];
+  passkeys: PasskeyInfo[];
+};
+
+export type OauthProviderInfo = {
+  providerId: string;
+  issuer: string;
+  providerName?: string;
+  userDisplayName?: string;
+};
+
+export type PasskeyInfo = {
+  authenticatorId: string;
+  name: string;
+  createdAt: number;
+};
+
+export type AddOauthProviderParams = {
+  providerName: string;
+  oidcToken: string;
+};
+
+/**
+ * Re-exporting a type defined internally in Turnkey's SDK (where it's called
+ * TStamper).
+ */
+export type TurnkeyStamper = {
+  stamp: (input: string) => Promise<TurnkeyStamp>;
+  clear?(): void;
+};
+
+/**
+ * The methods we will use from the flavor of stamper that exposes a public TEK
+ * and becomes fully initialized upon receiving a "credential bundle" which is
+ * an encrypted private key. Describes both IFrameStamper and NativeTEKStamper.
+ */
+export type TurnkeyTekStamper = TurnkeyStamper & {
+  /**
+   * Initializes the stamper and returns its public key.
+   */
+  init: () => Promise<string>;
+  injectCredentialBundle: (bundle: string) => Promise<boolean>;
+};
+
+export type CreateTekStamperFn = () => Promise<TurnkeyTekStamper>;
+
+/**
+ * Creates a flavor of stamper which stamps requests by directly using a private
+ * key accessed with WebAuthn (e.g. passkeys).
+ */
+export type CreateWebAuthnStamperFn = (
+  params: CreateWebAuthnStamperParams
+) => Promise<TurnkeyStamper>;
+
+export type CreateWebAuthnStamperParams = {
+  credentialId: string | undefined;
+};
+
+export type TurnkeyStamp = {
+  stampHeaderName: string;
+  stampHeaderValue: string;
+};
