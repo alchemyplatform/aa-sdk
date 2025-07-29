d430abbe5e9f7f0fd1e9b23071314c2f2d8a0153
Andrew Webb <androbwebb@gmail.com>
2025-07-29 16:16:49 +0100

    chore: lint

--- examples/ui-demo/src/components/small-cards/DepositAndSwapsCard.tsx
+++ examples/ui-demo/src/components/small-cards/DepositAndSwapsCard.tsx
@@ -8,52 +8,44 @@ import { LoadingIcon } from "../icons/loading";
 export const DepositAndSwapsCard = () => {
   const uri = "https://alchemyapi.typeform.com/to/pkjSr57v";
 
-  const imageSlot = (
-    <div className="w-full h-full bg-[#8797D5] flex justify-center items-center relative">
-      {uri ? (
-        <Image
-          src="/images/deposits.png"
-          alt="Physical coins of ethereum and bitcoin being swapped"
-          width={300}
-          height={300}
-          className="w-full h-full object-cover object-[0_40%]"
-          priority
-        />
-      ) : (
-        <LoadingIcon />
-      )}
-    </div>
-  );
-
   return (
-    <>
-      <Card
-        badgeSlot={
-          <Badge
-            text="New!"
-            className="text-white"
-            style={{
-              background: "linear-gradient(132deg, #FF9C27 0%, #FD48CE 100%)",
-            }}
+    <Card
+      badgeSlot={
+        <Badge
+          text="New!"
+          className="text-white"
+          style={{
+            background: "linear-gradient(132deg, #FF9C27 0%, #FD48CE 100%)",
+          }}
+        />
+      }
+      imageSlot={
+        <div className="w-full h-full bg-[#8797D5] flex justify-center items-center relative">
+          <Image
+            src="/images/deposits.png"
+            alt="Physical coins of ethereum and bitcoin being swapped"
+            width={300}
+            height={300}
+            className="w-full h-full object-cover object-[0_40%]"
+            priority
           />
-        }
-        imageSlot={imageSlot}
-        heading="Swaps, Deposits, Payments"
-        content={
-          <p className="text-fg-primary text-sm mb-3">
-            Maximize liquidity onchain with easy to use swaps, payments, and
-            deposits.
-          </p>
-        }
-        buttons={
-          <Button asChild className="mt-auto w-full">
-            <a href={uri} target="_blank">
-              Request Early Access
-              <ArrowUpRightFromSquare className="w-4 h-4 mr-2" />
-            </a>
-          </Button>
-        }
-      />
-    </>
+        </div>
+      }
+      heading="Swaps, Deposits, Payments"
+      content={
+        <p className="text-fg-primary text-sm mb-3">
+          Maximize liquidity onchain with easy to use swaps, payments, and
+          deposits.
+        </p>
+      }
+      buttons={
+        <Button asChild className="mt-auto w-full">
+          <a href={uri} target="_blank" rel="noopener noreferrer">
+            Request Early Access
+            <ArrowUpRightFromSquare className="w-4 h-4 mr-2" />
+          </a>
+        </Button>
+      }
+    />
   );
 };
