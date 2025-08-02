454820e7a97030e921d742f322c0e57e4d73bd2b
jakehobbs <jacob.hobbs@alchemy.com>
2025-08-02 14:49:47 -0700

    fix: respect retryCount on alchemy transport & in alchemy smart account client

--- account-kit/infra/src/alchemyTransport.test.ts
+++ account-kit/infra/src/alchemyTransport.test.ts
@@ -1,7 +1,10 @@
 import * as AACoreModule from "@aa-sdk/core";
+import { createSmartAccountClient } from "@aa-sdk/core";
 import { avalanche } from "viem/chains";
 import { alchemy } from "./alchemyTransport.js";
-import { sepolia } from "./chains.js";
+import { arbitrumSepolia, sepolia } from "./chains.js";
+import { http } from "viem";
+import { createBundlerClient } from "viem/account-abstraction";
 
 describe("Alchemy Transport Tests", () => {
   it.each([
@@ -46,4 +49,69 @@ describe("Alchemy Transport Tests", () => {
       ]]
     `);
   });
+
+  it.each([0, 1, 2, 3])(
+    "respects retryCount of %i when used with a viem bundler client",
+    async (retryCount) => {
+      const { mockFetch, unstub } = givenMockFetchError();
+
+      const client = createBundlerClient({
+        transport: alchemy({
+          rpcUrl: "http://invalid",
+          retryCount,
+        }),
+        chain: arbitrumSepolia,
+      });
+
+      await expect(
+        client.request({
+          // @ts-expect-error - Method doesn't matter for this test.
+          method: "eth_blockNumber",
+        }),
+      ).rejects.toThrow();
+
+      expect(mockFetch.mock.calls.length).toEqual(retryCount + 1);
+      unstub();
+    },
+  );
+
+  it.each([0, 1, 2, 3])(
+    "respects retryCount of %i when used with an alchemy smart account client",
+    async (retryCount) => {
+      const { mockFetch, unstub } = givenMockFetchError();
+
+      const client = createSmartAccountClient({
+        transport: alchemy({
+          apiKey: "invalid",
+          retryCount,
+        }),
+        chain: arbitrumSepolia,
+      });
+
+      await expect(
+        client.request({
+          method: "eth_blockNumber",
+        }),
+      ).rejects.toThrow();
+
+      expect(mockFetch.mock.calls.length).toEqual(retryCount + 1);
+      unstub();
+    },
+  );
 });
+
+// Mocks global fetch to always return a 500 error.
+const givenMockFetchError = () => {
+  const mockFetch = vi.fn().mockResolvedValue({
+    ok: false,
+    status: 500,
+    statusText: "Internal Server Error",
+    json: () => Promise.resolve({ error: "Internal Server Error" }),
+    text: () => Promise.resolve("Internal Server Error"),
+  });
+  vi.stubGlobal("fetch", mockFetch);
+  return {
+    mockFetch,
+    unstub: () => vi.unstubAllGlobals(),
+  };
+};
