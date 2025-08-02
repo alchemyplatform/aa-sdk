43ccd842dd558e73038644d77a60e8cf9512aa06
jakehobbs <jacob.hobbs@alchemy.com>
2025-08-01 18:13:53 -0700

    chore: fmt

--- examples/react-native-expo-example/metro.config.js
+++ examples/react-native-expo-example/metro.config.js
@@ -10,14 +10,14 @@ const config = getDefaultConfig(projectRoot);
 
 // Add aliases for file-system import based modules
 const ALIASES = {
-  "@noble/hashes/crypto": path.resolve(
-    monorepoRoot,
-    "node_modules/@noble/hashes/crypto.js",
-  ),
-  "@sinclair/typebox": path.resolve(
-    monorepoRoot,
-    "node_modules/@sinclair/typebox/build/cjs/index.js",
-  ),
+    "@noble/hashes/crypto": path.resolve(
+        monorepoRoot,
+        "node_modules/@noble/hashes/crypto.js",
+    ),
+    "@sinclair/typebox": path.resolve(
+        monorepoRoot,
+        "node_modules/@sinclair/typebox/build/cjs/index.js",
+    ),
 };
 
 // 1. Watch all files within the monorepo
@@ -25,9 +25,9 @@ config.watchFolders = [projectRoot, monorepoRoot];
 
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(monorepoRoot, "node_modules"),
-  path.resolve(monorepoRoot, "account-kit/rn-signer/node_modules"),
+    path.resolve(projectRoot, "node_modules"),
+    path.resolve(monorepoRoot, "node_modules"),
+    path.resolve(monorepoRoot, "account-kit/rn-signer/node_modules"),
 ];
 
 // Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
@@ -35,28 +35,28 @@ config.resolver.disableHierarchicalLookup = true;
 
 // Default to file-based module resolution for file-system import based modules
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (ALIASES[moduleName]) {
-    return {
-      filePath: ALIASES[moduleName],
-      type: "sourceFile",
-    };
-  }
-  return context.resolveRequest(context, moduleName, platform);
+    if (ALIASES[moduleName]) {
+        return {
+            filePath: ALIASES[moduleName],
+            type: "sourceFile",
+        };
+    }
+    return context.resolveRequest(context, moduleName, platform);
 };
 
 config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  crypto: require.resolve("crypto-browserify"),
-  stream: require.resolve("stream-browserify"),
+    ...config.resolver.extraNodeModules,
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
 };
 
 // Important to allow importing package exports
 config.resolver.unstable_enablePackageExports = true;
 
 config.resolver.unstable_conditionNames = [
-  "browser",
-  "require",
-  "react-native",
+    "browser",
+    "require",
+    "react-native",
 ];
 
 module.exports = config;
