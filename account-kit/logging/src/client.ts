8f47c2b7af70a99c7b4ad4f62606624c83227e13
jakehobbs <jacob.hobbs@alchemy.com>
2025-08-25 17:48:01 -0700

    chore: update comment

--- account-kit/logging/src/client.ts
+++ account-kit/logging/src/client.ts
@@ -28,7 +28,7 @@ function getOrCreateAnonId(): AnonId {
 
 function loadHeap(projectId: string): Promise<void> {
   if (!window.heap) {
-    // Use `"https://static.alchemyapi.io/scripts/anayltics/heap-analytics-script.js"`, but it's an old
+    // We ideally load from `"https://static.alchemyapi.io/scripts/anayltics/heap-analytics-script.js"`, but it's an old version.
     const scriptSrc = `
     // Load Heap Analytics
     window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
