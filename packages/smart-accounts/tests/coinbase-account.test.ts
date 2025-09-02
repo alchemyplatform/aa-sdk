97dd44f90a5912df451155356f4df42df5eba95f
trevoraron <trevor.aron@alchemy.com>
2025-09-02 17:42:11 -0400

    chore: lint

--- packages/smart-accounts/tests/coinbase-account.test.ts
+++ packages/smart-accounts/tests/coinbase-account.test.ts
@@ -229,7 +229,7 @@ describe("Viem AA - Coinbase Smart Account", () => {
       account,
       chain: local070Instance.chain,
       transport: custom(client),
-      ...alchemyGasManagerHooks("test-policy", { 
+      ...alchemyGasManagerHooks("test-policy", {
         client,
         address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // USDC address
         maxTokenAmount: 10_000_000n, // 10 USDC
