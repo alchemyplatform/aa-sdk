ce9fb3466d62e0c79bf9b0fa5a2767e5797dbf68
Andrew Webb <androbwebb@gmail.com>
2025-09-04 09:45:38 +0100

    chore: lint

--- account-kit/signer/src/base.ts
+++ account-kit/signer/src/base.ts
@@ -59,7 +59,9 @@ import {
 import { assertNever } from "./utils/typeAssertions.js";
 import { hashAuthorization } from "viem/utils";
 
-export interface BaseAlchemySignerParams<TClient extends BaseSignerClient> {
+export interface BaseAlchemySignerParams<
+  TClient extends BaseSignerClient<any, any>,
+> {
   client: TClient;
   sessionConfig?: Omit<SessionManagerParams, "client">;
   initialError?: ErrorInfo;
@@ -118,8 +120,9 @@ type GetUserParams =
  * Base abstract class for Alchemy Signer, providing authentication and session management for smart accounts.
  * Implements the `SmartAccountAuthenticator` interface and handles various signer events.
  */
-export abstract class BaseAlchemySigner<TClient extends BaseSignerClient>
-  implements SmartAccountAuthenticator<AuthParams, User, TClient>
+export abstract class BaseAlchemySigner<
+  TClient extends BaseSignerClient<any, any>,
+> implements SmartAccountAuthenticator<AuthParams, User, TClient>
 {
   signerType: "alchemy-signer" | "rn-alchemy-signer" = "alchemy-signer";
   inner: TClient;
