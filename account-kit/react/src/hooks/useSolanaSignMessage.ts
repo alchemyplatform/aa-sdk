e3f23679cd1afc1cd17f2ac6b22c7c8fbb8d9b0e
jakehobbs <jacob.hobbs@alchemy.com>
2025-09-16 10:55:56 -0700

    docs: fix example

--- account-kit/react/src/hooks/useSolanaSignMessage.ts
+++ account-kit/react/src/hooks/useSolanaSignMessage.ts
@@ -57,7 +57,7 @@ export type UseSolanaSignMessageParams = {
  *   reset,
  * } = useSolanaSignMessage({});
  *
- * mutate({ message: "Hello" });
+ * signHello({ message: "Hello" });
  * ```
  * @param {UseSolanaSignMessageParams} opts - Options for the hook to get setup.
  * @returns {SolanaSignedMessage} This should be hook mutations plus a few more. Used to get the end result of the signing and the callbacks.
