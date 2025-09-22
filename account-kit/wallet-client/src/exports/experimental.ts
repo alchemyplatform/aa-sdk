19b08d3c713a14fe35acbde945ac1280a96b1b99
Adam Egyed <5456061+adamegyed@users.noreply.github.com>
2025-09-22 16:12:21 -0400

    feat: add swapi decorator for sdk v4 (#2091)
    * feat: add swapi decorator v4
    
    * test: update transport snapshots

---
+++ account-kit/wallet-client/src/exports/experimental.ts
@@ -0,0 +1,10 @@
+// individual actions
+export { requestQuoteV0 } from "../experimental/actions/requestQuoteV0.js";
+export {
+  type RequestQuoteV0Params,
+  type RequestQuoteV0Result,
+} from "../experimental/actions/requestQuoteV0.js";
+
+// decorator
+export { swapActions } from "../experimental/swapActionsDecorator.js";
+export { type SwapActions } from "../experimental/swapActionsDecorator.js";
