f6a57c8967c6ade170ad5c16e7571ab48bb317a1
jakehobbs <jacob.hobbs@alchemy.com>
2025-09-22 16:09:17 -0700

    chore: remove unnecessary dev deps & do not use async mutations

--- examples/react-v5-example/src/app/page.tsx
+++ examples/react-v5-example/src/app/page.tsx
@@ -1,37 +1,34 @@
 "use client";
 
-import { useAccount, useConnectors } from "wagmi";
+import { useAccount } from "wagmi";
 import { useSendEmailOtp, useSubmitOtpCode } from "@alchemy/react";
 
 export default function Home() {
   const account = useAccount();
-  const connectors = useConnectors();
-  // TODO(jh): these hooks are failing to find the query provider.
-  // continue debugging, see here: https://github.com/TanStack/query/issues/3595
-  const { sendEmailOtpAsync } = useSendEmailOtp();
-  const { submitOtpCodeAsync } = useSubmitOtpCode();
+  const { sendEmailOtp } = useSendEmailOtp();
+  const { submitOtpCode } = useSubmitOtpCode();
 
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-12">
       {account.isConnected ? "Connected" : "Not connected"}
       <button
-        onClick={async () => {
+        onClick={() => {
           const email = prompt("Enter your email:");
           if (!email) {
             return;
           }
-          await sendEmailOtpAsync({ email });
+          sendEmailOtp({ email });
         }}
       >
         Send OTP
       </button>
       <button
-        onClick={async () => {
+        onClick={() => {
           const otpCode = prompt("Enter the OTP code:");
           if (!otpCode) {
             return;
           }
-          await submitOtpCodeAsync({ otpCode });
+          submitOtpCode({ otpCode });
         }}
       >
         Enter OTP
