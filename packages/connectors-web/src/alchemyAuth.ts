43ebcfb4febaf6094625f0a11d8df2c92b73ca00
Brian Chen <brian@alchemy.com>
2025-09-26 09:14:13 -0700

    chore: reset doc comments

--- packages/connectors-web/src/alchemyAuth.ts
+++ packages/connectors-web/src/alchemyAuth.ts
@@ -19,7 +19,6 @@ import {
   type PersistedAuthSession,
 } from "./store/authSessionStorage.js";
 
-// Options interface for the alchemyAuth connector
 export interface AlchemyAuthOptions
   extends Pick<
     WebAuthClientParams,
