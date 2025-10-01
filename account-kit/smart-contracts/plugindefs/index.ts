c679b2b0d82a79d9878a54aa496c1878c0177429
jakehobbs <jacob.hobbs@alchemy.com>
2025-10-01 15:34:51 -0700

    chore: remove account-kit

--- account-kit/smart-contracts/plugindefs/index.ts
+++
@@ -1,3 +0,0 @@
-export { MultiOwnerPluginGenConfig } from "./multi-owner/config.js";
-export { SessionKeyPluginGenConfig } from "./session-key/config.js";
-export { MultisigPluginGenConfig } from "./multisig/config.js";
