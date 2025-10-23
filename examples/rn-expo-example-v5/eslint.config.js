6bfcaf258b927d6906351cead9f9522472629f98
jakehobbs <jacob.hobbs@alchemy.com>
2025-10-23 16:28:44 -0700

    chore: update eslint config

--- examples/rn-expo-example-v5/eslint.config.js
+++ examples/rn-expo-example-v5/eslint.config.js
@@ -1,10 +1,15 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    rules: {
+      "import/extensions": "off",
+      "jsdoc/require-param-type": "off",
+      "jsdoc/require-returns-type": "off",
+    },
+    ignores: ["dist/*"],
   },
 ]);
