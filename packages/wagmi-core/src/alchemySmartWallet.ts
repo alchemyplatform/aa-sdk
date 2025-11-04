82b5beb8753609ceac94d9dd4a00d6532629f959
Brian Chen <brian@alchemy.com>
2025-11-04 10:34:36 -0500

    fix: revert alchemySmartWalletchanges

--- packages/wagmi-core/src/alchemySmartWallet.ts
+++ packages/wagmi-core/src/alchemySmartWallet.ts
@@ -11,6 +11,7 @@ import {
   type Account,
   type Address,
   type Chain,
+  type EIP1193Provider,
   type ProviderConnectInfo,
   type Transport,
   type WalletClient,
@@ -49,7 +50,7 @@ type ConnectResult = {
 
 type ChangeEvent = {
   chainId?: number;
-  accounts?: readonly Address[];
+  accounts?: Address[];
 };
 
 /**
@@ -126,8 +127,9 @@ export function alchemySmartWallet(
         const client = await innerConnector.getClient({
           chainId: currentChainId,
         });
-        if (client) {
-          return client as any;
+        if ("signMessage" in client) {
+          // Assume that a client which has `signMessage` is a wallet client.
+          return client as WalletClient<Transport, Chain, Account>;
         }
       }
       const innerAccounts = await innerConnector.getAccounts();
@@ -135,121 +137,152 @@ export function alchemySmartWallet(
         throw new BaseError("No accounts found in base connector");
       }
       const signerAddress = innerAccounts[0];
-      const baseProvider = (await innerConnector.getProvider()) as any;
+      const provider = (await innerConnector.getProvider()) as EIP1193Provider;
       const signer = createWalletClient({
         account: signerAddress,
         chain,
-        transport: custom(baseProvider),
+        transport: custom(provider),
       });
       return signer;
     };
 
-    async function getProviderIfConnected(): Promise<Provider | undefined> {
-      if (!providerPromise) return undefined;
-      try {
-        const provider = await providerPromise;
-        return provider;
-      } catch {
-        return undefined;
-      }
-    }
+    const createProvider =
+      async (): Promise<SmartWalletClientEip1193Provider> => {
+        const chainId = await innerConnector.getChainId();
+        const chain = getChainFromConfig(config, chainId);
+        const signer = await getSignerClient(chain);
 
-    const getAccounts = async (): Promise<Address[]> => {
-      const baseAccounts = await innerConnector.getAccounts();
-      if (baseAccounts.length === 0) {
-        throw new BaseError("No accounts found in owner connector");
-      }
-      const signer = baseAccounts[0];
-      if (!providerPromise) {
-        return [signer];
-      }
-      const provider = await providerPromise;
-      try {
-        const accounts = await provider.request({ method: "eth_accounts" });
-        return (accounts as any).length > 0 ? accounts : [signer];
-      } catch {
-        return [signer];
-      }
-    };
-
-    const outerHandleChange = async (event: ChangeEvent): Promise<void> => {
-      // Detect account changes.
-      if (event.accounts) {
-        const innerAccount = event.accounts[0];
-        if (innerAccount !== currentInnerAccount) {
-          currentInnerAccount = innerAccount;
-          config.emitter.emit("change", { accounts: await getAccounts() });
-        }
-      }
-
-      // Detect chain changes.
-      if (event.chainId && event.chainId !== currentChainId) {
-        currentChainId = event.chainId;
-        config.emitter.emit("change", { chainId: currentChainId });
-      }
-    };
-
-    const outerConnect = async (chainId: number): Promise<ConnectResult> => {
-      resetState();
-      currentChainId = chainId;
-      const chain = getChainFromConfig(config, chainId);
-      providerPromise = Promise.resolve(
-        createEip1193Provider(
+        return createEip1193Provider(
           {
-            chain,
+            signer,
             transport,
-            signer: await getSignerClient(chain),
+            chain,
             policyId: options.policyId,
             policyIds: options.policyIds,
           },
-          {},
-        ),
-      );
+          // TODO(v5): support other account params
+          // https://app.asana.com/1/1129441638109975/project/1210112085973163/task/1211568982181252
+          {
+            accountType: "7702",
+          },
+        );
+      };
 
-      config.emitter.emit("connect", {
-        account: (await getAccounts())[0],
-        chainId,
-        provider: await providerPromise,
-      } as any);
-
-      return { chainId, accounts: await getAccounts() };
+    const initializeSmartWalletClient = async (): Promise<void> => {
+      providerPromise = createProvider();
+      const provider = await providerPromise;
+      const innerAccounts = await innerConnector.getAccounts();
+      if (innerAccounts.length === 0) {
+        throw new BaseError("No accounts found in owner connector");
+      }
+      const chainId = await provider.request({ method: "eth_chainId" });
+      currentChainId = +chainId;
+      currentInnerAccount = innerAccounts[0];
     };
 
-    const outerDisconnect = (): void => {
-      if (providerPromise) {
-        const provider = providerPromise;
-        // Ignore the result of this promise.
-        void provider.then((_provider) => {
-          // No-op: provider cleanup not required
-        });
+    // A trivial function, but gives a name to indicate how the promise is used.
+    const getProviderIfConnected = async (): Promise<
+      SmartWalletClientEip1193Provider | undefined
+    > => {
+      return providerPromise;
+    };
+
+    const getAccounts = async (): Promise<Address[]> => {
+      const provider = await getProviderIfConnected();
+      if (!provider) {
+        return [];
       }
+      return provider.request({ method: "eth_accounts" });
+    };
+
+    // TODO: to be robust, we should have handling for additional updates
+    // occurring while a previous update is still in progress.
+
+    // The following "outer" functions update this connector's state without
+    // touching the inner connector. This lets us reuse logic between when a
+    // change is triggered externally (in which case we need to notify the inner
+    // connector) vs when it's triggered by the inner connector itself (in which
+    // case we don't).
+
+    /**
+     * Handles a change event for this connector, emitting its own change event after updating its state.
+     *
+     * @param {ChangeEvent} event - The change event.
+     * @returns {Promise<void>} A promise that resolves when the change event has been handled.
+     */
+    const outerHandleChange = async ({
+      chainId,
+      accounts,
+    }: ChangeEvent): Promise<void> => {
+      const innerAccount = accounts?.[0];
+      const chainIdChanged = chainId !== currentChainId;
+      const innerAccountChanged = innerAccount !== currentInnerAccount;
+      if (!chainIdChanged && !innerAccountChanged) {
+        return;
+      }
+      currentChainId = chainId;
+      currentInnerAccount = innerAccount;
+      try {
+        await initializeSmartWalletClient();
+      } catch (error) {
+        resetState();
+        throw error;
+      }
+      config.emitter.emit("change", {
+        ...(chainIdChanged && { chainId }),
+        ...(innerAccountChanged && { accounts: await getAccounts() }),
+      });
+    };
+
+    /**
+     * Handles connecting to a chain for this connector without touching the inner
+     * connector.
+     *
+     * @param {number} chainId - The chain ID to connect to.
+     * @returns {ConnectResult} The smart account address and chain ID.
+     */
+    const outerConnect = async (chainId: number): Promise<ConnectResult> => {
+      resetState();
+      await initializeSmartWalletClient();
+      const accounts = await getAccounts();
+      config.emitter.emit("connect", { chainId, accounts });
+      return { accounts, chainId };
+    };
+
+    /**
+     * Handles disconnection of this connector without touching the inner
+     * connector.
+     */
+    const outerDisconnect = (): void => {
       resetState();
       config.emitter.emit("disconnect");
     };
 
+    /**
+     * Handles switching the chain for this connector without touching the inner
+     * connector.
+     *
+     * @param {number} chainId - The chain ID to switch to.
+     */
     const outerSwitchChain = async (chainId: number): Promise<void> => {
-      // Track whether we're switching a chain via external invocation or an inner connector event.
-      // If invoked externally, emit a change event after switching.
-      // If triggered by an inner connector event, connect will follow and emit the proper events.
-      const isExternalInvocation = chainId !== currentChainId;
-      const connectResult = await innerConnector.connect({ chainId });
-      if (isExternalInvocation) {
-        outerHandleChange(connectResult);
-      } else {
-        outerConnect(connectResult.chainId);
-      }
+      await outerHandleChange({ chainId });
     };
-    innerEmitter.on("disconnect", outerDisconnect);
-    innerEmitter.on("change", (event) => {
-      outerHandleChange(event as any as ChangeEvent);
-    });
+
     innerEmitter.on("connect", (params) => {
+      // The inner connector may emit a connect event while we're already
+      // connected. If so, we should emit a change event instead.
       const connectResult = params as any as ConnectResult;
+      // TODO: if we add more robust handling around simultaneous updates, we
+      // should modify this check for connected status.
       if (currentInnerAccount != null) {
         outerHandleChange(connectResult);
       } else {
         outerConnect(connectResult.chainId);
       }
+    });
+    innerEmitter.on("disconnect", outerDisconnect);
+    innerEmitter.on("change", (event) => {
+      outerHandleChange(event as any as ChangeEvent);
     });
 
     // Cache clients by chainId to avoid recreating them unnecessarily.
