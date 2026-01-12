7f0b05a0a351076dff117fe2d01ced03865b08a5
jakehobbs <jacob.hobbs@alchemy.com>
2026-01-12 11:32:29 -0800

    test: add missing swap test from v4

--- packages/wallet-apis/src/experimental/swapActionsDecorator.e2e.test.ts
+++ packages/wallet-apis/src/experimental/swapActionsDecorator.e2e.test.ts
@@ -1,16 +1,15 @@
-import { arbitrum } from "viem/chains";
+import { arbitrum, base } from "viem/chains";
 import { apiTransport } from "../testSetup.js";
 import { createSmartWalletClient } from "../client.js";
 import { swapActions } from "./swapActionsDecorator.js";
 import { privateKeyToAccount } from "viem/accounts";
+import { size, toHex } from "viem";
 
 describe("swapActions decorator tests", () => {
   it("should successfully request a quote", async () => {
     const dummySigner = privateKeyToAccount(
       "0xd7b061ef04d29cf68b3c89356678eccec9988de8d5ed892c19461c4a9d65925d",
     );
-
-    const testAccountAddr = "0x0d1Ea60Dddd2a76F3a3afD6d78660d366C6A30c0";
 
     const client = createSmartWalletClient({
       transport: apiTransport,
@@ -19,7 +18,6 @@ describe("swapActions decorator tests", () => {
     }).extend(swapActions);
 
     const account = await client.requestAccount({
-      signerAddress: testAccountAddr,
       creationHint: {
         accountType: "7702",
       },
@@ -39,5 +37,42 @@ describe("swapActions decorator tests", () => {
     expect(quote.quote.fromAmount).toBeDefined();
     expect(quote.quote.minimumToAmount).toBeDefined();
     expect(quote.quote.expiry).toBeDefined();
+    expect(quote.callId).not.toBeDefined();
   });
+
+  it("should successfully request a cross-chain quote", async () => {
+    const dummySigner = privateKeyToAccount(
+      "0xd7b061ef04d29cf68b3c89356678eccec9988de8d5ed892c19461c4a9d65925d",
+    );
+
+    const client = createSmartWalletClient({
+      transport: apiTransport,
+      chain: arbitrum,
+      signer: dummySigner,
+    }).extend(swapActions);
+
+    const account = await client.requestAccount({
+      creationHint: {
+        accountType: "7702",
+      },
+    });
+
+    const USDC_ARB = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
+    const NATIVE_TOKEN_ADDR = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEee"; // from ERC-7528
+
+    const quote = await client.requestQuoteV0({
+      from: account.address,
+      fromToken: USDC_ARB,
+      toToken: NATIVE_TOKEN_ADDR,
+      toChainId: toHex(base.id),
+      minimumToAmount: "0x5AF3107A4000",
+    });
+
+    expect(quote.quote).toBeDefined();
+    expect(quote.quote.fromAmount).toBeDefined();
+    expect(quote.quote.minimumToAmount).toBeDefined();
+    expect(quote.quote.expiry).toBeDefined();
+    expect(quote.callId).toBeDefined();
+    expect(size(quote.callId!)).toBeGreaterThan(64);
+  }, 30_000);
 });
